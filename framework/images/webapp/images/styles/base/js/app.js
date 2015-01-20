b077f01c00ebf5c9423dcb4c342b80723f19b976
Paul Piper <pp@ilscipio.com>
2015-01-20 18:29:42 +0000

    NoRef
    * Fixed a bug  concerning the slider
    
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@6936 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/images/webapp/images/styles/base/js/app.js
+++ framework/images/webapp/images/styles/base/js/app.js
@@ -2,10 +2,10 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 
 function collapseFieldset(){
-	$( ".toggleField .row" ).wrapAll( "<div class='hide' />");
+	$( ".toggleField .row" ).wrapAll( "<div class='collapsehide' style='display:none'/>");
 	$(".toggleField legend").click(function(){
         $(this).children("i").toggleClass(" fa-arrow-right").toggleClass(" fa-arrow-down");
-        $(this).nextAll("div.hide").slideToggle(500);
+        $(this).nextAll("div.collapsehide").slideToggle(500);
 	});
 }
 

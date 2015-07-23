812ebc889620304599c11d97ded061c1af44a87d
Pascal Proulx <pplx@ilscipio.com>
2015-07-23 19:58:20 +0000

    NoRef now an object; misc fixes; labels load on demand only
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@7329 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- applications/party/webapp/partymgr/static/PartyProfileContent.js
+++ applications/party/webapp/partymgr/static/PartyProfileContent.js
@@ -17,19 +17,16 @@ specific language governing permissions and limitations
 under the License.
 * */
 
-var ppcUiLabelJsonObjects = null;
-function ppcLoadUiLabels() {
-	if (ppcUiLabelJsonObjects == null) {
+var catoUploadProgLabels = null;
+function catoLoadUploadProgressLabels() {
+	if (catoUploadProgLabels == null) {
 	    var labelObject = {
 	            "CommonUiLabels" : ["CommonUpload", "CommonSave", "CommonCompleted", "CommonError", "CommonServerCommunicationError"]
 	          };
-	    ppcUiLabelJsonObjects = getJSONuiLabels(labelObject);
+	    catoUploadProgLabels = getJSONuiLabels(labelObject);
 	}
 }
 
-jQuery(document).ready(function() {
-	ppcLoadUiLabels();
-});
 
 function CatoUploadProgress(options) {
 	if (!options) {
@@ -43,137 +40,143 @@ function CatoUploadProgress(options) {
     this.msgContainerId = options.msgContainerId; // required ("#content-messages"), if no sibling must exist, if sibling must NOT exist
     this.msgContainerSiblingId = options.msgContainerSiblingId; // optional ("#partyContentList"), if not specified, won't generate
     
+    
+    /* Public functions */
+    
     this.reset = function() {
-    	ppcLoadUiLabels();
-        ppcResetUploadProgress();
+    	catoLoadUploadProgressLabels();
+        this.resetProgress();
     };
     
     this.initUpload = function() {
-    	ppcLoadUiLabels();
-        ppcUploadPartyContent();
-        ppcGetUploadProgressStatus();
+    	catoLoadUploadProgressLabels();
+        this.resetInitContainers();
+        this.beginProgressStatus();
     };
-}
 
 
-function ppcSetUploadProgressValue(percent) {
-	jQuery("#upc_progress_bar_meter").css({"width": percent + "%"});
-	if (typeof jQuery("#upc_progress_bar_meter").attr("aria-valuenow") !== 'undefined') {
-		jQuery("#upc_progress_bar_meter").attr("aria-valuenow", percent.toString());
-	}
-	jQuery('#upcProgressBarSavingMsg').html(ppcUiLabelJsonObjects.CommonUiLabels[0] + "... (" + percent + "%)");
-}
-
-function ppcSetUploadProgressState(classStr) {
-	var stateStyles = [catoStyles.color_info, catoStyles.color_success, catoStyles.color_alert, catoStyles.color_warning].join(" ");
-	jQuery("#upc_progress_bar").removeClass(stateStyles).addClass(classStr);
-	jQuery('#upcProgressBarSavingMsg').removeClass(stateStyles).addClass(classStr);
-}
-
-function ppcSetUploadProgressMsg(msg) {
-	jQuery('#upcProgressBarSavingMsg').html(msg);
-}
-
-function ppcResetUploadProgress() {
-	ppcSetUploadProgressValue(0);
-	ppcSetUploadProgressState(catoStyles.color_info)
-}
-
-function ppcUploadPartyContent(event){
-	ppcResetUploadProgress();
-	jQuery("#upc_progress_bar").removeClass(catoStyles.hidden);
-    var targetFrame = jQuery('#target_upload');
-    var infodiv = jQuery('#content-messages');
-    if(infodiv.length < 1){
-        jQuery('<div class="' + catoStyles.grid_row + '"><div class="' + catoStyles.grid_large + '12 ' + catoStyles.grid_cell + '" id="content-messages"></div></div>').insertAfter(jQuery("#partyContentList"));
-    }
-    if (targetFrame.length < 1){
-        jQuery('#partyContent').append("<iframe id='target_upload' name='target_upload' style='display: none' src=''> </iframe>");
-    }
-    jQuery('#uploadPartyContent').attr("target", "target_upload");
-
-    var labelField = jQuery("#upcProgressBarSavingMsg");
-    if (labelField.length) {
-        labelField.remove();
-    }
-}
-
-function ppcUploadCompleted(){
-    var iframePartyContentList = jQuery("#target_upload").contents().find("#partyContentList").html();
-
-    // update partyContentList - copy the Data from the iFrame partyContentList
-    // to the page partyContentList
-    jQuery("#partyContentList").html(iframePartyContentList);
-
-    ppcSetUploadProgressValue(100);
-    ppcSetUploadProgressState(catoStyles.color_success);
-    ppcSetUploadProgressMsg(ppcUiLabelJsonObjects.CommonUiLabels[2]);
+    /* Private functions */
     
-    // remove iFrame
-    jQuery("#target_upload").remove();
-    return;
+	this.setProgressValue = function(percent) {
+		jQuery("#upc_progress_bar_meter").css({"width": percent + "%"});
+		if (typeof jQuery("#upc_progress_bar_meter").attr("aria-valuenow") !== 'undefined') {
+			jQuery("#upc_progress_bar_meter").attr("aria-valuenow", percent.toString());
+		}
+		jQuery('#upcProgressBarSavingMsg').html(catoUploadProgLabels.CommonUiLabels[0] + "... (" + percent + "%)");
+	};
+	
+	this.setProgressState = function(classStr) {
+		var stateStyles = [catoStyles.color_info, catoStyles.color_success, catoStyles.color_alert, catoStyles.color_warning].join(" ");
+		jQuery("#upc_progress_bar").removeClass(stateStyles).addClass(classStr);
+		jQuery('#upcProgressBarSavingMsg').removeClass(stateStyles).addClass(classStr);
+	};
+	
+	this.setProgressMsg = function(msg) {
+		jQuery('#upcProgressBarSavingMsg').html(msg);
+	};
+	
+	this.resetProgress = function() {
+		this.setProgressValue(0);
+		this.setProgressState(catoStyles.color_info)
+	};
+	
+	this.resetInitContainers = function(event){
+		this.resetProgress();
+		jQuery("#upc_progress_bar").removeClass(catoStyles.hidden);
+	    var targetFrame = jQuery('#target_upload');
+	    var infodiv = jQuery('#content-messages');
+	    if(infodiv.length < 1){
+	        jQuery('<div class="' + catoStyles.grid_row + '"><div class="' + catoStyles.grid_large + '12 ' + catoStyles.grid_cell + '" id="content-messages"></div></div>').insertAfter(jQuery("#partyContentList"));
+	    }
+	    if (targetFrame.length < 1){
+	        jQuery('#partyContent').append("<iframe id='target_upload' name='target_upload' style='display: none' src=''> </iframe>");
+	    }
+	    jQuery('#uploadPartyContent').attr("target", "target_upload");
+	
+	    var labelField = jQuery("#upcProgressBarSavingMsg");
+	    if (labelField.length) {
+	        labelField.remove();
+	    }
+	};
+	
+	this.processUploadComplete = function(){
+	    var iframePartyContentList = jQuery("#target_upload").contents().find("#partyContentList").html();
+	
+	    // update partyContentList - copy the Data from the iFrame partyContentList
+	    // to the page partyContentList
+	    jQuery("#partyContentList").html(iframePartyContentList);
+	
+	    this.setProgressValue(100);
+	    this.setProgressState(catoStyles.color_success);
+	    this.setProgressMsg(catoUploadProgLabels.CommonUiLabels[2]);
+	    
+	    // remove iFrame
+	    jQuery("#target_upload").remove();
+	    return;
+	};
+	
+	this.checkIframeStatus = function() {
+	    var iframePartyContentList = null;
+	    // if the new partyContentList isn't created wait a few ms and call the
+	    // method again
+	    var uploadProg = this;
+	    jQuery.fjTimer({
+	        interval: 500,
+	        repeat: true,
+	        tick: function(counter, timerId) {
+	            iframePartyContentList = jQuery("#target_upload").contents().find("#partyContentList");
+	            if (iframePartyContentList != null && iframePartyContentList.length > 0) {
+	                timerId.stop();
+	                uploadProg.processUploadComplete();
+	            }
+	        }
+	    });
+	    return;
+	};
+	
+	this.showError = function(errdata) {
+		jQuery('#content-messages').html('<div data-alert class="' + catoStyles.alert_wrap + ' ' + catoStyles.alert_prefix_type + 'alert">' + errdata + "</div>");
+		this.setProgressState(catoStyles.color_alert);
+		this.setProgressMsg(catoUploadProgLabels.CommonUiLabels[3]);
+	};
+	
+	this.beginProgressStatus = function(event){
+	    jQuery('#upcProgressMsgBox').append("<span id='upcProgressBarSavingMsg' class='label'>" + catoUploadProgLabels.CommonUiLabels[0] + "...</span>");
+	    var i=0;
+	    var uploadProg = this;
+	    jQuery.fjTimer({
+	        interval: 1000,
+	        repeat: true,
+	        tick: function(counter, timerId) {
+	            var timerId = timerId;
+	            jQuery.ajax({
+	                url: getOfbizUrl('getFileUploadProgressStatus'),
+	                dataType: 'json',
+	                success: function(data) {
+	                    if (data._ERROR_MESSAGE_LIST_ != undefined) {
+	                    	uploadProg.showError(data._ERROR_MESSAGE_LIST_);
+	                        timerId.stop();
+	                    } else if (data._ERROR_MESSAGE_ != undefined) {
+	                    	uploadProg.showError(data._ERROR_MESSAGE_);
+	                        timerId.stop();
+	                    } else {
+	                        var readPercent = data.readPercent;
+	                        uploadProg.setProgressValue(readPercent);
+	                        if(readPercent > 99){
+	                        	uploadProg.setProgressMsg(catoUploadProgLabels.CommonUiLabels[1] + "...");
+	                            // stop the fjTimer
+	                            timerId.stop();
+	                            // call the upload complete method to do final stuff
+	                            uploadProg.checkIframeStatus();
+	                        }
+	                     }
+	                },
+	                error: function(data) {
+	                	uploadProg.showError(catoUploadProgLabels.CommonUiLabels[4]);
+	                    timerId.stop();
+	                }
+	            });
+	        }
+	    });
+	};
 }
-
-function ppcCheckIframeStatus() {
-    var iframePartyContentList = null;
-    // if the new partyContentList isn't created wait a few ms and call the
-    // method again
-    jQuery.fjTimer({
-        interval: 500,
-        repeat: true,
-        tick: function(counter, timerId) {
-            iframePartyContentList = jQuery("#target_upload").contents().find("#partyContentList");
-            if (iframePartyContentList != null && iframePartyContentList.length > 0) {
-                timerId.stop();
-                ppcUploadCompleted();
-            }
-        }
-    });
-    return;
-}
-
-function ppcShowUploadError(errdata) {
-	jQuery('#content-messages').html('<div data-alert class="' + catoStyles.alert_wrap + ' ' + catoStyles.alert_prefix_type + 'alert">' + errdata + "</div>");
-	ppcSetUploadProgressState(catoStyles.color_alert);
-	ppcSetUploadProgressMsg(ppcUiLabelJsonObjects.CommonUiLabels[3]);
-}
-
-function ppcGetUploadProgressStatus(event){
-    jQuery('#upcProgressMsgBox').append("<span id='upcProgressBarSavingMsg' class='label'>" + ppcUiLabelJsonObjects.CommonUiLabels[0] + "...</span>");
-    var i=0;
-    jQuery.fjTimer({
-        interval: 1000,
-        repeat: true,
-        tick: function(counter, timerId) {
-            var timerId = timerId;
-            jQuery.ajax({
-                url: getOfbizUrl('getFileUploadProgressStatus'),
-                dataType: 'json',
-                success: function(data) {
-                    if (data._ERROR_MESSAGE_LIST_ != undefined) {
-                    	ppcShowUploadError(data._ERROR_MESSAGE_LIST_);
-                        timerId.stop();
-                     } else if (data._ERROR_MESSAGE_ != undefined) {
-                    	 ppcShowUploadError(data._ERROR_MESSAGE_);
-                        timerId.stop();
-                     } else {
-                        var readPercent = data.readPercent;
-                        ppcSetUploadProgressValue(readPercent);
-                        if(readPercent > 99){
-                        	ppcSetUploadProgressMsg(ppcUiLabelJsonObjects.CommonUiLabels[1] + "...");
-                            // stop the fjTimer
-                            timerId.stop();
-                            // call the upload complete method to do final stuff
-                            ppcCheckIframeStatus();
-                        }
-                     }
-                },
-                error: function(data) {
-                	ppcShowUploadError(ppcUiLabelJsonObjects.CommonUiLabels[4]);
-                    timerId.stop();
-                }
-            });
-        }
-    });
-}
-

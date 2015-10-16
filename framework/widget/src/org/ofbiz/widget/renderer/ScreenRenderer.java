39542d2cfcac613c0f6dd8555b61d81b16348451
Pascal Proulx <pplx@ilscipio.com>
2015-10-16 19:22:19 +0000

    NoRef/OFBIZ patch: rendererVisualThemeResources patch: part 3: implemented new version of getVisualThemeResources that supports per-WebSite visualThemeSelectorScript, and added field to WebSite entity. added load call to populateContextForRequest and populateContextForService so will run on all types of contexts (html renderer and most emails) (note: unclear if best run here or in ScreenRenderer.render). WARN: all non-request theme selection is untested.
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@8074 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/widget/src/org/ofbiz/widget/renderer/ScreenRenderer.java
+++ framework/widget/src/org/ofbiz/widget/renderer/ScreenRenderer.java
@@ -317,6 +317,9 @@ public class ScreenRenderer {
             context.put("isError", Boolean.TRUE);
         }
 
+        // Cato: ensure rendererVisualThemeResources has been set (only other central place for this call would be render() method)
+        getVisualThemeResources(context);
+        
         populateContextScripts(context);
         
         // to preserve these values, push the MapStack
@@ -368,6 +371,9 @@ public class ScreenRenderer {
         this.populateBasicContext(serviceContext, dctx.getDelegator(), dctx.getDispatcher(),
                 dctx.getSecurity(), (Locale) serviceContext.get("locale"), (GenericValue) serviceContext.get("userLogin"));
         
+        // Cato: ensure rendererVisualThemeResources has been set (only other central place for this call would be render() method)
+        getVisualThemeResources(context);
+        
         populateContextScripts(serviceContext);
     }
     
@@ -375,16 +381,20 @@ public class ScreenRenderer {
      * Cato: Gets visual theme resources from context or if missing, calculates appropriate
      * based on values in context, best-effort, using as generalized logic as possible, saves in context and returns.
      * <p>
-     * Resources are read from and stored in <code>rendererVisualThemeResources</code> context variable.
+     * Resources are cached in <code>rendererVisualThemeResources</code> context variable.
      */
-    public static Map<String, List<String>> getVisualThemeResources(Map<String, Object> context) throws GenericServiceException {
+    public static Map<String, List<String>> getVisualThemeResources(Map<String, Object> context) {
         Map<String, List<String>> themeResources = null;
         
         if (Boolean.TRUE.equals((Boolean) context.get("rendererVisualThemeResourcesChecked"))) {
             themeResources = UtilGenerics.cast(context.get("rendererVisualThemeResources"));
         }
         else {
-            themeResources = loadVisualThemeResources(context);
+            try {
+                themeResources = loadVisualThemeResources(context);
+            } catch (GenericServiceException e) {
+                Debug.logError(e, "Could not load visual theme resources", module);
+            }
 
             context.put("rendererVisualThemeResources", themeResources);
             context.put("rendererVisualThemeResourcesChecked", Boolean.TRUE);
@@ -399,7 +409,65 @@ public class ScreenRenderer {
      * Original code only checked for preference of userPreferences.
      */    
     public static Map<String, List<String>> loadVisualThemeResources(Map<String, Object> context) throws GenericServiceException {
+        HttpServletRequest request = (HttpServletRequest) context.get("request");
+        Map<String, List<String>> themeResources = null;
+        if (request != null) {
+            GenericValue webSite = WebSiteWorker.getWebSite(request);
+            String script = null;
+            if (webSite != null) {
+                script = webSite.getString("visualThemeSelectorScript");
+            }
+            
+            if (UtilValidate.isNotEmpty(script)) {
+                themeResources = loadVisualThemeResourcesFromScript(context, script, webSite);
+            }
+            else {
+                // original stock behavior
+                themeResources = getVisualThemeResourcesFromUserPrefs(context);
+            }
+        }
+        else {
+            // this might be an email...
+            String webSiteId = (String) context.get("baseWebSiteId"); // set by org.ofbiz.common.email.NotificationServices#setBaseUrl
+            if (UtilValidate.isEmpty(webSiteId)) {
+                // FIXME? fallback to webSiteId var, but it's not clear this is a good idea
+                webSiteId = (String) context.get("webSiteId");
+            }
+            String script = null;
+            GenericValue webSite = null;
+            if (UtilValidate.isNotEmpty(webSiteId)) {
+                webSite = WebSiteWorker.findWebSite((Delegator) context.get("delegator"), webSiteId);
+                if (webSite != null) {
+                    script = webSite.getString("visualThemeSelectorScript");
+                }
+            }
+            
+            if (UtilValidate.isNotEmpty(script)) {
+                themeResources = loadVisualThemeResourcesFromScript(context, script, webSite);
+            }
+            else {
+                // FIXME? not sure original stock behavior makes sense here because userLoginId ambiguous - may not be end user
+                //themeResources = getVisualThemeResourcesFromUserPrefs(context);
+                themeResources = getDefaultVisualThemeResources(context);           
+            }
+        }
+        return themeResources;
+    }
+    
+    /**
+     * Cato: Gets resources from user preferences already in context.
+     * <p>
+     * Code migrated from {@link org.ofbiz.widget.renderer.macro.MacroScreenViewHandler#loadRenderers(HttpServletRequest, 
+     * HttpServletResponse, Map<String, Object>, Writer)}.
+     * @throws GenericServiceException 
+     */
+    public static Map<String, List<String>> getVisualThemeResourcesFromUserPrefs(Map<String, Object> context) throws GenericServiceException {
         Map<String, Object> userPreferences = UtilGenerics.cast(context.get("userPreferences"));
+        return getVisualThemeResourcesFromUserPrefs(context, userPreferences);
+    }
+    
+    public static Map<String, List<String>> getVisualThemeResourcesFromUserPrefs(Map<String, Object> context, 
+            Map<String, Object> userPreferences) throws GenericServiceException {
         if (userPreferences != null) {
             String visualThemeId = (String) userPreferences.get("VISUAL_THEME");
             if (visualThemeId != null) {
@@ -415,4 +483,46 @@ public class ScreenRenderer {
         }
         return null;
     }
+    
+    /**
+     * Cato: Gets default system visual theme resources, i.e. for _NA_ user login.
+     */
+    public static Map<String, List<String>> getDefaultVisualThemeResources(Map<String, Object> context) throws GenericServiceException {
+        // emulate default call exactly, but with no userLogin
+        LocalDispatcher dispatcher = (LocalDispatcher) context.get("dispatcher");
+        Map<String, Object> userPreferences = null;
+        try {
+            Map<String, Object> result = dispatcher.runSync("getUserPreferenceGroup", UtilMisc.toMap("userLogin", null, "userPrefGroupTypeId", "GLOBAL_PREFERENCES"));
+            userPreferences = UtilGenerics.cast(result.get("userPrefMap"));
+        } catch (GenericServiceException e) {
+            Debug.logError(e, "Error while getting user preferences: ", module);
+        }
+        
+        return getVisualThemeResourcesFromUserPrefs(context, userPreferences);
+    }
+    
+    /**
+     * Cato: Invokes a script to choose a visualThemeId and load its resources.
+     */
+    public static Map<String, List<String>> loadVisualThemeResourcesFromScript(Map<String, Object> context, 
+            String script, GenericValue webSite) throws GenericServiceException {
+        Map<String, Object> scriptCtx = new HashMap<String, Object>(context);
+        scriptCtx.put("webSite", webSite);
+        scriptCtx.remove("visualThemeId");
+        ScriptUtil.executeScript(script, null, scriptCtx);
+        
+        String visualThemeId = (String) scriptCtx.get("visualThemeId");
+        if (UtilValidate.isNotEmpty(visualThemeId)) {
+            LocalDispatcher dispatcher = (LocalDispatcher) context.get("dispatcher");
+            Map<String, Object> serviceCtx = dispatcher.getDispatchContext().makeValidContext("getVisualThemeResources",
+                    ModelService.IN_PARAM, context);
+            serviceCtx.put("visualThemeId", visualThemeId);
+            Map<String, Object> serviceResult = dispatcher.runSync("getVisualThemeResources", serviceCtx);
+            if (ServiceUtil.isSuccess(serviceResult)) {
+                return UtilGenerics.cast(serviceResult.get("themeResources"));
+            }
+        }
+        return null;
+    }
+
 }

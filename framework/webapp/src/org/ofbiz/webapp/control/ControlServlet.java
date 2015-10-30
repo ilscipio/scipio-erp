31da07e8d9d1887a7a28d06410d703000318fcc3
Pascal Proulx <pplx@ilscipio.com>
2015-10-30 20:32:19 +0000

    NoRef/OFBIZ patch: log fix
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@8429 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/webapp/src/org/ofbiz/webapp/control/ControlServlet.java
+++ framework/webapp/src/org/ofbiz/webapp/control/ControlServlet.java
@@ -247,7 +247,7 @@ public class ControlServlet extends HttpServlet {
                 try {
                     rd.forward(request, response); //CATO: Changed from include to forward so that the response can be handled appropriately
                 } catch (Throwable t) {
-                    Debug.logWarning("Error while trying to send error page using rd.include (will try response.getOutputStream or response.getWriter): " + t.toString(), module);
+                    Debug.logWarning("Error while trying to send error page using rd.forward (will try response.getOutputStream or response.getWriter): " + t.toString(), module);
 
                     String errorMessage = "ERROR rendering error page [" + errorPage + "], but here is the error text: " + request.getAttribute("_ERROR_MESSAGE_");
                     try {

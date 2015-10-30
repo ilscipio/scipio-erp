d7bf7a0482745b289e9719b8a763f8b7e2c1004f
Pascal Proulx <pplx@ilscipio.com>
2015-10-30 23:23:14 +0000

    NoRef created a PrepareNewScreenRender hook in widget context scripts (prior cato renderer patch); clears request vars so more cache problems across servlet forwards
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@8431 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

---
+++ framework/common/webcommon/WEB-INF/actions/includes/cato/PrepareNewScreenRender.groovy
@@ -0,0 +1,21 @@
+/**
+ * Cato: Prepares request and widget context for new render.
+ * <p>
+ * Invoked by the screen renderer upon start of view/screen rendering.
+ * <p>
+ * This is essential to:
+ * - clean up request vars across servlet forwards
+ */
+
+import org.ofbiz.base.util.template.FreeMarkerWorker;
+import freemarker.template.TemplateHashModelEx;
+import freemarker.template.TemplateScalarModel;
+import org.ofbiz.base.util.*;
+import com.ilscipio.cato.webapp.ftl.FtlTransformUtil;
+import com.ilscipio.cato.webapp.ftl.CommonFtlUtil;
+
+final module = "PrepareNewScreenRender.groovy"
+
+Debug.logInfo("Cato: Preparing new templating API render; clearing request-scope templating vars", module);
+
+CommonFtlUtil.clearRequestVars(context.request, context, FreeMarkerWorker.getCurrentEnvironment());

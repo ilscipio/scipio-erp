4f24d1394292ba5eb5262e4e2710cc0de836dc27
Pascal Proulx <pplx@ilscipio.com>
2016-04-08 21:59:35 +0000

    NoRef LangFtlUtil: add a TemplateNullModel
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@10135 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

---
+++ framework/webapp/src/com/ilscipio/cato/ce/webapp/ftl/lang/TemplateNullModel.java
@@ -0,0 +1,24 @@
+package com.ilscipio.cato.ce.webapp.ftl.lang;
+
+import freemarker.template.AdapterTemplateModel;
+
+/**
+ * Cato: Special Freemarker TemplateModel that can be used to represent null in some functions.
+ */
+public final class TemplateNullModel implements AdapterTemplateModel {
+
+    public static final TemplateNullModel nullModel = new TemplateNullModel();
+    
+    public static TemplateNullModel getNullModel() {
+        return nullModel;
+    }
+    
+    private TemplateNullModel(){
+    }
+    
+    @Override
+    public Object getAdaptedObject(Class arg0) {
+        return null;
+    }
+    
+}

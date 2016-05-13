d823fc4b5452db019612b84b5bb662e1ce08500d
Jonatan Soto <jonatan.soto@ilscipio.com>
2016-05-13 12:09:48 +0000

    Refs #1114: Marketing campaing screens cleanup completed.
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@10629 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- applications/marketing/webapp/marketing/WEB-INF/actions/campaign/FindMarketingCampaign.groovy
+++ applications/marketing/webapp/marketing/WEB-INF/actions/campaign/FindMarketingCampaign.groovy
@@ -1,5 +1,17 @@
 
+marketingCampaignId = parameters.marketingCampaignId;
+campaignName = parameters.campaignName;
+parentCampaignId = parameters.parentCampaignId;
+statusId = parameters.statusId;
 
-//parameters.
-//
-//listMarketingCampaign=from("MarketingCampaign").where("").queryList
+condition=[:];
+if (marketingCampaignId)
+    condition.put("marketingCampaignId", marketingCampaignId);
+if (campaignName)
+    condition.put("campaignName", campaignName);
+if (parentCampaignId)    
+    condition.put("parentCampaignId", parentCampaignId);
+if (statusId)
+    condition.put("statusId", statusId);
+listMarketingCampaign=from("MarketingCampaign").where(condition).queryList();
+context.listMarketingCampaign=listMarketingCampaign;

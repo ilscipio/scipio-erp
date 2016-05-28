b3c325137251516bfc8bdcde335837d7f94e7463
Paul Piper <pp@ilscipio.com>
2016-05-28 15:08:11 +0000

    NoRef
    * WIP on a new metro theme (*requires reseed*)
    
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@10827 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

---
+++ themes/metro/webapp/metro/js/cato_boxes.js
@@ -0,0 +1,21 @@
+/**
+ * CATO: jquery plugin to add a class to subsets of html elements. 
+ * Used to add classes to elements, so that they can be styled as boxes.
+ * */
+var elSelector = 'pre.scrollable,.section-screenlet-content';
+var notSubSelector = 'pre.scrollable,.section-screenlet-content';
+var catoBoxClass = 'box';
+
+
+function cato_boxify(){
+	$(elSelector).each(function(){
+		if($(this).has(notSubSelector).length == 0 && $(this).text().trim().length > 0){
+			$(this).addClass(catoBoxClass);
+		}
+	});
+}
+
+$(function(){
+	cato_boxify();
+});
+

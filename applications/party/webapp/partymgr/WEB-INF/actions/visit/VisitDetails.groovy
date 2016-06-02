600f29d55fcc812c94fc15d3ac556bfbcbce556b
Jonatan Soto <jonatan.soto@ilscipio.com>
2016-06-02 10:25:09 +0000

    Refs #1135: Covered party groovy files (just one)
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@10923 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- applications/party/webapp/partymgr/WEB-INF/actions/visit/VisitDetails.groovy
+++ applications/party/webapp/partymgr/WEB-INF/actions/visit/VisitDetails.groovy
@@ -36,7 +36,7 @@ try {
     viewIndex = 0;
 }
 
-// Cato: unhardcode
+// Scipio: unhardcode
 //viewSize = 20;
 defaultViewSize = org.ofbiz.base.util.UtilProperties.getPropertyAsInteger("general.properties", "record.paginate.defaultViewSize", 20);
 viewSize = defaultViewSize;

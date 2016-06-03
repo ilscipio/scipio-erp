dbd8ac95ddc99b7c0e7f90c763922abc2fe2a9ba
Jonatan Soto <jonatan.soto@ilscipio.com>
2016-06-03 20:50:08 +0000

    Refs #1099: Removed all unneeded sh|bat scripts. Renamed the stock startOfbiz.sh|bat and placed in the root folder. Removed the new script and all its references. Updated installation instructions in README
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@11016 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# set OFBIZ's relevant paths
-OFBIZ_HOME="$( cd -P "$( dirname "$0" )" && pwd )"
-
-echo "Scipio-Commerce: Stopping framework..."        
-sh ant stop &
-echo "Scipio-Commerce: Framework successfully stopped."

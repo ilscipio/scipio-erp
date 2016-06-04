9ee2efaa1da0385f0442022d6525ce4a5b5b9fd3
Jonatan Soto <jonatan.soto@ilscipio.com>
2016-06-04 10:42:07 +0000

    Refs #1131: Fixed the interval scope filter. Commented out the sales channel dropdown (I'm not sure I can filter by this, it seems to be included in BI entities which are not available)
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@11036 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- applications/marketing/webapp/marketing/WEB-INF/actions/analytics/SalesChart.groovy
+++ applications/marketing/webapp/marketing/WEB-INF/actions/analytics/SalesChart.groovy
@@ -5,48 +5,6 @@ import org.ofbiz.base.util.Debug
 import org.ofbiz.base.util.UtilDateTime
 import org.ofbiz.base.util.UtilMisc
 
-//salesOrderItemFacts = null;
-//
-//if (parameters.salesChannel) {
-//    salesOrderItemFacts = delegator.findByAnd("SalesOrderItemFact", UtilMisc.toMap("saleChannel", parameters.salesChannel), null, false);
-//} else {
-//    salesOrderItemFacts = delegator.findList("SalesOrderItemFact", null, null, null, null, false);
-//}
-//
-//context.salesOrderItemFacts = salesOrderItemFacts;
-//
-//for (item in salesOrderItemFacts) {
-//    Debug.log("item ================> " + item);
-//}
-//contentCache = UtilCache.getOrCreateUtilCache("stats.order", 0, 0, 0, true, false);
-
-
-//def begin, end,dailyStats,weeklyStats,monthlyStats;
-//SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-//if(context.chartIntervalScope != null){
-//    String iscope = context.chartIntervalScope; //day|week|month|year
-//    int icount = context.chartIntervalCount != null ? Integer.parseInt(context.chartIntervalCount) : 0;
-//    icount = icount *(-1);
-//    if(iscope=="day"){
-//        begin = UtilDateTime.getDayStart(nowTimestamp, icount, timeZone, locale);
-//    }
-//    if(iscope=="week"){
-//        begin = UtilDateTime.getWeekStart(nowTimestamp, 0, icount, timeZone, locale);
-//    }
-//    if(iscope=="month"){
-//        begin = UtilDateTime.getMonthStart(nowTimestamp, 0, icount, timeZone, locale);
-//    }
-//    if(iscope=="year"){
-//        begin = UtilDateTime.getYearStart(nowTimestamp, 0, icount, timeZone, locale);
-//    }
-//}else{
-//    begin = UtilDateTime.getYearStart(nowTimestamp, timeZone, locale);
-//}
-
-//end = UtilDateTime.getYearEnd(nowTimestamp, timeZone, locale);
-//beginText = sdf.format(begin);
-//endText = sdf.format(end);
-//cacheId = "order_"+begin+"-"+end;
 
 Map processResult() {
     int iCount = context.chartIntervalCount != null ? Integer.parseInt(context.chartIntervalCount) : -1;
@@ -57,44 +15,35 @@ Map processResult() {
     thruDate = parameters.thruDate;
     Timestamp fromDateTimestamp = null;
     Timestamp thruDateTimestamp = null;
-    
     if (fromDate)
-        fromDateTimestamp = UtilDateTime.toTimestamp(fromDate);
+        fromDateTimestamp = UtilDateTime.stringToTimeStamp(fromDate, "yyyy-MM-dd HH:mm:ss", context.timeZone, context.locale);    
     if (thruDate)
-        thruDateTimestamp = UtilDateTime.toTimestamp(thruDate);
-    
-    if (fromDateTimestamp && fromDateTimestamp < thruDateTimeStamp) {
+        thruDateTimestamp = UtilDateTime.stringToTimeStamp(thruDate, "yyyy-MM-dd HH:mm:ss", context.timeZone, context.locale);           
+    if (fromDateTimestamp && fromDateTimestamp < thruDateTimestamp) {
         fromDate = null;
         thruDate = null;
-    }
-
+    }  
     if (!fromDateTimestamp) {
         iCount = UtilDateTime.getIntervalDefaultCount(iScope);        
-        fromDateTimestamp = UtilDateTime.getTimeStampFromIntervalScope(iScope, iCount);
-        if (iScope.equals("quarter")) iCount = Math.round(iCount / 3);
-        if (iScope.equals("semester")) iCount = Math.round(iCount / 6);
+        fromDateTimestamp = UtilDateTime.getTimeStampFromIntervalScope(iScope, iCount);        
     }
-    dateIntervals = UtilDateTime.getPeriodIntervalAndFormatter(iScope, fromDateTimestamp, context.locale, context.timeZone);
-    Debug.log("dateBegin ===========> " + dateIntervals["dateBegin"] + "  dateEnd =================> " + dateIntervals["dateEnd"]);
+    if (iScope.equals("quarter")) iCount = Math.round(iCount / 3);
+    if (iScope.equals("semester")) iCount = Math.round(iCount / 6);
     
-//    if (thruDateTimestamp && dateIntervals["dateEnd"] < thruDateTimestamp)
-//        dateIntervals["dateEnd"] = thruDate;
-//
-//    fromDateText = UtilDateTime.timeStampToString(fromDateTimestamp, "yyyy-MM-dd HH:mm:ss.SSS", context.timeZone, context.locale);
-//    thruDateText = null;
-//    if (thruDateTimestamp)
-//        thruDateText = UtilDateTime.timeStampToString(thruDateTimestamp, "yyyy-MM-dd HH:mm:ss.SSS", context.timeZone, context.locale);
+   
+    dateIntervals = UtilDateTime.getPeriodIntervalAndFormatter(iScope, fromDateTimestamp, context.locale, context.timeZone);
+    Debug.log("dateBegin ===========> " + dateIntervals["dateBegin"] + "  dateEnd =================> " + dateIntervals["dateEnd"]);    
+    if (thruDateTimestamp && dateIntervals["dateEnd"] < thruDateTimestamp)
+        dateIntervals["dateEnd"] = thruDateTimestamp;
+
     fromDateText = UtilDateTime.timeStampToString(dateIntervals["dateBegin"], "yyyy-MM-dd HH:mm:ss.SSS", context.timeZone, context.locale);
     thruDateText = UtilDateTime.timeStampToString(dateIntervals["dateEnd"], "yyyy-MM-dd HH:mm:ss.SSS", context.timeZone, context.locale);
         
     Map resultMap = new TreeMap<String, Object>();
     for (int i = 0; i <= iCount; i++) {
         Map findOrderMap = dispatcher.runSync("findOrdersFull", UtilMisc.toMap("minDate", fromDateText, "maxDate", thruDateText,"userLogin",userLogin));
-        
-        Debug.log("findOrderMap =====================> " + findOrderMap);
-        
+//        Debug.log("findOrderMap =====================> " + findOrderMap);
         List orderList = findOrderMap.orderList;
-
         orderList.each { header ->        
             String date = dateIntervals["dateFormatter"].format(header.orderDate);
             if (resultMap.get(date) != null) {
@@ -118,6 +67,8 @@ Map processResult() {
         }
         
         dateIntervals = UtilDateTime.getPeriodIntervalAndFormatter(iScope, 1, dateIntervals["dateBegin"], context.locale, context.timeZone);
+        if (thruDateTimestamp && dateIntervals["dateEnd"] < thruDateTimestamp)
+            dateIntervals["dateEnd"] = thruDateTimestamp
         Debug.log("dateBegin ===========> " + dateIntervals["dateBegin"] + "  dateEnd =================> " + dateIntervals["dateEnd"]);
         fromDateText = UtilDateTime.timeStampToString(dateIntervals["dateBegin"], "yyyy-MM-dd HH:mm:ss.SSS", context.timeZone, context.locale);
         thruDateText = UtilDateTime.timeStampToString(dateIntervals["dateEnd"], "yyyy-MM-dd HH:mm:ss.SSS", context.timeZone, context.locale);
@@ -125,18 +76,6 @@ Map processResult() {
     return resultMap;
 }
 
-
-//if (contentCache.get(cacheId) == null) {
-//    GenericValue userLogin = context.get("userLogin");
-//    Map cacheMap = [:];
-//    dailyStats = processResult();    
-//    cacheMap.dailyStats = processResult();
-
-//    contentCache.put(cacheId,cacheMap);
-//} else {
-//    cacheMap = contentCache.get(cacheId);
-//    dailyStats = cacheMap.dailyStats;
-//}
 sales = processResult();
 Debug.log("sales ===========> " + sales);
 context.sales = sales;
--- applications/marketing/webapp/sfa/analytics/FindAnalyticsSales.ftl
+++ applications/marketing/webapp/sfa/analytics/FindAnalyticsSales.ftl
@@ -1,7 +1,7 @@
 <@section>
     <form name="SaleOrdersByChannel" action="<@ofbizUrl>AnalyticsSales</@ofbizUrl>" method="POST">
-        <@field name="fromDate" type="datetime" value="" label=uiLabelMap.CommonFrom />
-        <@field name="thruDate" type="datetime" value="" label=uiLabelMap.CommonThru />
+        <@field name="fromDate" type="datetime" value="${parameters.fromDate!}" label=uiLabelMap.CommonFrom />
+        <@field name="thruDate" type="datetime" value="${parameters.thruDate!}" label=uiLabelMap.CommonThru />
         <@field type="select" name="intervalScope" label="${uiLabelMap.CommonIntervalScope}">
             <#assign intervals = Static["org.ofbiz.base.util.UtilDateTime"].TIME_INTERVALS />
             <option value=""></option>
@@ -9,11 +9,14 @@
                 <option value="${interval}" <#if parameters.intervalScope?has_content && parameters.intervalScope == interval>selected="selected"</#if>>${interval?capitalize}</option>
             </#list>
        </@field>
+       <#-- Scipio: Commenting out this for now, I'm not sure we can filter by this -->
+       <#--
         <@field type="select" name="salesChannel" label="${uiLabelMap.OrderSalesChannel}">
             <#list salesChannelList as salesChannel>
                 <option value="${salesChannel.enumId}" <#if parameters.salesChannel?has_content && parameters.salesChannel == salesChannel.enumId>selected="selected"</#if>>${salesChannel.description}</option>
             </#list>
        </@field>
+       -->
        <@field name="run" type="submit" value="" text=uiLabelMap.CommonSubmit />
     </form>
 </@section>

70483b58e47c548debfece0e698f3e0c4cb630bd
Jonatan Soto <jonatan.soto@ilscipio.com>
2016-06-04 15:04:38 +0000

    NoRef/OFBIZ patch: Showing a default success messages for services that either don't set a message in their logic or services that perform a direct DB operation.
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@11049 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/webapp/src/org/ofbiz/webapp/event/ServiceEventHandler.java
+++ framework/webapp/src/org/ofbiz/webapp/event/ServiceEventHandler.java
@@ -42,6 +42,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilGenerics;
 import org.ofbiz.base.util.UtilHttp;
+import org.ofbiz.base.util.UtilProperties;
 import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.GenericValue;
@@ -56,6 +57,8 @@ import org.ofbiz.service.ServiceValidationException;
 import org.ofbiz.webapp.control.ConfigXMLReader;
 import org.ofbiz.webapp.control.ConfigXMLReader.Event;
 import org.ofbiz.webapp.control.ConfigXMLReader.RequestMap;
+
+import com.sun.xml.internal.ws.resources.UtilMessages;
 
 /**
  * ServiceEventHandler - Service Event Handler
@@ -376,6 +379,14 @@ public class ServiceEventHandler implements EventHandler {
 
             request.setAttribute("_EVENT_MESSAGE_LIST_", result.get(ModelService.SUCCESS_MESSAGE_LIST));
             request.setAttribute("_EVENT_MESSAGE_", result.get(ModelService.SUCCESS_MESSAGE));
+            
+            // Scipio: Some services don't set any result messages, either because they aren't explicitly set in the service logic (minilang, groovy, java...) 
+            // or because the service is just a direct DB operation
+            if (responseString.equals(ModelService.RESPOND_SUCCESS) && UtilValidate.isEmpty(request.getAttribute("_EVENT_MESSAGE_LIST_"))
+                    && UtilValidate.isEmpty(request.getAttribute("_EVENT_MESSAGE_"))) {
+                request.setAttribute("_EVENT_MESSAGE_", UtilProperties.getMessage("CommonUiLabels", "CommonServiceSuccessMessage", locale));
+
+            }
 
             // set the results in the request
             for (Map.Entry<String, Object> rme: result.entrySet()) {

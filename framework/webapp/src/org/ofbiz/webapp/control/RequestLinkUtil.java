4fe0bd0c2cfec5000a12cab73c4829d069e7d5dd
Pascal Proulx <pplx@ilscipio.com>
2016-07-14 18:21:49 +0000

    NoRef/OFBIZ patch: ofbiz URL: checkFullSecureOrStandard now static and can handle static cases where request not present (treats current as insecure request, so should produce secure links by default when in static context)
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@11636 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/webapp/src/org/ofbiz/webapp/control/RequestLinkUtil.java
+++ framework/webapp/src/org/ofbiz/webapp/control/RequestLinkUtil.java
@@ -152,15 +152,19 @@ public abstract class RequestLinkUtil {
             webSiteProps = WebSiteProperties.from(request);
         } catch (GenericEntityException e) {
             Debug.logError(e, module);
-            return false;
+            return true;
         }
-        RequestHandler rh = RequestHandler.getRequestHandler(request.getServletContext());
-        return rh.checkFullSecureOrStandard(request, webSiteProps, null, interWebapp, fullPath, secure);
+        //RequestHandler rh = RequestHandler.getRequestHandler(request.getServletContext());
+        return RequestHandler.checkFullSecureOrStandard(request, webSiteProps, null, interWebapp, fullPath, secure);
     }
     
     public static Boolean checkFullSecureOrStandard(Delegator delegator, WebSiteProperties webSiteProps,
-            Boolean interWebapp, Boolean fullPath, Boolean secure) {
-        // FIXME: This case is too simplistic currently, check the website
+            Boolean interWebapp, Boolean fullPath, Boolean secure, HttpServletRequest request, HttpServletResponse response) {
+        // what we can do here depends on whether we got a request/response or not
+        // checkFullSecureOrStandard should handle the case where request was missing (treats as insecure current request)
+        return RequestHandler.checkFullSecureOrStandard(request, webSiteProps, null, interWebapp, fullPath, secure);
+        
+        /* old logic, too simplistic (wrong and insecure compared to new RequestHandler logic)
         if (Boolean.TRUE.equals(secure)) {
             return Boolean.TRUE;
         } else if (Boolean.TRUE.equals(fullPath)) {
@@ -168,18 +172,21 @@ public abstract class RequestLinkUtil {
         } else {
             return null;
         }
+        */
     }
     
     public static Boolean checkFullSecureOrStandard(Delegator delegator, String webSiteId,
-            Boolean interWebapp, Boolean fullPath, Boolean secure) {
-        // FIXME: This case is too simplistic currently, check the website
-        if (Boolean.TRUE.equals(secure)) {
-            return Boolean.TRUE;
-        } else if (Boolean.TRUE.equals(fullPath)) {
-            return Boolean.FALSE;
-        } else {
-            return null;
+            Boolean interWebapp, Boolean fullPath, Boolean secure, HttpServletRequest request, HttpServletResponse response) {
+        WebSiteProperties webSiteProps = null;
+        if (webSiteId != null) {
+            try {
+                webSiteProps = WebSiteProperties.from(delegator, webSiteId);
+            } catch (GenericEntityException e) {
+                Debug.logError(e, module);
+                return true;
+            }
         }
+        return checkFullSecureOrStandard(delegator, webSiteProps, interWebapp, fullPath, secure, request, response);
     }
     
     public static String buildLinkHostPartAndEncode(Delegator delegator, String webSiteId, String url,
@@ -191,7 +198,7 @@ public abstract class RequestLinkUtil {
         StringBuilder newURL = new StringBuilder();
         
         // NOTE: this is always treated as inter-webapp, because we don't know our webapp
-        Boolean secureFullPathFlag = checkFullSecureOrStandard(delegator, webSiteId, true, fullPath, secure);
+        Boolean secureFullPathFlag = checkFullSecureOrStandard(delegator, webSiteId, true, fullPath, secure, request, response);
         if (secureFullPathFlag != null) {
             if (secureFullPathFlag) {
                 didFullSecure = true;

e126fadee78a028a7ec9f1cf590dccfcf4163a3b
Pascal Proulx <pplx@ilscipio.com>
2016-08-30 03:11:09 +0000

    NoRef/OFBIZ patch: screen widgets: include-menu: implemented new max-depth attribute (sub-menu rendering depth limit) on include-menu elem. created new MenuRenderState context variable for tracking.
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@12227 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/widget/src/org/ofbiz/widget/renderer/macro/MacroMenuRenderer.java
+++ framework/widget/src/org/ofbiz/widget/renderer/macro/MacroMenuRenderer.java
@@ -43,6 +43,7 @@ import org.ofbiz.webapp.control.RequestHandler;
 import org.ofbiz.webapp.taglib.ContentUrlTag;
 import org.ofbiz.widget.WidgetWorker;
 import org.ofbiz.widget.model.CommonWidgetModels.Image;
+import org.ofbiz.widget.model.MenuRenderState;
 import org.ofbiz.widget.model.ModelMenu;
 import org.ofbiz.widget.model.ModelMenuItem;
 import org.ofbiz.widget.model.ModelMenuItem.MenuLink;
@@ -448,7 +449,19 @@ public class MacroMenuRenderer implements MenuStringRenderer {
         // SCIPIO: we have a better check now
         //boolean containsNestedMenus = !menuItem.getMenuItemList().isEmpty();
         boolean containsNestedMenus = menuItem.hasSubMenu();
+        
+        // SCIPIO: 2016-08-29: max depth check
+        MenuRenderState renderState = MenuRenderState.retrieve(context);
+        if (renderState == null) {
+            Debug.logWarning("No MenuRenderState present in context; no depth checks possible", module);
+        } else {
+            if (renderState.hasReachedMaxDepth()) {
+                containsNestedMenus = false;
+            }
+        }
+        
         parameters.put("containsNestedMenus", containsNestedMenus);
+        
         
         // Scipio: menu context role
         String menuCtxRole = (String) context.get("menuStringRender_menuCtxRole");
@@ -487,8 +500,17 @@ public class MacroMenuRenderer implements MenuStringRenderer {
             //for (ModelMenuItem childMenuItem : menuItem.getMenuItemList()) {
             //    childMenuItem.renderMenuItemString(writer, context, this);
             //}
-            for(ModelSubMenu childSubMenu : menuItem.getSubMenuList()) {
-                childSubMenu.renderSubMenuString(writer, context, this);
+            if (renderState != null) {
+                renderState.increaseCurrentDepth();
+            }
+            try {
+                for(ModelSubMenu childSubMenu : menuItem.getSubMenuList()) {
+                    childSubMenu.renderSubMenuString(writer, context, this);
+                }
+            } finally {
+                if (renderState != null) {
+                    renderState.decreaseCurrentDepth();
+                }
             }
         }
  

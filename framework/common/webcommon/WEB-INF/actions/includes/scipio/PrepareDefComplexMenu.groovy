332301602905216dfaac80e72803871e6fab60af
Pascal Proulx <pplx@ilscipio.com>
2016-09-18 03:43:04 +0000

    NoRef Merge from scipioce-latest: r12279: Refs #1234 all: menu widgets: formalize advanced menu wrapper pattern: MainSideBarMenu, DefMainSideBarMenu -> (Common) ComplexSideBarMenu/ComplexMenu. misc fixes.
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/branches/scipioce-1.14@12640 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

---
+++ framework/common/webcommon/WEB-INF/actions/includes/scipio/PrepareDefComplexMenu.groovy
@@ -0,0 +1,15 @@
+/**
+ * SCIPIO: Prepares default generic menu.
+ * This is a prelude to a call to PrepareAdvMenu.groovy;
+ * causes it to ignore any activeSubMenu set by the screen.
+ */
+
+//import org.ofbiz.base.util.*;
+
+//final module = "PrepareDefComplexMenu.groovy";
+
+context.activeSubMenu = null;
+// NOTE: it's possible to not want to set this to null...
+// for now do it. see PrepareComplexMenu.groovy for details.
+context.activeSubMenuItem = null; 
+

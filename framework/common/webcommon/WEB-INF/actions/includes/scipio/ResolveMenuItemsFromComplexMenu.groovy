829670d837c4060886a927ddd696a0c8a15c412a
Pascal Proulx <pplx@ilscipio.com>
2016-11-11 21:50:18 +0000

    Refs #1234 ResolveMenuItemsFromComplexMenu: groovy script to (hopefully) automatically determine activeMainMenuItem from activeSubMenu/activeSubMenuItem
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@13474 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

---
+++ framework/common/webcommon/WEB-INF/actions/includes/scipio/ResolveMenuItemsFromComplexMenu.groovy
@@ -0,0 +1,53 @@
+/**
+ * SCIPIO: Uses the activeSubMenu/activeSubMenuItem in context to automatically
+ * determine other menu items (if not explicitly set):
+ * * activeMainMenuItem
+ * 
+ * 2016-11-11: Added for 1.14.3.
+ */
+
+import org.ofbiz.base.util.Debug;
+
+final module "ResolveMenuItemsFromComplexMenuGroovy";
+
+activeMainMenuItem = context.activeMainMenuItem;
+
+if (!activeMainMenuItem) {
+    cmplMenuLoc = context.mainSideBarMenu?.location;
+    cmplMenuName = context.mainSideBarMenu?.name;
+    
+    cplxMenuModel = null;
+    if (cmplMenuLoc && cmplMenuName) {
+        cplxMenuModel = null;
+        try {
+            cplxMenuModel = org.ofbiz.widget.model.MenuFactory.getMenuFromLocation(
+                cplxLoc, cplxName);
+            if (cplxMenuModel == null) {
+                throw new IllegalArgumentException("Could not find menu with name [" + cplxName
+                    + "] in location [" + cplxLoc + "]");
+            }
+        } catch (Exception e) {
+            Debug.logError(e, "Error loading complex menu model; unable to determine activeMainMenuItem", module);
+        }
+    } else {
+        Debug.logWarning("Could not find mainSideBarMenu.location/name context variables; " +
+            "unable to determine activeMainMenuItem", module);
+    }
+
+    if (cplxMenuModel != null) {
+        activeSubMenu = context.activeSubMenu;
+        activeSubMenuItem = context.activeSubMenuItem;
+        
+        subItem = cplxMenuModel.getModelMenuItemBySubName(activeSubMenuItem, activeSubMenu);
+        if (subItem != null) {
+            topItem = subItem.getTopParentMenuItem();
+            activeMainMenuItem = topItem.getName();
+            context.activeMainMenuItem = activeMainMenuItem;
+            Debug.logInfo("Automatically determined activeMainMenuItem: " + activeMainMenuItem, module);
+        } else {
+            Debug.logWarning("Could not find sub menu item activeSubMenu/activeSubMenuItem [" +
+                activeSubMenu + "/" + activeSubMenuItem + "] in menu [" + cmplMenuLoc + "#" +
+                cmplMenuName + "]; unable to determine activeMainMenuItem", module);
+        }
+    }
+}

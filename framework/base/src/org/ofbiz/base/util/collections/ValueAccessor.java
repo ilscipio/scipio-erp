cddd415e60872c778dbabb4d54e0b41ccb374b41
Pascal Proulx <pplx@ilscipio.com>
2016-11-16 17:04:05 +0000

    NoRef ValueAccessor: generalized this helper class
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@13591 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/base/src/org/ofbiz/base/util/collections/ValueAccessor.java
+++ framework/base/src/org/ofbiz/base/util/collections/ValueAccessor.java
@@ -7,19 +7,22 @@ import org.ofbiz.base.util.string.FlexibleStringExpander;
 import org.w3c.dom.Element;
 
 /**
- * SCIPIO: This is a simple switch between FlexibleMapAccessor and
- * FLexibleStringExpander.
+ * SCIPIO: Simple generic value accessor and utils, mainly used in xml models.
  */
 @SuppressWarnings("serial")
-public abstract class FlexibleSwitchedAccessor implements Serializable {
+public abstract class ValueAccessor implements Serializable {
     
-    public static final String module = FlexibleSwitchedAccessor.class.getName();
+    public static final String module = ValueAccessor.class.getName();
 
     public static final NullAccessor NULL_ACCESSOR = new NullAccessor();
     public static final ConstantAccessor BOOLEAN_TRUE_ACCESSOR = new ConstantAccessor(Boolean.TRUE);
     public static final ConstantAccessor BOOLEAN_FALSE_ACCESSOR = new ConstantAccessor(Boolean.FALSE);
 
-    public static FlexibleSwitchedAccessor getInstance(Element element, String fieldAttrName, String valueAttrName) {
+    public abstract Object getValue(Map<String, Object> context);
+    
+    public abstract String getLogRepr();
+    
+    public static ValueAccessor getFieldOrExpanderAccessor(Element element, String fieldAttrName, String valueAttrName) {
         FlexibleMapAccessor<Object> fieldAccessor = null;
         FlexibleStringExpander valueExpander = null;
         
@@ -31,10 +34,13 @@ public abstract class FlexibleSwitchedAccessor implements Serializable {
         if (!valueVal.isEmpty()) {
             valueExpander = FlexibleStringExpander.getInstance(valueVal);
         }
-        return getInstance(fieldAccessor, valueExpander);
+        return getFieldOrExpanderAccessor(fieldAccessor, valueExpander);
     }
     
-    public static FlexibleSwitchedAccessor getInstance(FlexibleMapAccessor<Object> fieldAccessor, 
+    /**
+     * Simple switch between FlexibleMapAccessor and FlexibleStringExpander.
+     */
+    public static ValueAccessor getFieldOrExpanderAccessor(FlexibleMapAccessor<Object> fieldAccessor, 
             FlexibleStringExpander valueExpander) {
         if (fieldAccessor != null && valueExpander != null) {
             throw new IllegalArgumentException("both field and value accessors cannot be specified at once");
@@ -47,17 +53,8 @@ public abstract class FlexibleSwitchedAccessor implements Serializable {
             throw new IllegalArgumentException("no field or value accessor was specified");
         }
     }
-    
-    public abstract Object getValue(Map<String, Object> context);
-    
-    public abstract FlexibleMapAccessor<Object> getFieldAscr();
-    
-    public abstract FlexibleStringExpander getValueExdr();
-    
-    public abstract String getLogDesc();
-    
-    public static class FieldAccessor extends FlexibleSwitchedAccessor {
-        
+
+    public static class FieldAccessor extends ValueAccessor {
         protected final FlexibleMapAccessor<Object> fieldAcsr;
 
         public FieldAccessor(FlexibleMapAccessor<Object> fieldAcsr) {
@@ -69,22 +66,12 @@ public abstract class FlexibleSwitchedAccessor implements Serializable {
         }
 
         @Override
-        public FlexibleMapAccessor<Object> getFieldAscr() {
-            return fieldAcsr;
-        }
-
-        @Override
-        public FlexibleStringExpander getValueExdr() {
-            return null;
-        }
-
-        @Override
-        public String getLogDesc() {
+        public String getLogRepr() {
             return "context field '" + fieldAcsr.getOriginalName() + "'";
         }
     }
     
-    public static class ExpanderAccessor extends FlexibleSwitchedAccessor {
+    public static class ExpanderAccessor extends ValueAccessor {
         protected final FlexibleStringExpander valueExdr;
         
         public ExpanderAccessor(FlexibleStringExpander valueExdr) {
@@ -92,54 +79,18 @@ public abstract class FlexibleSwitchedAccessor implements Serializable {
         }
 
         public Object getValue(Map<String, Object> context) {
-            return valueExdr.expandString(context);
+            return valueExdr.expand(context);
         }
 
         @Override
-        public FlexibleMapAccessor<Object> getFieldAscr() {
-            return null;
-        }
-
-        @Override
-        public FlexibleStringExpander getValueExdr() {
-            return valueExdr;
-        }
-
-        @Override
-        public String getLogDesc() {
+        public String getLogRepr() {
             return "value expression '" + valueExdr.getOriginal() + "'";
         }
     }
     
-    public static final class NullAccessor extends FlexibleSwitchedAccessor {
-
-        private NullAccessor() {
-        }
-        
-        @Override
-        public Object getValue(Map<String, Object> context) {
-            return null;
-        }
-
-        @Override
-        public FlexibleMapAccessor<Object> getFieldAscr() {
-            return null;
-        }
-
-        @Override
-        public FlexibleStringExpander getValueExdr() {
-            return null;
-        }
-
-        @Override
-        public String getLogDesc() {
-            return "null accessor";
-        }
-    }
-    
-    public static final class ConstantAccessor extends FlexibleSwitchedAccessor {
-        private final Object value;
-        private ConstantAccessor(Object value) {
+    public static final class ConstantAccessor extends ValueAccessor {
+        protected final Object value;
+        public ConstantAccessor(Object value) {
             this.value = value;
         }
         
@@ -149,19 +100,22 @@ public abstract class FlexibleSwitchedAccessor implements Serializable {
         }
 
         @Override
-        public FlexibleMapAccessor<Object> getFieldAscr() {
-            return null;
-        }
-
-        @Override
-        public FlexibleStringExpander getValueExdr() {
-            return null;
-        }
-
-        @Override
-        public String getLogDesc() {
+        public String getLogRepr() {
             return "constant value '" + value + "' accessor";
         }
     }
     
+    public static final class NullAccessor extends ValueAccessor {
+        private NullAccessor() {} // singleton
+        
+        @Override
+        public Object getValue(Map<String, Object> context) {
+            return null;
+        }
+
+        @Override
+        public String getLogRepr() {
+            return "null accessor";
+        }
+    }
 }

8140cc4e22d22fa154e02c672d459b85724febf7
Pascal Proulx <pplx@ilscipio.com>
2016-11-16 01:50:06 +0000

    NoRef/OFBIZ patch: UtilXml: NEW ElementHelper class
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@13572 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/base/src/org/ofbiz/base/util/UtilXml.java
+++ framework/base/src/org/ofbiz/base/util/UtilXml.java
@@ -55,11 +55,18 @@ import org.apache.xerces.xni.XMLLocator;
 import org.apache.xerces.xni.XMLResourceIdentifier;
 import org.apache.xerces.xni.XMLString;
 import org.apache.xerces.xni.XNIException;
+import org.ofbiz.base.util.string.FlexibleStringExpander;
+import org.w3c.dom.Attr;
 import org.w3c.dom.DOMConfiguration;
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.DocumentFragment;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.TypeInfo;
+import org.w3c.dom.UserDataHandler;
 import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 import org.w3c.dom.ls.DOMImplementationLS;
 import org.w3c.dom.ls.LSOutput;
@@ -1148,4 +1155,292 @@ public class UtilXml {
         }
     }
 
+    /**
+     * SCIPIO: Returns the element wrapped in a helper that even implements the Element
+     * interface for convenience.
+     */
+    public static ElementHelper getHelper(Element element) {
+        return new ElementHelper(element);
+    }
+    
+    /**
+     * SCIPIO: Wraps an element to provide helper methods, in addition to supporting all
+     * the regular Element methods.
+     */
+    public static class ElementHelper implements Element {
+        protected final Element element;
+
+        public ElementHelper(Element element) {
+            this.element = element;
+        }
+        
+        public Element getWrappedElement() {
+            return element;
+        }
+        
+        public String attr(String attrName) {
+            return element.getAttribute(attrName);
+        }
+        
+        public Boolean attrAsBoolean(String attrName) {
+            return UtilMisc.booleanValue(element.getAttribute(attrName));
+        }
+        
+        public boolean attrAsBoolean(String attrName, boolean defaultVal) {
+            return UtilMisc.booleanValue(element.getAttribute(attrName), defaultVal);
+        }
+        
+        public FlexibleStringExpander attrAsExdr(String attrName) {
+            return FlexibleStringExpander.getInstance(element.getAttribute(attrName));
+        }
+        
+        public Element firstChildElement(String tagName) {
+            return UtilXml.firstChildElement(element, tagName);
+        }
+        
+        public Element firstChildElement() {
+            return UtilXml.firstChildElement(element);
+        }
+        
+        public List<? extends Element> childElementList(String tagName) {
+            return UtilXml.childElementList(element, tagName);
+        }
+        
+        public List<? extends Element> childElementList() {
+            return UtilXml.childElementList(element);
+        }
+
+        // ELEMENT INTERFACE METHODS
+
+        public String getTagName() {
+            return element.getTagName();
+        }
+
+        public String getAttribute(String name) {
+            return element.getAttribute(name);
+        }
+
+        public void setAttribute(String name, String value) throws DOMException {
+            element.setAttribute(name, value);
+        }
+
+        public void removeAttribute(String name) throws DOMException {
+            element.removeAttribute(name);
+        }
+
+        public Attr getAttributeNode(String name) {
+            return element.getAttributeNode(name);
+        }
+
+        public Attr setAttributeNode(Attr newAttr) throws DOMException {
+            return element.setAttributeNode(newAttr);
+        }
+
+        public Attr removeAttributeNode(Attr oldAttr) throws DOMException {
+            return element.removeAttributeNode(oldAttr);
+        }
+
+        public String getNodeName() {
+            return element.getNodeName();
+        }
+
+        public String getNodeValue() throws DOMException {
+            return element.getNodeValue();
+        }
+
+        public NodeList getElementsByTagName(String name) {
+            return element.getElementsByTagName(name);
+        }
+
+        public void setNodeValue(String nodeValue) throws DOMException {
+            element.setNodeValue(nodeValue);
+        }
+
+        public String getAttributeNS(String namespaceURI, String localName) throws DOMException {
+            return element.getAttributeNS(namespaceURI, localName);
+        }
+
+        public short getNodeType() {
+            return element.getNodeType();
+        }
+
+        public Node getParentNode() {
+            return element.getParentNode();
+        }
+
+        public NodeList getChildNodes() {
+            return element.getChildNodes();
+        }
+
+        public void setAttributeNS(String namespaceURI, String qualifiedName, String value) throws DOMException {
+            element.setAttributeNS(namespaceURI, qualifiedName, value);
+        }
+
+        public Node getFirstChild() {
+            return element.getFirstChild();
+        }
+
+        public Node getLastChild() {
+            return element.getLastChild();
+        }
+
+        public Node getPreviousSibling() {
+            return element.getPreviousSibling();
+        }
+
+        public Node getNextSibling() {
+            return element.getNextSibling();
+        }
+
+        public NamedNodeMap getAttributes() {
+            return element.getAttributes();
+        }
+
+        public Document getOwnerDocument() {
+            return element.getOwnerDocument();
+        }
+
+        public Node insertBefore(Node newChild, Node refChild) throws DOMException {
+            return element.insertBefore(newChild, refChild);
+        }
+
+        public void removeAttributeNS(String namespaceURI, String localName) throws DOMException {
+            element.removeAttributeNS(namespaceURI, localName);
+        }
+
+        public Node replaceChild(Node newChild, Node oldChild) throws DOMException {
+            return element.replaceChild(newChild, oldChild);
+        }
+
+        public Attr getAttributeNodeNS(String namespaceURI, String localName) throws DOMException {
+            return element.getAttributeNodeNS(namespaceURI, localName);
+        }
+
+        public Node removeChild(Node oldChild) throws DOMException {
+            return element.removeChild(oldChild);
+        }
+
+        public Attr setAttributeNodeNS(Attr newAttr) throws DOMException {
+            return element.setAttributeNodeNS(newAttr);
+        }
+
+        public Node appendChild(Node newChild) throws DOMException {
+            return element.appendChild(newChild);
+        }
+
+        public NodeList getElementsByTagNameNS(String namespaceURI, String localName) throws DOMException {
+            return element.getElementsByTagNameNS(namespaceURI, localName);
+        }
+
+        public boolean hasChildNodes() {
+            return element.hasChildNodes();
+        }
+
+        public Node cloneNode(boolean deep) {
+            return element.cloneNode(deep);
+        }
+
+        public boolean hasAttribute(String name) {
+            return element.hasAttribute(name);
+        }
+
+        public boolean hasAttributeNS(String namespaceURI, String localName) throws DOMException {
+            return element.hasAttributeNS(namespaceURI, localName);
+        }
+
+        public void normalize() {
+            element.normalize();
+        }
+
+        public TypeInfo getSchemaTypeInfo() {
+            return element.getSchemaTypeInfo();
+        }
+
+        public void setIdAttribute(String name, boolean isId) throws DOMException {
+            element.setIdAttribute(name, isId);
+        }
+
+        public boolean isSupported(String feature, String version) {
+            return element.isSupported(feature, version);
+        }
+
+        public void setIdAttributeNS(String namespaceURI, String localName, boolean isId) throws DOMException {
+            element.setIdAttributeNS(namespaceURI, localName, isId);
+        }
+
+        public String getNamespaceURI() {
+            return element.getNamespaceURI();
+        }
+
+        public String getPrefix() {
+            return element.getPrefix();
+        }
+
+        public void setIdAttributeNode(Attr idAttr, boolean isId) throws DOMException {
+            element.setIdAttributeNode(idAttr, isId);
+        }
+
+        public void setPrefix(String prefix) throws DOMException {
+            element.setPrefix(prefix);
+        }
+
+        public String getLocalName() {
+            return element.getLocalName();
+        }
+
+        public boolean hasAttributes() {
+            return element.hasAttributes();
+        }
+
+        public String getBaseURI() {
+            return element.getBaseURI();
+        }
+
+        public short compareDocumentPosition(Node other) throws DOMException {
+            return element.compareDocumentPosition(other);
+        }
+
+        public String getTextContent() throws DOMException {
+            return element.getTextContent();
+        }
+
+        public void setTextContent(String textContent) throws DOMException {
+            element.setTextContent(textContent);
+        }
+
+        public boolean isSameNode(Node other) {
+            return element.isSameNode(other);
+        }
+
+        public String lookupPrefix(String namespaceURI) {
+            return element.lookupPrefix(namespaceURI);
+        }
+
+        public boolean isDefaultNamespace(String namespaceURI) {
+            return element.isDefaultNamespace(namespaceURI);
+        }
+
+        public String lookupNamespaceURI(String prefix) {
+            return element.lookupNamespaceURI(prefix);
+        }
+
+        public boolean isEqualNode(Node arg) {
+            return element.isEqualNode(arg);
+        }
+
+        public Object getFeature(String feature, String version) {
+            return element.getFeature(feature, version);
+        }
+
+        public Object setUserData(String key, Object data, UserDataHandler handler) {
+            return element.setUserData(key, data, handler);
+        }
+
+        public Object getUserData(String key) {
+            return element.getUserData(key);
+        }
+        
+        
+    }
+    
 }

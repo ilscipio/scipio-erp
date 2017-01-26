25bc05c356c418b254779d6fc86ec57343732500
Pascal Proulx <pplx@ilscipio.com>
2017-01-26 17:18:44 +0000

    Refs #1384/OFBIZ patch: GroovyUtil: added new exposed getScriptFromLocation methods (document)
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@14477 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/base/src/org/ofbiz/base/util/GroovyUtil.java
+++ framework/base/src/org/ofbiz/base/util/GroovyUtil.java
@@ -203,25 +203,39 @@ public class GroovyUtil {
         runScriptAtLocation(location, methodName, context);
         return context;
     }
-
-    /**
-     * SCIPIO: Returns a {@link groovy.lang.Script} instance for the script groovy class at the given location,
-     * initialized with binding built from the given context.
-     * <p>
-     * Added 2017-01-26.
-     */
-    public static Script getScriptFromLocation(String location, Map<String, Object> context) throws GeneralException {
-        return InvokerHelper.createScript(getScriptClassFromLocation(location), getBinding(context));
-    }
     
     /**
      * SCIPIO: Returns a {@link groovy.lang.Script} instance for the script groovy class at the given location,
      * initialized with the given {@link groovy.lang.Binding}.
      * <p>
+     * Similar to calling (in Groovy code):
+     * <pre>
+     * {@code
+     * def inst = GroovyUtil.getScriptClassFromLocation(location).newInstance();
+     * inst.setBinding(binding);
+     * }
+     * </pre>
+     * <p>
+     * NOTE: Although this can handle groovy classes that do not extend <code>groovy.lang.Script</code>,
+     * in such cases you may want to call {@link #getScriptClassFromLocation}
+     * together with <code>.newInstance()</code> instead of this.
+     * <p>
      * Added 2017-01-26.
+     * @see #getScriptClassFromLocation
      */
     public static Script getScriptFromLocation(String location, Binding binding) throws GeneralException {
         return InvokerHelper.createScript(getScriptClassFromLocation(location), binding);
+    }
+    
+    /**
+     * SCIPIO: Returns a {@link groovy.lang.Script} instance for the script groovy class at the given location,
+     * initialized with binding built from the given context.
+     * <p>
+     * Added 2017-01-26.
+     * @see #getScriptFromLocation(String, Binding)
+     */
+    public static Script getScriptFromLocation(String location, Map<String, Object> context) throws GeneralException {
+        return InvokerHelper.createScript(getScriptClassFromLocation(location), getBinding(context));
     }
     
     private GroovyUtil() {}

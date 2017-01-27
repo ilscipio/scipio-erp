899543a6e32c243fc48e2ccd05ca5eb6145b7c78
Pascal Proulx <pplx@ilscipio.com>
2017-01-27 21:10:47 +0000

    Refs #1366/OFBIZ patch: ScriptUtil: GROOVY ENHANCEMENT: inline Groovy script in flexible expressions (${groovy:...}) now support the Ofbiz methods defined on GroovyBaseScript (such as from() entity lookups and other helpers) - ScriptUtil.parseScript now uses the custom ofbiz GroovyClassLoader (instead of default one)
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@14508 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/base/src/org/ofbiz/base/util/ScriptUtil.java
+++ framework/base/src/org/ofbiz/base/util/ScriptUtil.java
@@ -415,7 +415,10 @@ public final class ScriptUtil {
     public static Class<?> parseScript(String language, String script) {
         Class<?> scriptClass = null;
         if ("groovy".equals(language)) {
-            scriptClass = GroovyUtil.parseClass(script);
+            // SCIPIO: 2017-01-27: this now parses using the custom GroovyClassLoader,
+            // so that groovy snippets from FlexibleStringExpander support extra additions
+            //scriptClass = GroovyUtil.parseClass(script);
+            scriptClass = GroovyUtil.parseClass(script, GroovyUtil.getGroovyScriptClassLoader());
         }
         return scriptClass;
     }

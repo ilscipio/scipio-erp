f1331083bcbf0ef1661cd0bc33f31fa79eb98b32
Pascal Proulx <pplx@ilscipio.com>
2017-03-29 00:02:37 +0000

    NoRef/OFBIZ patch: FtlContextFetcher, ScreenRenderer: refactored FtlContextFetcher because it was extremely confusing and alternative implementations now needed
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@15284 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/webapp/src/org/ofbiz/webapp/renderer/FtlContextFetcher.java
+++ framework/webapp/src/org/ofbiz/webapp/renderer/FtlContextFetcher.java
@@ -8,49 +8,62 @@ import org.ofbiz.base.util.template.FreeMarkerWorker;
 import freemarker.core.Environment;
 import freemarker.ext.util.WrapperTemplateModel;
 import freemarker.template.TemplateModel;
-import freemarker.template.TemplateModelException;
 
 /**
- * SCIPIO: Advanced context fetcher that fetches the Writer out of the Freemarker
- * environment if any is currently active; otherwise falls back on initial.
+ * SCIPIO: Context fetchers that can get writers and contexts out of the Freemarker Environment.
  */
-public class FtlContextFetcher extends SimpleContextFetcher {
-    public FtlContextFetcher(Appendable writer, MapStack<String> context) {
+public abstract class FtlContextFetcher extends SimpleContextFetcher {
+    protected FtlContextFetcher(Appendable writer, MapStack<String> context) {
         super(writer, context);
     }
 
-    @Override
-    public Appendable getWriter() {
-        Environment env = FreeMarkerWorker.getCurrentEnvironment();
-        if (env != null) {
-            Writer out = env.getOut();
-            if (out != null) {
-                return out;
-            }
+    /**
+     * SCIPIO: Advanced context fetcher that fetches the Writer out of the Freemarker
+     * environment if any is currently active; otherwise falls back on initial.
+     */
+    public static class FtlWriterOnlyFetcher extends FtlContextFetcher {
+        public FtlWriterOnlyFetcher(Appendable writer, MapStack<String> context) {
+            super(writer, context);
         }
-        return writer;
+        
+        @Override
+        public Appendable getWriter() {
+            Appendable writer = getWriterFromEnvironment(FreeMarkerWorker.getCurrentEnvironment());
+            if (writer != null) return writer;
+            return this.writer;
+        }
     }
-    
+
+    /**
+     * SCIPIO: Advanced context fetcher that fetches the Writer out of the Freemarker
+     * environment if any is currently active; otherwise falls back on initial.
+     * It does the same for the MapStack context.
+     */
+    public static class FtlFullFetcher extends FtlWriterOnlyFetcher {
+        public FtlFullFetcher(Appendable writer, MapStack<String> context) {
+            super(writer, context);
+        }
+        
+        @Override
+        public MapStack<String> getContext() {
+            MapStack<String> context = getContextFromEnvironment(FreeMarkerWorker.getCurrentEnvironment());
+            if (context != null) return context;
+            return this.context;
+        }
+    }
+
     /**
      * FTL context fetcher that only gets writer from Environment if it was null.
      */
-    public static class FallbackFtlContextFetcher extends SimpleContextFetcher {
-
-        public FallbackFtlContextFetcher(Appendable writer, MapStack<String> context) {
+    public static class FallbackFtlWriterOnlyFetcher extends FtlContextFetcher {
+        public FallbackFtlWriterOnlyFetcher(Appendable writer, MapStack<String> context) {
             super(writer, context);
         }
         
         @Override
         public Appendable getWriter() {
             if (writer != null) return writer;
-            Environment env = FreeMarkerWorker.getCurrentEnvironment();
-            if (env != null) {
-                Writer out = env.getOut();
-                if (out != null) {
-                    return out;
-                }
-            }
-            return null;
+            return getWriterFromEnvironment(FreeMarkerWorker.getCurrentEnvironment());
         }
     }
     
@@ -58,32 +71,43 @@ public class FtlContextFetcher extends SimpleContextFetcher {
      * FTL context fetcher that only gets writer from Environment if it was null,
      * and also fetches the context from Environment if it was null.
      */
-    public static class FullFallbackFtlContextFetcher extends FallbackFtlContextFetcher {
-
-        public FullFallbackFtlContextFetcher(Appendable writer, MapStack<String> context) {
+    public static class FallbackFtlFullFetcher extends FallbackFtlWriterOnlyFetcher {
+        public FallbackFtlFullFetcher(Appendable writer, MapStack<String> context) {
             super(writer, context);
         }
 
-        @SuppressWarnings("unchecked")
         @Override
         public MapStack<String> getContext() {
             if (context != null) return context;
-            Environment env = FreeMarkerWorker.getCurrentEnvironment();
-            if (env != null) {
-                try {
-                    TemplateModel model = env.getGlobalVariable("context");
-                    if (model != null && model instanceof WrapperTemplateModel) {
-                        Object obj = ((WrapperTemplateModel) model).getWrappedObject();
-                        if (obj instanceof MapStack) {
-                            return (MapStack<String>) obj;
-                        }
-                    }
-                } catch (TemplateModelException e) {
-                    ;
-                }
-            }
-            return null;
+            return getContextFromEnvironment(FreeMarkerWorker.getCurrentEnvironment());
         }
     }
 
+    public static Appendable getWriterFromEnvironment(Environment env) {
+        if (env != null) {
+            Writer out = env.getOut();
+            if (out != null) {
+                return out;
+            }
+        }
+        return null;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static MapStack<String> getContextFromEnvironment(Environment env) {
+        if (env != null) {
+            try {
+                TemplateModel model = env.getGlobalVariable("context");
+                if (model != null && model instanceof WrapperTemplateModel) {
+                    Object obj = ((WrapperTemplateModel) model).getWrappedObject();
+                    if (obj instanceof MapStack) {
+                        return (MapStack<String>) obj;
+                    }
+                }
+            } catch (Exception e) {
+                ;
+            }
+        }
+        return null;
+    }
 }

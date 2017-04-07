e94192382a53a369296f47d98dd657a12c716390
Paul Piper <pp@ilscipio.com>
2017-04-07 16:00:33 +0000

    Ref #1459
    * Fixed an issue with the SystemMessages table
    * Implemented a seca to automatically capture new system messages
    * Removed annoying debug log entry
    
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@15397 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/common/webcommon/WEB-INF/actions/includes/scipio/GetUserNotifications.groovy
+++ framework/common/webcommon/WEB-INF/actions/includes/scipio/GetUserNotifications.groovy
@@ -7,7 +7,6 @@ if(context.userLogin!=null){
     inMap = [:];
     inMap.toPartyId = context.userLogin.partyId;
     result = runService("getSystemMessages", inMap);
-    Debug.logInfo(""+result,module);
     context.systemNotifications = result.messages !=null ? result.messages : [];
     context.systemNotificationsCount = result.count;
 }

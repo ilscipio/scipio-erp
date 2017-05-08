e49c1629172b4238c733cae108259f32c1cfd7a5
Pascal Proulx <pplx@ilscipio.com>
2017-05-08 13:51:49 +0000

    Refs #1484/OFBIZ patch: renderer: targeted rendering support WIP: just use scpRenderTargetExpr less confusing
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@15684 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/webapp/src/org/ofbiz/webapp/renderer/RenderEvents.java
+++ framework/webapp/src/org/ofbiz/webapp/renderer/RenderEvents.java
@@ -60,10 +60,10 @@ public abstract class RenderEvents {
         String encoding = getStrParam(request, "encoding", RENDER_VIEW_ATTR_PREFIX);
         */
         String view = getStrParam(request, "view", RENDER_VIEW_ATTR_PREFIX);
-        String renderTargetExpr = getStrParam(request, "renderTargetExpr", RENDER_VIEW_ATTR_PREFIX);
-        if (renderTargetExpr != null) {
-            request.setAttribute("scpRenderTargetExpr", renderTargetExpr);
-        }
+        //String renderTargetExpr = getStrParam(request, "renderTargetExpr", RENDER_VIEW_ATTR_PREFIX);
+        //if (renderTargetExpr != null) {
+        //    request.setAttribute("scpRenderTargetExpr", renderTargetExpr);
+        //}
         
         HttpServletRequest req = request;
         HttpServletResponse resp = response;

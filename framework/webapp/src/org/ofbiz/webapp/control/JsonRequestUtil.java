62e4284c03d856e35754a1f7b4f56c70a9ce4297
Pascal Proulx <pplx@ilscipio.com>
2017-05-13 01:22:10 +0000

    Refs #1484/OFBIZ patch: renderer: targeted rendering: JsonRequestUtil: new methods for scp/isViewAsJson, other fixes
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@15775 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/webapp/src/org/ofbiz/webapp/control/JsonRequestUtil.java
+++ framework/webapp/src/org/ofbiz/webapp/control/JsonRequestUtil.java
@@ -1,11 +1,9 @@
 package org.ofbiz.webapp.control;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,8 +18,12 @@ import org.ofbiz.webapp.control.ConfigXMLReader.ControllerConfig;
 public abstract class JsonRequestUtil {
 
     public static final String VIEWASJSON_REQPARAM = "scpViewAsJson";
+    public static final String VIEWASJSONUSESESSION_REQPARAM = "scpViewAsJsonUpSess";
     
-    private static final List<String> allowedOutParamNames = UtilMisc.unmodifiableArrayList(
+    // FIXME: unhardcode
+    private static final String defaultViewAsJsonRequestUri = "jsonExplicit";
+    
+    private static final Set<String> allowedOutParamNames = UtilMisc.unmodifiableHashSet(
         "_ERROR_MESSAGE_", "_ERROR_MESSAGE_LIST_", "_EVENT_MESSAGE_", "_EVENT_MESSAGE_LIST_"
     );
     
@@ -30,7 +32,7 @@ public abstract class JsonRequestUtil {
     private JsonRequestUtil() {
     }
 
-    public static List<String> getDefaultAllowedParamNames(HttpServletRequest request) {
+    public static Set<String> getDefaultAllowedParamNames(HttpServletRequest request) {
         return allowedOutParamNames;
     }
 
@@ -47,23 +49,27 @@ public abstract class JsonRequestUtil {
         return UtilGenerics.checkMap(request.getAttribute("scipioOutParams"));
     }
     
-    public static List<String> getRenderOutAttrNames(HttpServletRequest request) {
-        List<String> outAttrNames = UtilGenerics.checkList(request.getAttribute("scipioOutAttrNames"));
+    public static Set<String> getRenderOutAttrNames(HttpServletRequest request) {
+        Set<String> outAttrNames = UtilGenerics.checkSet(request.getAttribute("scipioOutAttrNames"));
         if (outAttrNames == null) {
-            outAttrNames = new ArrayList<>();
+            outAttrNames = new HashSet<>();
         }
         request.setAttribute("scipioOutAttrNames", outAttrNames);
         return outAttrNames;
     }
     
-    public static List<String> getRenderOutAttrNamesOrNull(HttpServletRequest request) {
-        return UtilGenerics.checkList(request.getAttribute("scipioOutAttrNames"));
+    public static Set<String> getRenderOutAttrNamesOrNull(HttpServletRequest request) {
+        return UtilGenerics.checkSet(request.getAttribute("scipioOutAttrNames"));
     }
 
+    public static void addAllDefaultAllowedParamNames(HttpServletRequest request) {
+        getRenderOutAttrNames(request).addAll(getDefaultAllowedParamNames(request));
+    }
+    
     public static Map<String, Object> collectRenderOutAttributes(HttpServletRequest request) {
         Map<String, Object> outAttrMap = JsonRequestUtil.getRenderOutParamsOrNull(request);
         Map<String, Object> attrMap = (outAttrMap != null) ? UtilHttp.transformJSONAttributeMap(outAttrMap) : new HashMap<String, Object>();
-        List<String> attrNames = JsonRequestUtil.getRenderOutAttrNamesOrNull(request);
+        Set<String> attrNames = JsonRequestUtil.getRenderOutAttrNamesOrNull(request);
         if (attrNames != null && attrNames.size() > 0) {
             // TODO: optimize
             Map<String, Object> allAttrMap = UtilHttp.getJSONAttributeMap(request);
@@ -76,4 +82,23 @@ public abstract class JsonRequestUtil {
         return attrMap;
     }
     
+    public static boolean isViewAsJson(HttpServletRequest request) {
+        Boolean viewAsJson = (Boolean) request.getAttribute(VIEWASJSON_REQPARAM);
+        if (viewAsJson != null) return viewAsJson;
+        else return "true".equals(request.getParameter(VIEWASJSON_REQPARAM));
+    }
+    
+    public static boolean isViewAsJsonUpdateSession(HttpServletRequest request) {
+        Boolean viewAsJson = (Boolean) request.getAttribute(VIEWASJSONUSESESSION_REQPARAM);
+        if (viewAsJson != null) return viewAsJson;
+        else return "true".equals(request.getParameter(VIEWASJSONUSESESSION_REQPARAM));
+    }
+    
+    public static String getDefaultViewAsJsonRequestUri() {
+        return defaultViewAsJsonRequestUri; // FIXME: unhardcode
+    }
+    
+    public static String getViewAsJsonRequestUri(HttpServletRequest request, ControllerConfig controllerConfig) {
+        return defaultViewAsJsonRequestUri; // FIXME: unhardcode (needs site-conf.xsd entry)
+    }
 }

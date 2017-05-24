a870da4328d4c7bd75aead927a5b51ac856d8e82
Pascal Proulx <pplx@ilscipio.com>
2017-05-24 00:13:43 +0000

    NoRef accounting: TaxAuthorityForms: factor out tax auth list prep code
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@15907 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

---
+++ applications/accounting/webapp/accounting/WEB-INF/actions/tax/GetTaxAuthorityListForDisplay.groovy
@@ -0,0 +1,19 @@
+/**
+ * SCIPIO: prepares tax authority list and entries for display in a drop-down.
+ */
+
+taxAuthorityList = from('TaxAuthority').orderBy("taxAuthPartyId", "taxAuthGeoId").queryList();
+context.taxAuthorityList = taxAuthorityList;
+ 
+infoList = [];
+if (taxAuthorityList) {
+    for(taxAuthority in taxAuthorityList) {
+        def info = [:];
+        info.putAll(taxAuthority);
+        info.taxAuthCombinedId = taxAuthority.taxAuthPartyId + "::" + taxAuthority.taxAuthGeoId;
+        info.party = from("PartyNameView").where("partyId", taxAuthority.taxAuthPartyId).queryOne();
+        info.geo = from("Geo").where("geoId", taxAuthority.taxAuthGeoId).queryOne();
+        infoList.add(info);
+    }
+}
+context.taxAuthorityInfoList = infoList;

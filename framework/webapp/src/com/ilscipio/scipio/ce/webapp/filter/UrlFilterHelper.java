3c91a747b0557f4cd1f125a5ba2a4f051574a423
Pascal Proulx <pplx@ilscipio.com>
2017-08-14 20:29:14 +0000

    NoRef UrlFilterHelper: new utility
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@17048 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

---
+++ framework/webapp/src/com/ilscipio/scipio/ce/webapp/filter/UrlFilterHelper.java
@@ -0,0 +1,52 @@
+package com.ilscipio.scipio.ce.webapp.filter;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * SCIPIO: Helper methods for URL rewriting and filtering.
+ * Added 2017-08-14.
+ */
+public class UrlFilterHelper {
+
+    public void verifySameWebappContext(HttpServletRequest request, HttpServletResponse response) {
+        String outboundUrlStr = (String) request.getAttribute("urlRewriteMod.outboundUrlWebapp.outboundUrl");
+        boolean isSameContextPath = isSameWebappContext(request, outboundUrlStr);
+        request.setAttribute("urlRewriteMod.outboundUrlWebapp.isSameContext", isSameContextPath ? "true" : "false");
+    }
+    
+    public boolean isSameWebappContext(HttpServletRequest request, String outboundUrlStr) {
+        if (outboundUrlStr != null) {
+            String currentContextPath = request.getContextPath();
+            String urlContextPath = getPathFromUrl(outboundUrlStr);
+            if (urlContextPath.equals(currentContextPath)) {
+                return true;
+            }
+            else {
+                if (!currentContextPath.endsWith("/")) currentContextPath += "/";
+                return urlContextPath.startsWith(currentContextPath);
+            }
+        }
+        return false;
+    }
+
+    // FIXME: ideally should optimize the regexp away
+    private static final Pattern pathPat = Pattern.compile("^([^/]*//[^/]*)?(/.*?)?([?;].*)?$");
+    protected static String getPathFromUrl(String url) {
+        String result = null;
+        Matcher matcher = pathPat.matcher(url);
+        if (matcher.matches()) {
+            String pathMatch = matcher.group(2);
+            if (pathMatch.isEmpty()) {
+                result = "/";
+            }
+            else {
+                result = pathMatch;
+            }
+        }
+        return result;
+    }
+}

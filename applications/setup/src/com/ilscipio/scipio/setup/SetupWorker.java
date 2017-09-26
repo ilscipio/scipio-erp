f5427e4c1112cc8c8a471a226924b3a0e4fc3144
Jonatan Soto <jonatan.soto@ilscipio.com>
2017-09-26 17:39:26 +0000

    NoRef (Setup): Changed accounting required params
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@17489 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- applications/setup/src/com/ilscipio/scipio/setup/SetupWorker.java
+++ applications/setup/src/com/ilscipio/scipio/setup/SetupWorker.java
@@ -1220,7 +1220,7 @@ public abstract class SetupWorker implements Serializable {
         }
         
         private static final StepParamInfo accountingStepParamInfo =
-                StepParamInfo.fromRequiredAndOptional(nameSet("orgPartyId", "productStoreId"), nameSet());
+                StepParamInfo.fromRequiredAndOptional(nameSet("userPartyId"), nameSet());
         protected class AccountingStepState extends CommonStepState {
             public AccountingStepState(StaticStepState partial) { super(partial); }
             

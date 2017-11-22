ccd56f852118f7393a46ce8dd4f424aeef4b08c2
Pascal Proulx <pplx@ilscipio.com>
2017-11-22 18:23:43 +0000

    NoRef PropertyMessage: bunch of helpers and standardize which will become important in short time
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@18083 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/base/src/org/ofbiz/base/util/PropertyMessage.java
+++ framework/base/src/org/ofbiz/base/util/PropertyMessage.java
@@ -1,6 +1,9 @@
 package org.ofbiz.base.util;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -12,13 +15,31 @@ import java.util.Map;
 @SuppressWarnings("serial")
 public abstract class PropertyMessage implements Serializable {
 
+    public static final String module = PropertyMessage.class.getName();
+    
     protected abstract String getMessageImpl(Locale locale);
+    
+    public static Locale getDefPropLocale() {
+        return UtilProperties.getFallbackLocale();
+    }
+    
+    public static Locale getDefSysLocale() {
+        return Locale.getDefault();
+    }
+    
+    public static Locale getLogLocale() {
+        return Locale.ENGLISH;
+    }
+    
+    public static Locale getDefExLocale() {
+        return Locale.ENGLISH;
+    }
     
     /**
      * Gets message in the specified locale, or in the default/fallback property locale if null ({@link UtilProperties#getFallbackLocale()}).
      */
     public String getMessage(Locale locale) {
-        return getMessageImpl(locale != null ? locale : UtilProperties.getFallbackLocale());
+        return getMessageImpl(locale != null ? locale : getDefPropLocale());
     }
 
     /**
@@ -39,35 +60,28 @@ public abstract class PropertyMessage implements Serializable {
      * Gets message in default/fallback locale specified by locale.properties.fallback in general.properties ({@link UtilProperties#getFallbackLocale()}).
      */
     public String getDefPropLocaleMessage() {
-        return getMessage(UtilProperties.getFallbackLocale());
+        return getMessage(getDefPropLocale());
     }
     
     /**
      * Gets message in default system locale ({@link Locale#getDefault()}).
      */
     public String getDefSysLocaleMessage() {
-        return getMessage(Locale.getDefault());
+        return getMessage(getDefSysLocale());
     }
     
     /**
      * Gets message in the log language (always {@link Locale#ENGLISH} in scipio/ofbiz).
      */
-    public String getLogMessage() { // logs always in english
-        return getMessage(Locale.ENGLISH);
+    public String getLogMessage() {
+        return getMessage(getLogLocale());
     }
     
     /**
-     * Gets message in the {@link Locale#ENGLISH} locale (convenience method).
+     * Gets message in the exception language (always {@link Locale#ENGLISH} in scipio/ofbiz).
      */
-    public String getEnMessage() {
-        return getMessage(Locale.ENGLISH);
-    }
-    
-    /**
-     * Gets message in the {@link Locale#GERMAN} locale (convenience method).
-     */
-    public String getDeMessage() {
-        return getMessage(Locale.GERMAN);
+    public String getDefExLocaleMessage() {
+        return getMessage(getDefExLocale());
     }
     
     /**
@@ -77,6 +91,33 @@ public abstract class PropertyMessage implements Serializable {
     @Deprecated
     public String getMessage() {
         return getMessage(Locale.getDefault());
+    }
+    
+    public static List<String> getMessages(Collection<PropertyMessage> propMsgs, Locale locale) {
+        if (propMsgs == null) return null;
+        List<String> msgs = new ArrayList<>(propMsgs.size());
+        for(PropertyMessage propMsg : propMsgs) {
+            if (propMsg == null) continue;
+            String msg = propMsg.getMessage(locale);
+            if (msg != null) msgs.add(msg);
+        }
+        return msgs;
+    }
+    
+    public static List<String> getDefPropLocaleMessages(Collection<PropertyMessage> propMsgs) {
+        return getMessages(propMsgs, getDefPropLocale());
+    }
+    
+    public static List<String> getDefSysLocaleMessages(Collection<PropertyMessage> propMsgs) {
+        return getMessages(propMsgs, getDefSysLocale());
+    }
+    
+    public static List<String> getLogMessages(Collection<PropertyMessage> propMsgs) {
+        return getMessages(propMsgs, getLogLocale());
+    }
+    
+    public static List<String> getDefExLocaleMessages(Collection<PropertyMessage> propMsgs) {
+        return getMessages(propMsgs, getDefExLocale());
     }
     
     public static PropertyMessage make(String resource, String propertyName, Map<String, ?> propArgs, String prefix, String suffix) {
@@ -103,6 +144,57 @@ public abstract class PropertyMessage implements Serializable {
         return new ExceptionPropertyMessage(t);
     }
     
+    /**
+     * Makes an appropriate PropertyMessage instance for the object instance type.
+     * @param message the source message object
+     * @param strict if true and not a String, Throwable or PropertyMessage, throws IllegalArgumentException; 
+     *               if false, unknown type gets toString() and wrapped in StaticPropertyMessage
+     */
+    public static PropertyMessage makeAuto(Object message, boolean strict) {
+        if (message == null) return null;
+        else if (message instanceof PropertyMessage) return (PropertyMessage) message;
+        else if (message instanceof String) return new StaticPropertyMessage((String) message);
+        else if (message instanceof Throwable) return new ExceptionPropertyMessage((Throwable) message);
+        else {
+            if (strict) throw new IllegalArgumentException("cannot convert object to PropertyMessage, unknown type: " + message.getClass().getName());
+            else {
+                try {
+                    return new StaticPropertyMessage(message.toString());
+                } catch(Throwable t) {
+                    // this will likely never happen, it's just to be safe in case toString() explodes on some instance
+                    Debug.logError(t, "PropertyMessage.makeAuto: Unexpected error converting message to PropertyMessage instance (error message swallowed): " + t.getMessage(), module);
+                    return null;
+                }
+            }
+        }
+    }
+    
+    /**
+     * SCIPIO: Returns a new list where each given message is converted to a PropertyMessage instance
+     * if it is not already one.
+     * @param messages the source messages objects
+     * @param strict if true and not a String, Throwable or PropertyMessage, throws IllegalArgumentException; 
+     *               if false, safe mode, unknown type gets toString() and wrapped in StaticPropertyMessage, and no exceptions are thrown (logged instead)
+     */
+    public static List<PropertyMessage> makeAutoFromList(Collection<?> messages, boolean strict) {
+        if (messages == null) return null;
+        List<PropertyMessage> propMsgs = new ArrayList<>(messages.size());
+        for(Object msg : propMsgs) {
+            if (msg == null) {
+                if (strict) {
+                    throw new NullPointerException("PropertyMessage.makeAutoFromList: encountered null message in list");
+                } else {
+                    Debug.logWarning("PropertyMessage.makeAutoFromList: null message encountered; filtering out (caller may not be creating error messages properly)", module);
+                    continue;
+                }
+            }
+            PropertyMessage propMsg = makeAuto(msg, strict);
+            if (propMsg != null) {
+                propMsgs.add(propMsg);
+            }
+        }
+        return propMsgs;
+    }
     
     public static class StaticPropertyMessage extends PropertyMessage {
         private final String message;

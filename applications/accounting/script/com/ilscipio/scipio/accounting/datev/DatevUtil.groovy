1b0e85f3c89077b605b4f22afb727b5b0b75810b
Jonatan Soto <jonatan.soto@ilscipio.com>
2017-12-04 15:52:01 +0000

    Refs #1575: Implemented csv upload logic in a groovy script
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@18244 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

---
+++ applications/accounting/script/com/ilscipio/scipio/accounting/datev/DatevUtil.groovy
@@ -0,0 +1,58 @@
+import java.nio.ByteBuffer
+
+import org.apache.commons.fileupload.FileItem
+import org.apache.commons.fileupload.FileUploadException
+import org.apache.commons.fileupload.disk.DiskFileItemFactory
+import org.apache.commons.fileupload.servlet.ServletFileUpload
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.FileUtil
+import org.ofbiz.base.util.UtilGenerics
+import org.ofbiz.base.util.UtilProperties
+import org.ofbiz.content.content.UploadContentAndImage
+import org.ofbiz.webapp.event.FileUploadProgressListener
+
+import javolution.util.FastMap
+
+final String module = "DatevUtil.groovy";
+
+ServletFileUpload dfu = new ServletFileUpload(new DiskFileItemFactory(10240, FileUtil.getFile("runtime/tmp")));
+dfu.setHeaderEncoding("ISO-8859-1")
+
+// SCIPIO: patch - from ServiceEventHandler: create the progress listener and add it to the session
+FileUploadProgressListener listener = new FileUploadProgressListener();
+dfu.setProgressListener(listener);
+request.getSession().setAttribute("uploadProgressListener", listener);
+
+List<FileItem> lst = null;
+try {
+    lst = UtilGenerics.checkList(dfu.parseRequest(request));
+} catch (FileUploadException e4) {
+    request.setAttribute("_ERROR_MESSAGE_", e4.getMessage());
+    Debug.logError("[UploadDatevCSV] " + e4.getMessage(), module);
+    return "error";
+}
+//if (Debug.infoOn()) Debug.logInfo("[UploadContentAndImage]lst " + lst, module);
+
+if (lst.size() == 0) {
+    String errMsg = UtilProperties.getMessage(UploadContentAndImage.err_resource, "uploadContentAndImage.no_files_uploaded", locale);
+    request.setAttribute("_ERROR_MESSAGE_", errMsg);
+    Debug.logWarning("[UploadDatevCSV] No files uploaded", module);
+    return "error";
+}
+
+Map<String, Object> passedParams = FastMap.newInstance();
+FileItem csvFileItem = null;
+byte[] csvFileBytes = [];
+for (int i = 0; i < lst.size(); i++) {
+    csvFileItem = lst.get(i);
+    String fieldName = csvFileItem.getFieldName();
+    if (csvFileItem.isFormField()) {
+        String fieldStr = csvFileItem.getString();
+        passedParams.put(fieldName, fieldStr);
+    } else {        
+        csvFileBytes = csvFileItem.get();
+    }
+}
+
+context.passedParams=passedParams;
+context.uploadedFile=ByteBuffer.wrap(csvFileItem.get());

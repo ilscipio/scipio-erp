228c815623fce1deb43def0f3a0f0c8dad08ec3b
Pascal Proulx <pplx@ilscipio.com>
2017-12-04 23:24:48 +0000

    NoRef PropertyMessageEx: formalize SettablePropertyMessageEx to try to make less confusing for client code, added PropertyMessageExUtil to fill the implementation void
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@18247 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/base/src/org/ofbiz/base/util/PropertyMessageEx.java
+++ framework/base/src/org/ofbiz/base/util/PropertyMessageEx.java
@@ -1,11 +1,17 @@
 package org.ofbiz.base.util;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
- * Interface for exceptions that allow passing around a PropertyMessage representing the
+ * SCIPIO: Interface for exceptions that allow passing around a PropertyMessage representing the
  * main exception message in PropertyMessage form, and additionally a separate list
- * of property messages that behaves like
+ * of property messages that behaves like and is meant to be used like a services 
+ * errorMessageList or successMessageList.
+ * <p>
+ * NOTE: The message list is meant to be user-friendly like services message lists. 
+ * If the exception needs to pass a list of more "exception-like" error messages, 
+ * it should manage a separate list of such messages.
  */
 public interface PropertyMessageEx {
     
@@ -21,4 +27,58 @@ public interface PropertyMessageEx {
      */
     List<PropertyMessage> getPropertyMessageList();
     
+    /**
+     * Setter methods for property message and lists, for exceptions that choose to
+     * support this separate from the constructor.
+     * <p>
+     * This is needed because changing the exception constructor hierarchies is prohibitively
+     * intrusive, despite that modifying exceptions post-constructors is not very common
+     * (but is not disallowed).
+     */
+    public interface SettablePropertyMessageEx extends PropertyMessageEx {
+        /**
+         * SCIPIO: Setter for property message.
+         * Workaround for massive constructor inheritance.
+         * Returns the exception so that can be easily chained in a throw statement.
+         */
+        public SettablePropertyMessageEx setPropertyMessage(PropertyMessage propertyMessage);
+        
+        /**
+         * SCIPIO: Setter for property messages, which may be in the form of any type
+         * convertible to PropertyMessage including simple Strings.
+         * Workaround for massive constructor inheritance.
+         * Returns the exception so that can be easily chained in a throw statement.
+         * <p>
+         * DEV NOTE: For implementation, simply pass the messageList to
+         * {@link PropertyMessageExUtil#makePropertyMessageList(Collection)} and store result
+         * in the instance.
+         */
+        public SettablePropertyMessageEx setPropertyMessageList(Collection<?> messageList);
+    }
+    
+    /**
+     * SCIPIO: Utilities for implementing and using {@link PropertyMessageEx}.
+     *
+     */
+    public static abstract class PropertyMessageExUtil {
+        protected PropertyMessageExUtil() {
+        }
+        
+        /**
+         * SCIPIO: Safely makes a PropertyMessage list for storage in an exception.
+         * If null given, returns null.
+         */
+        public static List<PropertyMessage> makePropertyMessageList(Collection<?> messages) {
+            return PropertyMessage.makeListAutoSafe(messages);
+        }
+        
+        /**
+         * SCIPIO: Safely makes a PropertyMessage list for storage in an exception.
+         * The argument can be a single message or collection.
+         * If null given, returns null.
+         */
+        public static List<PropertyMessage> makePropertyMessageList(Object messageOrList) {
+            return PropertyMessage.makeListAutoSafe(messageOrList);
+        }
+    }
 }

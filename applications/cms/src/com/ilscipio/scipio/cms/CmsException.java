2917c8eccdac50c0c5182a54e4bf89836656d89c
Pascal Proulx <pplx@ilscipio.com>
2017-12-04 23:33:48 +0000

    NoRef cms: CmsException: enhancements for SettablePropertyMessageEx
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@18251 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- applications/cms/src/com/ilscipio/scipio/cms/CmsException.java
+++ applications/cms/src/com/ilscipio/scipio/cms/CmsException.java
@@ -1,74 +1,101 @@
 package com.ilscipio.scipio.cms;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
 
+import org.ofbiz.base.util.GeneralException;
 import org.ofbiz.base.util.PropertyMessage;
 import org.ofbiz.base.util.PropertyMessageEx;
+import org.ofbiz.base.util.PropertyMessageEx.PropertyMessageExUtil;
+import org.ofbiz.base.util.PropertyMessageEx.SettablePropertyMessageEx;
 
 /**
  * Base CMS exception.
+ * <p>
+ * This is essentially an unchecked version of {@link org.ofbiz.base.util.GeneralException},
+ * and some of the subclasses are analogous.
  * <p>
  * 2016: Supports PropertyMessage for delayed localization of error message;
  * code that catches can call {@link #getPropertyMessage} and
  * pass it a locale to localize the message.
  */
 @SuppressWarnings("serial")
-public class CmsException extends RuntimeException implements PropertyMessageEx {
+public class CmsException extends RuntimeException implements SettablePropertyMessageEx {
 
-    protected final PropertyMessage propMsg;
+    protected PropertyMessage propertyMessage;
+    protected List<PropertyMessage> messages;
     
     public CmsException(String msg, Throwable e) {
         super(msg, e);
-        this.propMsg = PropertyMessage.makeFromStatic(msg);
+        this.propertyMessage = PropertyMessage.makeFromStatic(msg);
     }
     
     public CmsException(String msg) {
         super(msg);
-        this.propMsg = PropertyMessage.makeFromStatic(msg);
+        this.propertyMessage = PropertyMessage.makeFromStatic(msg);
     }
     
     public CmsException(Throwable e) {
         super(e);
-        this.propMsg = PropertyMessage.makeFromException(e);
+        this.propertyMessage = PropertyMessage.makeFromException(e);
     }
     
     public CmsException(String msg, PropertyMessage propMsg, Throwable e) {
         super(msg, e);
-        this.propMsg = propMsg;
+        this.propertyMessage = propMsg;
     }
     
     public CmsException(String msg, PropertyMessage propMsg) {
         super(msg);
-        this.propMsg = propMsg;
+        this.propertyMessage = propMsg;
     }
     
     public CmsException(PropertyMessage propMsg, Throwable e) {
-        super(propMsg.getMessage(Locale.ENGLISH), e); // NOTE: ex msg is always ENGLISH, not Locale.getDefault()
-        this.propMsg = propMsg;
+        super(propMsg.getDefExLocaleMessage(), e); // NOTE: ex msg is always ENGLISH, not Locale.getDefault()
+        this.propertyMessage = propMsg;
     }
     
     public CmsException(PropertyMessage propMsg) {
-        super(propMsg.getMessage(Locale.ENGLISH)); // NOTE: ex msg is always ENGLISH, not Locale.getDefault()
-        this.propMsg = propMsg;
+        super(propMsg.getDefExLocaleMessage()); // NOTE: ex msg is always ENGLISH, not Locale.getDefault()
+        this.propertyMessage = propMsg;
+    }
+    
+    /**
+     * Setter for property message.
+     * Workaround for massive constructor inheritance.
+     * Returns CmsException so that can be easily chained in a throw statement.
+     */
+    public CmsException setPropertyMessage(PropertyMessage propertyMessage) {
+        this.propertyMessage = propertyMessage;
+        return this;
+    }
+    
+    /**
+     * SCIPIO: Setter for message list including property messages.
+     * Workaround for massive constructor inheritance.
+     * Returns GeneralException so that can be easily chained in a throw statement.
+     */
+    @Override
+    public CmsException setPropertyMessageList(Collection<?> messageList) {
+        this.messages = PropertyMessageExUtil.makePropertyMessageList(messageList);
+        return this;
     }
     
     public PropertyMessage getPropertyMessage() {
-        return propMsg;
+        return propertyMessage;
     }
     
     public String getFormattedPropertyMessage() {
-        return propMsg.getDefPropLocaleMessage();
+        return propertyMessage.getDefPropLocaleMessage();
     }
     
     public String getFormattedPropertyMessage(Locale locale) {
-        return propMsg.getMessage(locale);
+        return propertyMessage.getMessage(locale);
     }
-
+    
     @Override
     public List<PropertyMessage> getPropertyMessageList() {
-        // TODO
-        return null;
+        return messages;
     }
-
 }

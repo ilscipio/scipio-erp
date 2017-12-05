d4084076958e7590b57cab7e3ad76e64bb68c621
Pascal Proulx <pplx@ilscipio.com>
2017-12-05 21:06:58 +0000

    NoRef ServletUtil: new ServletContextInitParamsMapAdapter wrapper to simplify context-param reading (due to nasty old servlet API interfaces)
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@18264 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/webapp/src/org/ofbiz/webapp/control/ServletUtil.java
+++ framework/webapp/src/org/ofbiz/webapp/control/ServletUtil.java
@@ -1,11 +1,18 @@
 package org.ofbiz.webapp.control;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 
 import org.ofbiz.base.util.Debug;
+import org.ofbiz.webapp.control.FilterUtil.FilterConfigInitParamsMapAdapter;
 
 /**
  * SCIPIO: Servlet and ServletContext generic utilities.
@@ -55,4 +62,68 @@ public abstract class ServletUtil {
         return mapping;
     }
     
+    /**
+     * Gets map of context-params from servlet context
+     * Fill-in for missing java servlet API method.
+     */
+    public static Map<String, String> getContextParams(ServletContext servletContext) {
+        Map<String, String> initParams = new HashMap<>();
+        Enumeration<String> names = servletContext.getInitParameterNames();
+        while(names.hasMoreElements()) {
+            String name = names.nextElement();
+            initParams.put(name, servletContext.getInitParameter(name));
+        }
+        return initParams;
+    }
+    
+    public static ServletContextInitParamsMapAdapter getContextParamsMapAdapter(ServletContext servletContext) {
+        return new ServletContextInitParamsMapAdapter(servletContext);
+    }
+    
+    /**
+     * Wraps a ServletContext and avoids creating whole map if only <code>get</code> calls are done.
+     */
+    public static class ServletContextInitParamsMapAdapter implements Map<String, String> {
+
+        private final ServletContext servletContext;
+        private Map<String, String> initParams = null;
+
+        public ServletContextInitParamsMapAdapter(ServletContext config) {
+            this.servletContext = config;
+        }
+        
+        protected Map<String, String> getInitParamsMap() {
+            Map<String, String> initParams = this.initParams;
+            if (initParams == null) {
+                initParams = Collections.unmodifiableMap(ServletUtil.getContextParams(servletContext));
+                this.initParams = initParams;
+            }
+            return initParams;
+        }
+
+        @Override
+        public int size() { return getInitParamsMap().size(); }
+        @Override
+        public boolean isEmpty() { return getInitParamsMap().size() == 0; }
+        @Override
+        public boolean containsKey(Object key) { return getInitParamsMap().containsKey(key); }
+        @Override
+        public boolean containsValue(Object value) { return getInitParamsMap().containsValue(value); }
+        @Override
+        public String get(Object key) { return servletContext.getInitParameter((String) key); }
+        @Override
+        public String put(String key, String value) { throw new UnsupportedOperationException(); }
+        @Override
+        public String remove(Object key) { throw new UnsupportedOperationException(); }
+        @Override
+        public void putAll(Map<? extends String, ? extends String> m) { throw new UnsupportedOperationException(); }
+        @Override
+        public void clear() { throw new UnsupportedOperationException(); }
+        @Override
+        public Set<String> keySet() { return getInitParamsMap().keySet(); }
+        @Override
+        public Collection<String> values() { return getInitParamsMap().values(); }
+        @Override
+        public Set<java.util.Map.Entry<String, String>> entrySet() { return getInitParamsMap().entrySet(); }        
+    }
 }

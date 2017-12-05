b55a8888d27868f8e555b251b21f14273cc24785
Jonatan Soto <jonatan.soto@ilscipio.com>
2017-12-05 08:57:03 +0000

    Refs #1575: Parsing datev csv on import (WIP)
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@18257 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- applications/accounting/src/com/ilscipio/scipio/accounting/datev/DatevServices.java
+++ applications/accounting/src/com/ilscipio/scipio/accounting/datev/DatevServices.java
@@ -5,13 +5,24 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.csv.QuoteMode;
+import org.apache.tika.mime.MediaType;
 import org.ofbiz.base.util.Debug;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityQuery;
 import org.ofbiz.service.DispatchContext;
 import org.ofbiz.service.ServiceUtil;
+
+import com.ilscipio.scipio.common.util.TikaUtil;
 
 public class DatevServices {
 
@@ -24,28 +35,44 @@ public class DatevServices {
      * @return
      */
     public static Map<String, Object> importDatevTransactionEntries(DispatchContext dctx, Map<String, Object> context) {
+        Delegator delegator = dctx.getDelegator();
+
         // TODO: Implement import datev data from a csv
         String orgPartyId = (String) context.get("organizationPartyId");
         ByteBuffer fileBytes = (ByteBuffer) context.get("uploadedFile");
         
-//        context.get(fieldName, ByteBuffer.wrap(item.get()));
-//        context.get("_" + fieldName + "_contentType"):
+        String fileSize = (String) context.get("_uploadedFile_size");
+        String fileName = (String) context.get("_uploadedFile_fileName");
+        String contentType = (String) context.get("_uploadedFile_contentType");
+        
+        Debug.log("contentType =====> " + contentType);
         
         String encoding = System.getProperty("file.encoding");
         Debug.log("encoding ======> " + encoding);
-        String csvString = Charset.forName(encoding).decode(fileBytes).toString();
+//        TikaUtil.findCharset(, fileName);
+        String csvString = Charset.forName("ISO-8859-1").decode(fileBytes).toString();
         final BufferedReader csvReader = new BufferedReader(new StringReader(csvString));
-        CSVFormat fmt = CSVFormat.DEFAULT.withHeader();
+        CSVFormat fmt = CSVFormat.DEFAULT.withDelimiter(';').withQuote('"').withQuoteMode(QuoteMode.NON_NUMERIC);
         
         try {
-            int i = 1;
-            for (final CSVRecord rec : fmt.parse(csvReader)) {
-                Map<String, String> recordMap = rec.toMap();
-                for (String key : recordMap.keySet()) {
-                    Debug.log("Record [" + i + "]: <" + key + ">  => " + recordMap.get(key));
+            List<GenericValue> datevTransactionEntryDefinitions = EntityQuery.use(delegator).from("DatevTransactionEntryDefinition").queryList();            
+            
+            CSVParser parser = fmt.parse(csvReader);
+            
+            MediaType mediaType = TikaUtil.findMediaType(fileBytes, fileName);
+            Debug.log("media type ====>" + mediaType.getType());
+
+            for (final CSVRecord rec : parser.getRecords()) {
+                Map<String, String> recordMap = rec.toMap();                
+                for (Iterator<String> iter = rec.iterator(); iter.hasNext();) {
+                    String value = iter.next();
+                    Debug.log("Record [" + rec.getRecordNumber() + "]: <" + value + ">");
                 }
             }
         } catch (IOException e) {
+            Debug.logError(e, module);
+            return ServiceUtil.returnError(e.getMessage());
+        } catch (GenericEntityException e) {
             Debug.logError(e, module);
             return ServiceUtil.returnError(e.getMessage());
         }

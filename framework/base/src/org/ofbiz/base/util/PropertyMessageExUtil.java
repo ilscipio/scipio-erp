4c1287cd6c075290fee71a87d757a7d12db4bb5e
Pascal Proulx <pplx@ilscipio.com>
2017-12-08 01:35:47 +0000

    NoRef PropertyMessageExUtil: partial revert, service component dependency missing
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@18294 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- framework/base/src/org/ofbiz/base/util/PropertyMessageExUtil.java
+++ framework/base/src/org/ofbiz/base/util/PropertyMessageExUtil.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import org.ofbiz.service.ServiceUtil;
-
 /**
  * SCIPIO: Utilities for implementing and using {@link PropertyMessageEx}.
  */
@@ -82,53 +80,53 @@ public abstract class PropertyMessageExUtil {
         return t.getMessage();
     }
     
-    /**
-     * Creates a service error result from the exception containing the main exception message
-     * and message list, taken from {@link PropertyMessageEx}, with optional intro message.
-     */
-    public static Map<String, Object> makeServiceErrorResult(PropertyMessage messageIntro, Throwable t, Locale locale) {
-        return ServiceUtil.returnError(makeServiceMessage(messageIntro, t, locale), getExceptionMessageList(t, locale));
-    }
-    
-    /**
-     * Creates a service error result from the exception containing the main exception message
-     * and message list, taken from {@link PropertyMessageEx}, with optional intro message.
-     */
-    public static Map<String, Object> makeServiceErrorResult(String messageIntro, Throwable t, Locale locale) {
-        return makeServiceErrorResult(PropertyMessage.makeFromStatic(messageIntro), t, locale);
-    }
-    
-    /**
-     * Creates a service error result from the exception containing the main exception message
-     * and message list, taken from {@link PropertyMessageEx}.
-     */
-    public static Map<String, Object> makeServiceErrorResult(Throwable t, Locale locale) {
-        return makeServiceErrorResult((PropertyMessage) null, t, locale);
-    }
-    
-    /**
-     * Creates a service failure result from the exception containing the main exception message
-     * and message list, taken from {@link PropertyMessageEx}, with optional intro message.
-     */
-    public static Map<String, Object> makeServiceFailureResult(PropertyMessage messageIntro, Throwable t, Locale locale) {
-        return ServiceUtil.returnFailure(makeServiceMessage(messageIntro, t, locale), getExceptionMessageList(t, locale));
-    }
-    
-    /**
-     * Creates a service failure result from the exception containing the main exception message
-     * and message list, taken from {@link PropertyMessageEx}, with optional intro message.
-     */
-    public static Map<String, Object> makeServiceFailureResult(String messageIntro, Throwable t, Locale locale) {
-        return makeServiceFailureResult(PropertyMessage.makeFromStatic(messageIntro), t, locale);
-    }
-    
-    /**
-     * Creates a service failure result from the exception containing the main exception message
-     * and message list, taken from {@link PropertyMessageEx}.
-     */
-    public static Map<String, Object> makeServiceFailureResult(Throwable t, Locale locale) {
-        return makeServiceFailureResult((PropertyMessage) null, t, locale);
-    }
+//    /**
+//     * Creates a service error result from the exception containing the main exception message
+//     * and message list, taken from {@link PropertyMessageEx}, with optional intro message.
+//     */
+//    public static Map<String, Object> makeServiceErrorResult(PropertyMessage messageIntro, Throwable t, Locale locale) {
+//        return ServiceUtil.returnError(makeServiceMessage(messageIntro, t, locale), getExceptionMessageList(t, locale));
+//    }
+//    
+//    /**
+//     * Creates a service error result from the exception containing the main exception message
+//     * and message list, taken from {@link PropertyMessageEx}, with optional intro message.
+//     */
+//    public static Map<String, Object> makeServiceErrorResult(String messageIntro, Throwable t, Locale locale) {
+//        return makeServiceErrorResult(PropertyMessage.makeFromStatic(messageIntro), t, locale);
+//    }
+//    
+//    /**
+//     * Creates a service error result from the exception containing the main exception message
+//     * and message list, taken from {@link PropertyMessageEx}.
+//     */
+//    public static Map<String, Object> makeServiceErrorResult(Throwable t, Locale locale) {
+//        return makeServiceErrorResult((PropertyMessage) null, t, locale);
+//    }
+//    
+//    /**
+//     * Creates a service failure result from the exception containing the main exception message
+//     * and message list, taken from {@link PropertyMessageEx}, with optional intro message.
+//     */
+//    public static Map<String, Object> makeServiceFailureResult(PropertyMessage messageIntro, Throwable t, Locale locale) {
+//        return ServiceUtil.returnFailure(makeServiceMessage(messageIntro, t, locale), getExceptionMessageList(t, locale));
+//    }
+//    
+//    /**
+//     * Creates a service failure result from the exception containing the main exception message
+//     * and message list, taken from {@link PropertyMessageEx}, with optional intro message.
+//     */
+//    public static Map<String, Object> makeServiceFailureResult(String messageIntro, Throwable t, Locale locale) {
+//        return makeServiceFailureResult(PropertyMessage.makeFromStatic(messageIntro), t, locale);
+//    }
+//    
+//    /**
+//     * Creates a service failure result from the exception containing the main exception message
+//     * and message list, taken from {@link PropertyMessageEx}.
+//     */
+//    public static Map<String, Object> makeServiceFailureResult(Throwable t, Locale locale) {
+//        return makeServiceFailureResult((PropertyMessage) null, t, locale);
+//    }
     
     public static String makeServiceMessage(PropertyMessage messageIntro, Throwable t, Locale locale) {
         String msg = null;

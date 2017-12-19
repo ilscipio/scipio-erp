6030d69a5abfc6d2d5a4b985e113b87c2dba7c6d
Jonatan Soto <jonatan.soto@ilscipio.com>
2017-12-19 13:06:23 +0000

    Refs #1575: Datev CSV parse implementation (WIP):
    * Helper no longer contains specific import/export logic. That has been delegated to proper classes that are instantiated through reflection.
    * New class used to collect stats of all actions performed (with different scopes and levels)
    
    
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@18400 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

---
+++ applications/accounting/src/com/ilscipio/scipio/accounting/external/datev/DatevHelper.java
@@ -0,0 +1,73 @@
+package com.ilscipio.scipio.accounting.external.datev;
+
+import java.lang.reflect.Constructor;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.ofbiz.entity.Delegator;
+
+import com.ilscipio.scipio.accounting.external.OperationStats;
+import com.ilscipio.scipio.accounting.external.OperationStats.NotificationLevel;
+import com.ilscipio.scipio.accounting.external.OperationStats.NotificationScope;
+import com.ilscipio.scipio.accounting.external.OperationStats.Stat;
+import com.ilscipio.scipio.accounting.external.datev.category.AbstractDatevDataCategory;
+
+public class DatevHelper {
+    private static final String module = DatevHelper.class.getName();
+
+    private final String orgPartyId;
+    private final OperationStats stats;
+    private final AbstractDatevDataCategory dataCategory;
+
+    public DatevHelper(Delegator delegator, String orgPartyId, Class<? extends AbstractDatevDataCategory> dataCategoryClass) throws DatevException {
+        this.orgPartyId = orgPartyId;
+        try {
+            Constructor<? extends AbstractDatevDataCategory> datevDataCategoryConstructor = dataCategoryClass.getConstructor(Delegator.class, DatevHelper.class);
+            this.dataCategory = datevDataCategoryConstructor.newInstance(delegator, this);
+            this.stats = dataCategory.getOperationStatsClass().newInstance();
+        } catch (Exception e) {
+            throw new DatevException("Internal error. Cannot initialize DATEV helper.");
+        }
+
+    }
+
+    public void processRecord(Map<String, String> recordMap) throws DatevException {
+        dataCategory.processRecord(recordMap);
+    }
+
+    public String[] getFieldNames() throws DatevException {
+        return dataCategory.getFieldNames();
+    }
+
+    public boolean validateField(Object o, String value) throws DatevException {
+        if (o instanceof String)
+            return dataCategory.validateField(String.valueOf(o), value);
+        else if (o instanceof Integer)
+            return dataCategory.validateField((Integer) o, value);
+        return false;
+    }
+
+    public String getOrgPartyId() {
+        return orgPartyId;
+    }
+
+    public boolean isMetaHeader(Iterator<String> metaHeader) throws DatevException {
+        return dataCategory.isMetaHeader(metaHeader);
+    }
+
+    public OperationStats getStats() {
+        return stats;
+    }
+
+    public void addRecordStat(String message, NotificationLevel level, int position, Map<String, String> value, boolean valid) {
+        addStat(stats.new RecordStat(message, level, position, value, valid));
+    }
+
+    public void addStat(String message, NotificationScope scope, NotificationLevel level) {
+        addStat(stats.new Stat(message, scope, level));
+    }
+
+    private void addStat(Stat stat) {
+        stats.addStat(stat);
+    }
+}
---
+++ applications/accounting/src/com/ilscipio/scipio/accounting/external/datev/DatevServices.java
@@ -0,0 +1,234 @@
+package com.ilscipio.scipio.accounting.external.datev;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.csv.QuoteMode;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.txt.UniversalEncodingDetector;
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.ServiceUtil;
+
+import com.ilscipio.scipio.accounting.external.OperationStats.NotificationLevel;
+import com.ilscipio.scipio.accounting.external.OperationStats.NotificationScope;
+import com.ilscipio.scipio.accounting.external.datev.category.Buchungsstapel;
+import com.ilscipio.scipio.common.util.TikaUtil;
+
+import javolution.util.FastList;
+import javolution.util.FastMap;
+
+public class DatevServices {
+
+    public final static String module = DatevServices.class.getName();
+    public final static Character DEFAULT_DELIMITER = ';';
+    public final static Character DEFAULT_QUOTE = '\"';
+
+    /**
+     * 
+     * @param dctx
+     * @param context
+     * @return
+     */
+    public static Map<String, Object> importDatevTransactionEntries(DispatchContext dctx, Map<String, Object> context) {
+        Delegator delegator = dctx.getDelegator();
+
+        // Prepare result objects
+        Map<String, Object> result = ServiceUtil.returnSuccess();
+        List<GenericValue> transactionEntries = FastList.newInstance();
+
+        // Get context params
+        String orgPartyId = (String) context.get("orgPartyId");
+        String topGlAccountId = (String) context.get("topGlAccountId");
+        ByteBuffer fileBytes = (ByteBuffer) context.get("uploadedFile");
+        String fileSize = (String) context.get("_uploadedFile_size");
+        String fileName = (String) context.get("_uploadedFile_fileName");
+        String contentType = (String) context.get("_uploadedFile_contentType");
+
+        BufferedReader csvReader = null;
+        DatevHelper datevHelper = null;
+        String errorMessage = null;
+        try {
+            // Initialize helper
+            datevHelper = new DatevHelper(delegator, orgPartyId, Buchungsstapel.class);
+
+            Character delimiter = DEFAULT_DELIMITER;
+            if (UtilValidate.isNotEmpty(context.get("delimiter")))
+                delimiter = ((String) context.get("delimiter")).charAt(0);
+            Character quote = DEFAULT_QUOTE;
+            if (UtilValidate.isNotEmpty(context.get("quote")))
+                quote = ((String) context.get("quote")).charAt(0);
+            Boolean hasMetaHeader = (Boolean) context.get("hasMetaHeader");
+            Boolean hasHeader = (Boolean) context.get("hasHeader");
+
+            if (Debug.isOn(Debug.VERBOSE)) {
+                Debug.log("Content Type :" + contentType);
+                Debug.log("File Name    :" + fileName);
+                Debug.log("File Size    :" + fileSize);
+            }
+
+            double fileSizeConverted = UtilMisc.toDouble(fileSize);
+            if (fileSizeConverted <= 0 && !fileBytes.hasRemaining()) {
+                throw new DatevException("Uploaded CSV file is empty");
+            }
+
+            // Find media type
+            MediaType mediaType = TikaUtil.findMediaTypeSafe(fileBytes, fileName);
+            if (mediaType != null) {
+                String[] splittedContentType = contentType.split("/");
+                if (splittedContentType.length != 2) {
+                    String notificationMessage = "File content type [" + contentType + "] is invalid";
+                    datevHelper.addStat(notificationMessage, NotificationScope.GLOBAL, NotificationLevel.WARNING);
+                    Debug.logWarning(notificationMessage, module);
+                }
+
+                String mediaTypeStr = mediaType.getType().concat("/").concat(mediaType.getSubtype());
+                if (!contentType.equals("text/csv") && !mediaTypeStr.equals("text/csv")) {
+                    ServiceUtil.returnError("File [" + fileName + "] is not a valid CSV file.");
+                } else if (!contentType.equals(mediaTypeStr)) {
+                    String notificationMessage = "File content type  [" + contentType + "] differs from the content type found by Tika [" + mediaType.getType() + "/"
+                            + mediaType.getSubtype() + "]";
+                    datevHelper.addStat(notificationMessage, NotificationScope.GLOBAL, NotificationLevel.WARNING);
+                    Debug.logWarning(notificationMessage, module);
+                }
+            }
+            fileBytes.rewind();
+
+            // Find charset
+            Charset detectedCharset = TikaUtil.findCharsetSafe(fileBytes, fileName, UniversalEncodingDetector.class, mediaType);
+            if (UtilValidate.isEmpty(detectedCharset)) {
+                String systemEncoding = System.getProperty("file.encoding");
+                detectedCharset = Charset.forName(systemEncoding);
+            }
+            fileBytes.rewind();
+
+            // Initialize CSV format
+            fileBytes.rewind();
+            csvReader = new BufferedReader(new StringReader(detectedCharset.decode(fileBytes).toString()));
+            CSVFormat fmt = CSVFormat.newFormat(delimiter).withQuote(quote).withQuoteMode(QuoteMode.NON_NUMERIC);
+
+            // Find out if CSV has a meta header so we can remove it and loop
+            // only real records
+            if (csvReader.markSupported())
+                csvReader.mark(fileBytes.limit());
+            String metaHeader = csvReader.readLine();
+            try {
+                Iterator<String> metaHeaderIter = CSVParser.parse(metaHeader, fmt).getRecords().get(0).iterator();
+                if (!datevHelper.isMetaHeader(metaHeaderIter)) {
+                    if (csvReader.markSupported()) {
+                        csvReader.reset();
+                    } else {
+                        csvReader.close();
+                        csvReader = new BufferedReader(new StringReader(detectedCharset.decode(fileBytes).toString()));
+                    }
+                }
+            } catch (Exception e) {
+                datevHelper.addStat("Failed parsing metadata header: " + e.getMessage(), NotificationScope.META_HEADER, NotificationLevel.IGNORE);
+                Debug.logWarning(e, module);
+            }
+
+            // Find out if CSV has a header so we can remove it and loop only
+            // real records
+            String[] datevTransactionsFieldNames = datevHelper.getFieldNames();
+            fmt = fmt.withHeader(datevTransactionsFieldNames);
+            if (fmt.getHeader() != null && fmt.getHeader().length > 0) {
+                fmt = fmt.withSkipHeaderRecord(true);
+            } else {
+                datevHelper.addStat("Header couldn't be found. CSV file parsed using column position according to DATEV specification.", NotificationScope.HEADER,
+                        NotificationLevel.WARNING);
+            }
+
+            // Parse CSV
+            CSVParser parser = fmt.parse(csvReader);
+            List<CSVRecord> records = parser.getRecords();
+            if (parser.getRecordNumber() <= 0) {
+                throw new DatevException("No records found after CSV has been parsed.");
+            } else {
+                Iterator<CSVRecord> recordIter = records.iterator();
+                for (int x = 0; recordIter.hasNext(); x++) {
+                    final CSVRecord rec = recordIter.next();
+                    if (Debug.isOn(Debug.VERBOSE)) {
+                        Debug.logInfo(rec.toString(), module);
+                    }
+                    boolean allFieldValid = true;
+                    Map<String, String> recordMap = FastMap.newInstance();
+                    if (rec.isConsistent()) {
+                        recordMap = rec.toMap();
+                        for (String key : recordMap.keySet()) {
+                            if (!datevHelper.validateField(key, recordMap.get(key))) {
+                                allFieldValid = false;
+                            }
+                        }
+                    } else {
+                        // Assuming here the fields (columns) are in the order
+                        // DATEV specification determines
+                        Iterator<String> iter = rec.iterator();
+                        for (int i = 0; iter.hasNext(); i++) {
+                            String value = iter.next();
+                            if (!datevHelper.validateField(i, value)) {
+                                allFieldValid = false;
+                            }
+                            recordMap.put(datevTransactionsFieldNames[i], value);
+                        }
+                    }
+                    if (allFieldValid) {
+                        datevHelper.processRecord(recordMap);
+                    } else {
+                        datevHelper.addRecordStat("Error processing record [" + x + "]", NotificationLevel.ERROR, x, recordMap, false);
+                    }
+                }
+            }
+
+        } catch (DatevException e) {
+            Debug.logError(e, module);
+            errorMessage = e.getMessage();
+        } catch (Exception e) {
+            Debug.logError(e, module);
+        } finally {
+            if (UtilValidate.isNotEmpty(datevHelper)) {
+                datevHelper.addStat(errorMessage, NotificationScope.GLOBAL, NotificationLevel.FATAL);
+                result.put("operationStats", datevHelper.getStats());
+            } else {
+                result = ServiceUtil.returnError(errorMessage);
+            }
+
+            fileBytes.clear();
+            try {
+                csvReader.close();
+            } catch (IOException e) {
+                ;
+            }
+        }
+
+        result.put("orgPartyId", orgPartyId);
+        result.put("topGlAccountId", topGlAccountId);
+        result.put("transactionEntries", transactionEntries);
+
+        return result;
+    }
+
+    /**
+     * 
+     * @param dctx
+     * @param context
+     * @return
+     */
+    public static Map<String, Object> exportDatevTransactionEntries(DispatchContext dctx, Map<String, Object> context) {
+        // TODO: Implement export datev data in csv format
+        return ServiceUtil.returnSuccess();
+    }
+
+}

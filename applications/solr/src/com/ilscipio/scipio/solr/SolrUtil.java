b445dd864ff079c83af797014a6a61cbe307fe0c
Pascal Proulx <pplx@ilscipio.com>
2018-01-03 22:11:15 +0000

    Refs #1583 solr: SolrStatus: new transaction-isolated SolrStatus update code, so that the DIRTY status update will no longer get reverted on errors (NOTE: did not apply this to the OK-marking code yet; using most conservative logic; see comments)
    git-svn-id: svn://data.ilscipio.com/ilscipio/ofbiz_foundation/trunk@18454 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- applications/solr/src/com/ilscipio/scipio/solr/SolrUtil.java
+++ applications/solr/src/com/ilscipio/scipio/solr/SolrUtil.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import javax.transaction.Transaction;
+
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.ofbiz.base.component.ComponentConfig;
 import org.ofbiz.base.component.ComponentConfig.WebappInfo;
@@ -16,6 +18,8 @@ import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.transaction.GenericTransactionException;
+import org.ofbiz.entity.transaction.TransactionUtil;
 import org.ofbiz.entity.util.EntityQuery;
 
 /**
@@ -151,35 +155,82 @@ public abstract class SolrUtil {
         return solrStatus != null ? solrStatus.getString("dataStatusId") : null;
     }
     
-    public static boolean setSolrDataStatusId(Delegator delegator, String dataStatusId, boolean updateVersion) {
-        GenericValue solrStatus;
-        try {
-            solrStatus = EntityQuery.use(delegator).from("SolrStatus")
-                    .where("solrId", "SOLR-MAIN").cache(false).queryOne();
-            //solrStatus = delegator.findOne("SolrStatus", UtilMisc.toMap("solrId", "SOLR-MAIN"), false);
-            if (solrStatus == null) {
-                Debug.logWarning("Could not get SolrStatus for SOLR-MAIN - creating new", module);
-                solrStatus = delegator.create("SolrStatus", 
-                        "solrId", "SOLR-MAIN", 
-                        "dataStatusId", dataStatusId, 
-                        "dataCfgVersion", getSolrConfigVersionStatic());
-            } else {
-                solrStatus.setString("dataStatusId", dataStatusId);
-                if (updateVersion) {
-                    solrStatus.setString("dataCfgVersion", getSolrConfigVersionStatic());
-                }
-                solrStatus.store();
+    public static void setSolrDataStatusId(Delegator delegator, String dataStatusId, boolean updateVersion) throws GenericEntityException {
+        GenericValue solrStatus = EntityQuery.use(delegator).from("SolrStatus")
+                .where("solrId", "SOLR-MAIN").cache(false).queryOne();
+        //solrStatus = delegator.findOne("SolrStatus", UtilMisc.toMap("solrId", "SOLR-MAIN"), false);
+        if (solrStatus == null) {
+            Debug.logWarning("Could not get SolrStatus for SOLR-MAIN - creating new", module);
+            solrStatus = delegator.create("SolrStatus", 
+                    "solrId", "SOLR-MAIN", 
+                    "dataStatusId", dataStatusId, 
+                    "dataCfgVersion", getSolrConfigVersionStatic());
+        } else {
+            solrStatus.setString("dataStatusId", dataStatusId);
+            if (updateVersion) {
+                solrStatus.setString("dataCfgVersion", getSolrConfigVersionStatic());
             }
-            return true;
-            
-        } catch (GenericEntityException e) {
-            Debug.logError(e, module);
-            return false;
+            solrStatus.store();
         }
     }
     
-    public static boolean setSolrDataStatusId(Delegator delegator, String dataStatusId) {
-        return setSolrDataStatusId(delegator, dataStatusId, false);
+    public static void setSolrDataStatusIdSafe(Delegator delegator, String dataStatusId, boolean updateVersion) {
+        try {
+            setSolrDataStatusId(delegator, dataStatusId, updateVersion);
+        } catch (Exception t) {
+            final String errMsg = "Error while trying to mark Solr data status (" + dataStatusId + "): " + t.getMessage();
+            Debug.logError(t, "Solr: " + errMsg, module);
+        }
+    }
+    
+    public static void setSolrDataStatusId(Delegator delegator, String dataStatusId) throws GenericEntityException {
+        setSolrDataStatusId(delegator, dataStatusId, false);
+    }
+    
+    // DEV NOTE: could have done this with service call, but just in case need fine-grained control...
+    public static boolean setSolrDataStatusIdSepTxSafe(Delegator delegator, String dataStatusId, boolean updateVersion) {
+        Transaction parentTransaction = null;
+        boolean beganTrans = false;
+        try {
+            try {
+                if (TransactionUtil.isTransactionInPlace()) {
+                    parentTransaction = TransactionUtil.suspend();
+                    // now start a new transaction
+                    beganTrans = TransactionUtil.begin();
+                } else {
+                    beganTrans = TransactionUtil.begin();
+                }
+            } catch (GenericTransactionException t) {
+                Debug.logError(t, "Solr: Cannot create transaction to mark Solr data status (" + dataStatusId + ")", module);
+            }
+            try {
+                SolrUtil.setSolrDataStatusId(delegator, dataStatusId, updateVersion);
+                return true;
+            } catch (Throwable t) {
+                final String errMsg = "Error while trying to mark Solr data status (" + dataStatusId + "): " + t.getMessage();
+                Debug.logError(t, "Solr: " + errMsg, module);
+                try {
+                    TransactionUtil.rollback(beganTrans, errMsg, t);
+                } catch (GenericTransactionException te) {
+                    Debug.logError(te, "Solr: Cannot rollback transaction to mark Solr data status (" + dataStatusId + ")", module);
+                }
+            } finally {
+                try {
+                    TransactionUtil.commit(beganTrans);
+                } catch (GenericTransactionException e) {
+                    Debug.logError(e, "Solr: Could not commit transaction to mark Solr data status (" + dataStatusId + ")", module);
+                }
+            }
+        } finally {
+            if (parentTransaction != null) {
+                try {
+                    TransactionUtil.resume(parentTransaction);
+                } catch (GenericTransactionException t) {
+                    Debug.logError(t, "Solr: Error resuming parent transaction after marking Solr data status (" + dataStatusId + ")", module);
+                }
+            }
+        }
+        return false;
     }
     
     public static HttpSolrClient getHttpSolrClient(String core) {

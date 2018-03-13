454d43acff60052708e46a7bece5db1d11476230
Jonatan Soto <jonatan.soto@ilscipio.com>
2018-03-13 11:36:39 +0100

    Refs https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/438
    * So far, only party data has been properly covered for jfairy provider. Other types of data might not covered by default with jfairy, meaning we may have to extend ourselves (future task).

--- specialpurpose/demosuite/script/com/ilscipio/data/PartyData.groovy
+++ specialpurpose/demosuite/script/com/ilscipio/data/PartyData.groovy
@@ -2,23 +2,22 @@ import java.sql.Timestamp
 
 import org.ofbiz.base.crypto.HashCrypt
 import org.ofbiz.base.util.Debug
-import org.ofbiz.base.util.UtilDateTime
 import org.ofbiz.base.util.UtilMisc
 import org.ofbiz.base.util.UtilRandom
-import org.ofbiz.base.util.UtilValidate
 import org.ofbiz.common.login.LoginServices
 import org.ofbiz.entity.*
 import org.ofbiz.entity.util.*
-import org.ofbiz.service.ServiceUtil
 
-import com.ilscipio.scipio.ce.demoSuite.dataGenerator.DemoSuiteDataWorker
-import com.ilscipio.scipio.ce.demoSuite.dataGenerator.MockarooDataGenerator
+import com.ilscipio.scipio.ce.demoSuite.dataGenerator.DataGenerator
+import com.ilscipio.scipio.ce.demoSuite.dataGenerator.dataObject.DemoDataObject
 import com.ilscipio.scipio.ce.demoSuite.dataGenerator.dataObject.DemoDataPerson
 import com.ilscipio.scipio.ce.demoSuite.dataGenerator.dataObject.DemoDataUserLogin
 import com.ilscipio.scipio.ce.demoSuite.dataGenerator.service.DataGeneratorGroovyBaseScript
 
 
 public class PartyData extends DataGeneratorGroovyBaseScript {
+	
+	final String DATA_TYPE = "party";
     
     PartyData() {
         Debug.logInfo("-=-=-=- DEMO DATA CREATION SERVICE - PARTY DATA-=-=-=-", "");
@@ -38,22 +37,22 @@ public class PartyData extends DataGeneratorGroovyBaseScript {
         "PARTY_ENABLED"
     ]
     
-    public void init() {        
-        List<DemoDataPerson> generatedPersons = DemoSuiteDataWorker.generatePerson(num, MockarooDataGenerator.class);
-        List<DemoDataUserLogin> generatedUserLogins = DemoSuiteDataWorker.generateUserLogin(num, MockarooDataGenerator.class);
-        setNumRecordsToBeGenerated(generatedPersons.size());
+	public String getDataType() {
+		return DATA_TYPE;
+	}
+	
+	public void init() {				
         List<GenericValue> roleTypeIds = from("RoleType").cache(false).queryList();
-        context.generatedPersons = generatedPersons;
-        context.generatedUserLogins = generatedUserLogins;
-        context.roleTypeIds = roleTypeIds;
+        context.roleTypeIds = roleTypeIds;		
     }
     
-    List prepareData(int index) throws Exception {
+    List prepareData(int index, DemoDataObject partyData) throws Exception {
         List<GenericValue> toBeStored = new ArrayList<GenericValue>();
-        List<GenericValue> partyEntrys = new ArrayList<GenericValue>();
+        List<GenericValue> partyEntrys = new ArrayList<GenericValue>();		
+		
+		DataGenerator generator = context.generator;		
         
-        if ( (context.generatedPersons && context.generatedPersons.get(index)) 
-            && (context.generatedUserLogins && context.generatedUserLogins.get(index))) {            
+        if (partyData) {            
             String partyId = "GEN_" + delegator.getNextSeqId("demo-partyId");
             String partyTypeId = partyTypeIds.get(UtilRandom.random(partyTypeIds));        
             String partyStatusId = partyStatus.get(UtilRandom.random(partyStatus)); 
@@ -62,14 +61,13 @@ public class PartyData extends DataGeneratorGroovyBaseScript {
             Map fields = UtilMisc.toMap("partyId", partyId, "partyTypeId", partyTypeId, "statusId", partyStatusId, "description", partyId + " description", "createdDate", createdDate);    
             GenericValue party = delegator.makeValue("Party", fields);
             toBeStored.add(party);
-
             
             GenericValue roleType = context.roleTypeIds.get(UtilRandom.random(context.roleTypeIds));
             fields = UtilMisc.toMap("roleTypeId", roleType.roleTypeId, "partyId", partyId);        
             GenericValue partyRole = delegator.makeValue("PartyRole", fields);        
             toBeStored.add(partyRole);
             
-            DemoDataPerson demoDataPerson = context.generatedPersons.get(index);            
+            DemoDataPerson demoDataPerson = partyData.getPerson();            
             String salutation = demoDataPerson.getTitle();
             String firstName = demoDataPerson.getFirstName();
             String lastName = demoDataPerson.getLastName();
@@ -82,21 +80,23 @@ public class PartyData extends DataGeneratorGroovyBaseScript {
             GenericValue person = delegator.makeValue("Person", fields);
             toBeStored.add(person);
             
-            DemoDataUserLogin demoDataUserLogin = context.generatedUserLogins.get(index);
-            String userLoginId = demoDataUserLogin.getUserLoginId();
-            String currentPassword = demoDataUserLogin.getCurrentPassword();
-            if (!context.generatePassword)
-                currentPassword = DEFAULT_USER_LOGIN_PWD;
-            boolean useEncryption = "true".equals(EntityUtilProperties.getPropertyValue("security.properties", "password.encrypt", delegator));
-            if (useEncryption)
-                currentPassword = HashCrypt.cryptUTF8(LoginServices.getHashType(), null, currentPassword)
-            userLoginEnabled = "Y";
-            if (partyStatusId.equals("PARTY_DISABLED"))
-                userLoginEnabled = "N";
-            
-            fields = UtilMisc.toMap("partyId", partyId, "userLoginId", userLoginId, "currentPassword", currentPassword, "enabled", userLoginEnabled);
-            GenericValue userLogin = delegator.makeValue("UserLogin", fields);
-            toBeStored.add(userLogin);
+			if (generator.getHelper().generateUserLogin()) {
+	            DemoDataUserLogin demoDataUserLogin = partyData.getUserLogin();
+	            String userLoginId = demoDataUserLogin.getUserLoginId();
+	            String currentPassword = demoDataUserLogin.getCurrentPassword();
+	            if (!context.generatePassword)
+	                currentPassword = DEFAULT_USER_LOGIN_PWD;
+	            boolean useEncryption = "true".equals(EntityUtilProperties.getPropertyValue("security.properties", "password.encrypt", delegator));
+	            if (useEncryption)
+	                currentPassword = HashCrypt.cryptUTF8(LoginServices.getHashType(), null, currentPassword)
+	            userLoginEnabled = "Y";
+	            if (partyStatusId.equals("PARTY_DISABLED"))
+	                userLoginEnabled = "N";
+	            
+	            fields = UtilMisc.toMap("partyId", partyId, "userLoginId", userLoginId, "currentPassword", currentPassword, "enabled", userLoginEnabled);
+	            GenericValue userLogin = delegator.makeValue("UserLogin", fields);
+	            toBeStored.add(userLogin);
+			}
         }
         return toBeStored;
     }

43e6d36ecff60ca3bd8df7abc4430743f914543e
Pascal Proulx <pplx@ilscipio.com>
2018-03-26 21:12:23 -0400

    OFBIZ patch: build: base/ivy: increased rome to 1.9.0
    Small package name change required in RomeEventHandler and services.
    
    Refs https://gitlab.ilscipio.com/SCIPIO-ERP/scipioce/issues/9,
    https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/466

--- applications/content/src/org/ofbiz/content/blog/BlogRssServices.java
+++ applications/content/src/org/ofbiz/content/blog/BlogRssServices.java
@@ -40,12 +40,12 @@ import org.ofbiz.service.DispatchContext;
 import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.service.ServiceUtil;
 
-import com.sun.syndication.feed.synd.SyndContent;
-import com.sun.syndication.feed.synd.SyndContentImpl;
-import com.sun.syndication.feed.synd.SyndEntry;
-import com.sun.syndication.feed.synd.SyndEntryImpl;
-import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.feed.synd.SyndFeedImpl;
+import com.rometools.rome.feed.synd.SyndContent;
+import com.rometools.rome.feed.synd.SyndContentImpl;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndEntryImpl;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.feed.synd.SyndFeedImpl;
 
 /**
  * BlogRssServices

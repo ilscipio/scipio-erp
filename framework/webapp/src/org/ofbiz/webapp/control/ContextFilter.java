4a74f16514f205c7433333619e443ffeb76e4829
Pascal Proulx <pplx@ilscipio.com>
2018-05-10 10:57:41 -0400

    OFBIZ merge: select ContextFilter fixes from 17.12 (up to 1827442)
    This commit merges some fixes to ContextFilter from 17.12 including but
    not limited to:
    
    1760528 - "set the character encoding to the system default (UTF-8) if
    and only if it is not set already by the client."
    1805677 - some fixes performed during upstream refactoring of
    ContextFilter with ControlFilter (the refactoring itself is not applied)
    
    Notable fixes:
    * ContextFilter previously failed to set delegator in request attribs
    for multitenant
    * Failed to reliably set a default delegator/dispatcher/security in
    request attribs
    * Bad root request check for multitenant
    
    NOTE: The _REQ_ATTR_MAP_ code removal from 1805677 is NOT applied;
    unclear if issue from that change applies to scipio or implications.
    
    1827442 was only latest change considered (not applied).
    
    from http://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/branches/release17.12

--- framework/webapp/src/org/ofbiz/webapp/control/ContextFilter.java
+++ framework/webapp/src/org/ofbiz/webapp/control/ContextFilter.java
@@ -77,13 +77,17 @@ public class ContextFilter implements Filter {
     protected Set<String> allowedPaths = null; // SCIPIO: new: prevent parsing at every request
     protected boolean forwardRootControllerUris = false; // SCIPIO: new
 
+    // default charset used to decode requests body data if no encoding is specified in the request
+    private String defaultCharacterEncoding;
+    private boolean isMultitenant;
+
     /**
      * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
      */
     public void init(FilterConfig config) throws ServletException {
         this.config = config;
 
-        // puts all init-parameters in ServletContext attributes for easier parameterization without code changes
+        // puts all init-parameters in ServletContext attributes for easier parametrization without code changes
         this.putAllInitParametersInAttributes();
 
         // set debug
@@ -92,6 +96,10 @@ public class ContextFilter implements Filter {
             debug = Debug.verboseOn();
         }
 
+        defaultCharacterEncoding = config.getServletContext().getInitParameter("charset");
+        if (UtilValidate.isEmpty(defaultCharacterEncoding)) {
+            defaultCharacterEncoding = "UTF-8";
+        }
         // check the serverId
         getServerId();
         // initialize the delegator
@@ -100,6 +108,9 @@ public class ContextFilter implements Filter {
         getSecurity();
         // initialize the services dispatcher
         getDispatcher(config.getServletContext());
+
+        // check if multi tenant is enabled
+        isMultitenant = EntityUtil.isMultiTenantEnabled();
 
         // this will speed up the initial sessionId generation
         new java.security.SecureRandom().nextLong();
@@ -301,15 +312,20 @@ public class ContextFilter implements Filter {
             }
         }
 
-        // check if multi tenant is enabled
-        boolean useMultitenant = EntityUtil.isMultiTenantEnabled();
-        setCharacterEncoding(request);
+        if (request.getCharacterEncoding() == null) {
+            request.setCharacterEncoding(defaultCharacterEncoding);
+        }
+
         setAttributesFromRequestBody(request);
-        if (useMultitenant) {
+
+        request.setAttribute("delegator", config.getServletContext().getAttribute("delegator"));
+        request.setAttribute("dispatcher", config.getServletContext().getAttribute("dispatcher"));
+        request.setAttribute("security", config.getServletContext().getAttribute("security"));
+
+        if (isMultitenant) {
             // get tenant delegator by domain name
             String serverName = httpRequest.getServerName();
             try {
-                
                 // if tenant was specified, replace delegator with the new per-tenant delegator and set tenantId to session attribute
                 Delegator delegator = getDelegator(config.getServletContext());
 
@@ -325,11 +341,11 @@ public class ContextFilter implements Filter {
                     tenantId = (String) httpRequest.getAttribute("userTenantId");
                 }
                 if(UtilValidate.isEmpty(tenantId)) {
-                    tenantId = (String) httpRequest.getParameter("userTenantId");
+                    tenantId = httpRequest.getParameter("userTenantId");
                 }
                 if (UtilValidate.isNotEmpty(tenantId)) {
                     // if the request path is a root mount then redirect to the initial path
-                    if (UtilValidate.isNotEmpty(requestPath) && requestPath.equals(contextUri)) {
+                    if ("".equals(httpRequest.getContextPath()) && "".equals(httpRequest.getServletPath())) {
                         GenericValue tenant = EntityQuery.use(baseDelegator).from("Tenant").where("tenantId", tenantId).queryOne();
                         String initialPath = tenant.getString("initialPath");
                         if (UtilValidate.isNotEmpty(initialPath) && !"/".equals(initialPath)) {
@@ -356,6 +372,7 @@ public class ContextFilter implements Filter {
                     LocalDispatcher dispatcher = getDispatcher(config.getServletContext());
 
                     // set web context objects
+                    request.setAttribute("delegator", delegator);
                     request.setAttribute("dispatcher", dispatcher);
                     request.setAttribute("security", security);
                     

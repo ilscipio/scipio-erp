ilscipio/scipio-erp

commit 2892ce1c339c14f7e05da3463a387dc4f751a795

java: com.ilscipio.*: strip .properties extensions
consistency issue only, no functional change.

applications/cms/src/com/ilscipio/scipio/cms/template/CmsRenderTemplate.java
@@ -187,9 +187,9 @@ public interface CmsRenderTemplate extends Serializable {
             ClassLoader loader = Thread.currentThread().getContextClassLoader();
             Enumeration<URL> resources;
             try {
-                resources = loader.getResources("cmsFreemarkerTransforms.properties");
+                resources = loader.getResources("cmsFreemarkerTransforms");
             } catch (IOException e) {
-                Debug.logError(e, "Could not load list of cmsFreemarkerTransforms.properties", module);
+                Debug.logError(e, "Could not load list of cmsFreemarkerTransforms", module);
                 throw UtilMisc.initCause(new InternalError(e.getMessage()), e);
             }
             while (resources.hasMoreElements()) {

applications/cms/src/com/ilscipio/scipio/cms/CmsUtil.java
@@ -15,7 +15,7 @@ public abstract class CmsUtil {
 
     private static final Debug.OfbizLogger module = Debug.getOfbizLogger(java.lang.invoke.MethodHandles.lookup().lookupClass());
     
-    private static final boolean DEBUG = UtilProperties.getPropertyAsBoolean("cms.properties", "debug", false);
+    private static final boolean DEBUG = UtilProperties.getPropertyAsBoolean("cms", "debug", false);
     
     static {
         if (DEBUG) {

applications/cms/src/com/ilscipio/scipio/cms/content/CmsPageContent.java
@@ -33,7 +33,7 @@ public class CmsPageContent extends AbstractPreloadable implements Serializable,
     
     //private static final Debug.OfbizLogger module = Debug.getOfbizLogger(java.lang.invoke.MethodHandles.lookup().lookupClass());
     
-    private static final String useNamesForMissingAttributesStr = UtilProperties.getPropertyValue("cms.properties", "render.attributes.useNamesForMissing", "never");
+    private static final String useNamesForMissingAttributesStr = UtilProperties.getPropertyValue("cms", "render.attributes.useNamesForMissing", "never");
     private static final boolean useNamesForMissingAttributesAlways = "always".equals(useNamesForMissingAttributesStr);
     private static final boolean useNamesForMissingAttributesPreview = "preview".equals(useNamesForMissingAttributesStr);
 

applications/cms/src/com/ilscipio/scipio/cms/data/CmsObject.java
@@ -37,9 +37,9 @@ public abstract class CmsObject implements Serializable, Preloadable {
     // will only disable caching but never enable
     // NOTE: the 
     
-    static final boolean ALLOW_DB_CACHE = UtilProperties.getPropertyAsBoolean("cms.properties", "cache.db.allow", true);
-    static final boolean ALLOW_LOCAL_OBJ_CACHE = UtilProperties.getPropertyAsBoolean("cms.properties", "cache.obj.local.allow", true);
-    static final boolean ALLOW_GLOBAL_OBJ_CACHE = UtilProperties.getPropertyAsBoolean("cms.properties", "cache.obj.global.allow", true);
+    static final boolean ALLOW_DB_CACHE = UtilProperties.getPropertyAsBoolean("cms", "cache.db.allow", true);
+    static final boolean ALLOW_LOCAL_OBJ_CACHE = UtilProperties.getPropertyAsBoolean("cms", "cache.obj.local.allow", true);
+    static final boolean ALLOW_GLOBAL_OBJ_CACHE = UtilProperties.getPropertyAsBoolean("cms", "cache.obj.global.allow", true);
 
 // 2016: currently no occurrences of these. 
 //    protected boolean isUseCache() {

applications/cms/src/com/ilscipio/scipio/cms/data/CmsObjectCache.java
@@ -26,7 +26,7 @@ public abstract class CmsObjectCache<T extends CmsObject> {
     
     private static final Debug.OfbizLogger module = Debug.getOfbizLogger(java.lang.invoke.MethodHandles.lookup().lookupClass());
     
-    private static final int EXPIRATION_TIME_DEFAULT = UtilProperties.getPropertyAsInteger("cms.properties", "cache.data.expiration", 10000);
+    private static final int EXPIRATION_TIME_DEFAULT = UtilProperties.getPropertyAsInteger("cms", "cache.data.expiration", 10000);
 
     /**
      * Debug-like flag: If true, the cached preloaded objects are kept mutable
@@ -121,7 +121,7 @@ public abstract class CmsObjectCache<T extends CmsObject> {
         Integer expireTime = null;
         // FIRST, lookup the cache name in cache.properties, so
         // consistent with rest of system
-        String expireTimeStr = UtilProperties.getPropertyValue("cache.properties", cacheName);
+        String expireTimeStr = UtilProperties.getPropertyValue("cache", cacheName);
         if (UtilValidate.isNotEmpty(expireTimeStr)) {
             try {
                 expireTime = (Integer) ObjectType.simpleTypeConvert(expireTimeStr, "Integer", null, null);

applications/cms/src/com/ilscipio/scipio/cms/media/CmsMediaServlet.java
@@ -231,7 +231,7 @@ public class CmsMediaServlet extends HttpServlet {
             }
             
             // SECURITY: 2017-08-02: isPublic check; borrowed from DataEvents.serveObjectData
-            String permissionService = EntityUtilProperties.getPropertyValue("content.properties", "stream.permission.service", "genericContentPermission", delegator);
+            String permissionService = EntityUtilProperties.getPropertyValue("content", "stream.permission.service", "genericContentPermission", delegator);
             // see if data resource is public or not
             if (UtilValidate.isEmpty(isPublic)) {
                 isPublic = "N";

applications/cms/src/com/ilscipio/scipio/cms/template/AttributeExpander.java
@@ -74,7 +74,7 @@ public abstract class AttributeExpander {
         FLEXIBLE("CmsExpandLang.description.FLEXIBLE", "${map1.field1}, ${groovy: [expr]}, ..."),
         SIMPLE("CmsExpandLang.description.SIMPLE", "{{map1.field1}}");
         
-        private static final ExpandLang defaultExpandLang = ExpandLang.fromStringSafe(UtilProperties.getPropertyValue("cms.properties", 
+        private static final ExpandLang defaultExpandLang = ExpandLang.fromStringSafe(UtilProperties.getPropertyValue("cms", 
                 "render.attributes.expandLang.default"), ExpandLang.NONE);
         
         private final String descriptionProperty;
@@ -612,7 +612,7 @@ public abstract class AttributeExpander {
     protected static class FtlAttributeExpander extends AttributeExpander {
         
         // NOTE: we currently don't get this involved in the ftl Configuration instance - no need?
-        private static final boolean bracketSyntaxEnabled = UtilProperties.getPropertyAsBoolean("cms.properties",
+        private static final boolean bracketSyntaxEnabled = UtilProperties.getPropertyAsBoolean("cms",
                 "render.attributes.expandLang.ftl.bracketSyntax", Boolean.TRUE);
         
         protected final Map<Type, TypeParser> typeParsers;

applications/cms/src/com/ilscipio/scipio/cms/template/CmsAssetTemplate.java
@@ -49,7 +49,7 @@ public class CmsAssetTemplate extends CmsMasterComplexTemplate<CmsAssetTemplate,
     private static final Debug.OfbizLogger module = Debug.getOfbizLogger(java.lang.invoke.MethodHandles.lookup().lookupClass());
     
     // default is TRUE
-    private static final boolean firstVersionActive = UtilProperties.getPropertyAsBoolean("cms.properties", "asset.template.firstVersionActive", true);
+    private static final boolean firstVersionActive = UtilProperties.getPropertyAsBoolean("cms", "asset.template.firstVersionActive", true);
     
     private static final CmsObjectCache<CmsAssetTemplate> idCache = CmsObjectCache.getGlobalCache("cms.template.asset.id");
     private static final CmsObjectCache<CmsAssetTemplate> nameCache = CmsObjectCache.getGlobalCache("cms.template.asset.name");

commit dd404c353400efb5080430e068763bf84748f2fd

cms: fix .properties extension bug introduced in 2892ce1c339

applications/cms/src/com/ilscipio/scipio/cms/template/CmsRenderTemplate.java
@@ -187,7 +187,7 @@ public interface CmsRenderTemplate extends Serializable {
             ClassLoader loader = Thread.currentThread().getContextClassLoader();
             Enumeration<URL> resources;
             try {
-                resources = loader.getResources("cmsFreemarkerTransforms");
+                resources = loader.getResources("cmsFreemarkerTransforms.properties");
             } catch (IOException e) {
                 Debug.logError(e, "Could not load list of cmsFreemarkerTransforms", module);
                 throw UtilMisc.initCause(new InternalError(e.getMessage()), e);
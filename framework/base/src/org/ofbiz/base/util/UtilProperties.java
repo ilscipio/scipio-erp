92c26e9d1cb8bc8a60d1b61ac6f513ea1e6fc0fb
Pascal Proulx <pplx@ilscipio.com>
2018-06-11 17:12:11 -0400

    UtilProperties: extractPropertiesWithPrefixAndId: support Object or String keys

--- framework/base/src/org/ofbiz/base/util/UtilProperties.java
+++ framework/base/src/org/ofbiz/base/util/UtilProperties.java
@@ -1286,7 +1286,8 @@ public class UtilProperties implements Serializable {
      * SCIPIO: Extracts properties having the given prefix and keyed by an ID as the next name part between dots.
      * Added 2017-11.
      */
-    public static void extractPropertiesWithPrefixAndId(Map<String, Map<String, Object>> out, Properties properties, String prefix) {
+    @SuppressWarnings("unchecked")
+    public static <T> void extractPropertiesWithPrefixAndId(Map<String, Map<String, T>> out, Properties properties, String prefix) {
         if (prefix == null) prefix = "";
         for(String name : properties.stringPropertyNames()) {
             if (name.startsWith(prefix)) {
@@ -1296,12 +1297,12 @@ public class UtilProperties implements Serializable {
                     String id = rest.substring(0, nextDotIndex);
                     String subName = rest.substring(nextDotIndex + 1);
                     String value = properties.getProperty(name);
-                    Map<String, Object> subMap = out.get(id);
+                    Map<String, T> subMap = out.get(id);
                     if (subMap == null) {
                         subMap = new HashMap<>();
                         out.put(id, subMap);
                     }
-                    subMap.put(subName, value);
+                    subMap.put(subName, (T) value);
                 }
             }
         }

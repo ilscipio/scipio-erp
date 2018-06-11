48a3a34de257fec8d504b04c1b3c3246fd22c559
Pascal Proulx <pplx@ilscipio.com>
2018-06-11 14:42:00 -0400

    security: ContentWrappers: INTERFACE CHANGE: encoderType is now fully honored again by content wrappers ("html" -> HtmlEncoder)
    Beware double escaping! Stock scipio instances have been fixed for this
    in prior commits.
    
    The ContentWrapper get(xxx, encoderType) and static
    getXxxContentAsText(xxx, yyy, ..., encoderType) methods now once again
    will recognize the exact type requested, so "html" is processed by
    UtilCodec.HtmlEncoder.
    
    The Difference with today's code is that since a2598e6281, the encoder's
    sanitize() method is now used instead of its aggressive encode() method.
    So depending on configuration and the encoder, some markup may pass
    through.
    
    NOTE: The screens in Scipio automatically html-escape the content
    wrapper result string (even from the get(xxx, encoderType) call), so to
    actually allow HTML through you must wrap them in ${rawString(...)},
    consistently. (This is much safer than the inconsistent stock Ofbiz
    solution where only one of the methods in ContentWrapper returns a
    StringWrapper, create a confusing messy interface).
    
    NOTE: Only the "html" encoder uses true sanitization at this time. The
    net effect is that even if you accidentally "double-encode" a content
    wrapper "html" using escapeAs(, 'html'), or ftl ?html, or screen
    auto-escaping, the issue might not be visible.
    BEWARE: the other encoders do not have true sanitization and will still
    produce double-encoding in this situation!!
    
    Refs https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/203

--- applications/content/src/org/ofbiz/content/content/ContentLangUtil.java
+++ applications/content/src/org/ofbiz/content/content/ContentLangUtil.java
@@ -10,28 +10,48 @@ import org.ofbiz.base.util.UtilCodec.SimpleEncoder;
 public class ContentLangUtil {
     
     /**
+     * SCIPIO: Returns a content wrapper sanitizer for the given language, or
+     * the "raw" encoder (no encoding) if null or not recognized.
+     * <p>
+     * <strong>UPDATE (2018-06-11):</strong> This method now returns a sanitizer
+     * exactly as requested, i.e., HtmlEncoder for "html". Prior to 2018-06-11
+     * this had been intentionally returning "raw" encoder for "html" and others (except "url")
+     * as a backward-compatibility measure but there should be no code left in stock Scipio
+     * for which this should cause any double-escaping.
+     * <p>
+     * <string>NOTE:</strong> In Scipio, the ContentWrapper get methods do not
+     * return a StringWrapper, so if you request an "html" encoder, usually you
+     * need to use <code>rawString(productContentWrapper.get(xxx, "html"))</code> 
+     * around the call.
+     * <p>
+     * Should be used to implement {@link ContentWrapper#get(String, String)}.
+     */
+    public static SimpleEncoder getContentWrapperSanitizer(String lang) {
+        SimpleEncoder encoder = UtilCodec.getEncoder(lang);
+        return (encoder != null) ? encoder : UtilCodec.getRawEncoder();
+        // Old pre-2018-08-11 code:
+        //if ("url".equals(lang)) {
+        //    return UtilCodec.getUrlEncoder();
+        //} else {
+        //    return UtilCodec.getRawEncoder();
+        //}
+    }
+    
+    /**
      * SCIPIO: Returns a content early sanitizer for the given language.
      * <p>
      * Should be used to implement {@link ContentWrapper#get(String, String)}.
      * <p>
-     * NOTE: 2016-10-14: Currently the only language which <em>may</em> receive sanitization is
+     * NOTE: 2016-10-14: Currently the only language which <em>may</em> receive early sanitization is
      * "url". The URL <em>may</em> receive URL encoding of its parameters (only).
      * TODO?: Said URL parameter escaping is not currently implemented (is not urgent as this is
-     * used to process values fresh out of database using {@link ContentWrapper}).
+     * used to process values fresh out of database.
      * <p>
      * All others such as HTML are left as-is. HTML gets automatically escaped
      * by the screen renderer auto-escaping when returned from the {@link ContentWrapper#get(String, String)}
      * method in a freemarker template or across the data prep boundary (since it no longer returns a StringWrapper) 
      * during freemarker/screen rendering.
      */
-    public static SimpleEncoder getContentWrapperSanitizer(String lang) {
-        if ("url".equals(lang)) {
-            return UtilCodec.getUrlEncoder();
-        } else {
-            return UtilCodec.getRawEncoder();
-        }
-    }
-    
     public static SimpleEncoder getEarlySanitizer(String lang) {
         if ("url".equals(lang)) {
             return UtilCodec.getUrlEncoder();

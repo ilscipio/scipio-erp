2d20e6ad4ab8cdd96e3e5fb19c5d10d657c95c46
Pascal Proulx <pplx@ilscipio.com>
2018-08-09 11:12:57 -0400

    WebSiteProperties: remove needless methods
    Caching is now done by FullWebappInfo instead.

--- framework/webapp/src/org/ofbiz/webapp/website/WebSiteProperties.java
+++ framework/webapp/src/org/ofbiz/webapp/website/WebSiteProperties.java
@@ -26,7 +26,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.ofbiz.base.lang.ThreadSafe;
 import org.ofbiz.base.start.Start;
 import org.ofbiz.base.util.Assert;
-import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilProperties;
 import org.ofbiz.entity.Delegator;
@@ -50,21 +49,6 @@ public final class WebSiteProperties {
      */
     public static WebSiteProperties defaults(Delegator delegator) {
         return new WebSiteProperties(delegator);
-    }
-
-    /**
-     * SCIPIO: Returns a <code>WebSiteProperties</code> instance initialized to the settings found
-     * in the <code>url.properties</code> file, with caching in the given map.
-     * Added 2018-07-31.
-     */
-    public static WebSiteProperties defaults(Delegator delegator, Map<String, WebSiteProperties> cache) {
-        if (cache == null) return defaults(delegator);
-        WebSiteProperties webSiteProps = cache.get("_defaults_");
-        if (webSiteProps == null) {
-            webSiteProps = new WebSiteProperties(delegator);
-            cache.put("_defaults_", webSiteProps);
-        }
-        return webSiteProps;
     }
 
     /**
@@ -306,23 +290,6 @@ public final class WebSiteProperties {
         } else {
             throw new GenericEntityException("Scipio: Could not find WebSite for webSiteId '" + webSiteId + "'");
         }
-    }
-    
-    /**
-     * SCIPIO: Returns a <code>WebSiteProperties</code> instance initialized to the settings found
-     * in the WebSite entity value for the given webSiteId, using the given cache.
-     * 
-     * @param delegator
-     * @param webSiteId
-     */
-    public static WebSiteProperties from(Delegator delegator, String webSiteId, Map<String, WebSiteProperties> cache) throws GenericEntityException {
-        if (cache == null) return from(delegator, webSiteId);
-        WebSiteProperties webSiteProps = cache.get(webSiteId);
-        if (webSiteProps == null) {
-            webSiteProps = from(delegator, webSiteId);
-            cache.put(webSiteId, webSiteProps);
-        }
-        return webSiteProps;
     }
 
     private final String httpPort;

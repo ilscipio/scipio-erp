201f8c3c14d6700979106619fab99581c8b4d274
Pascal Proulx <pplx@ilscipio.com>
2018-08-20 16:19:27 -0400

    ofbiz merge: 79250fad0 from trunk (DelegatorFactoryImpl exception)
    1 commit (79250fad062637933f8fe7f198f3710a4ae9c27d):
    "Improved error message logged by DelegatorFactoryImpl.getInstance(...)
    when a delegator can't be created; added a TODO comment to remind that
    the code should be refactored to throw the exception instead"
    from https://github.com/apache/ofbiz-framework.git trunk
    -e 
    Refs https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/626

--- framework/entity/src/org/ofbiz/entity/DelegatorFactoryImpl.java
+++ framework/entity/src/org/ofbiz/entity/DelegatorFactoryImpl.java
@@ -26,13 +26,14 @@ public class DelegatorFactoryImpl extends DelegatorFactory {
 
     private static final Debug.OfbizLogger module = Debug.getOfbizLogger(java.lang.invoke.MethodHandles.lookup().lookupClass());
 
+    // TODO: this method should propagate the GenericEntityException
     public Delegator getInstance(String delegatorName) {
         if (Debug.infoOn()) Debug.logInfo("Creating new delegator [" + delegatorName + "] (" + Thread.currentThread().getName() + ")", module);
         //Debug.logInfo(new Exception(), "Showing stack where new delegator is being created...", module);
         try {
             return new GenericDelegator(delegatorName);
         } catch (GenericEntityException e) {
-            Debug.logError(e, "Error creating delegator", module);
+            Debug.logError(e, "Error creating delegator: " + e.getMessage(), module);
             return null;
         }
     }

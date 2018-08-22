452f464ad1ad0801248e6de4e2c27912d04ac797
Pascal Proulx <pplx@ilscipio.com>
2018-08-22 12:11:44 -0400

    common: qrcode (scipio): fix tabs in files
    Refs https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/627

--- framework/common/src/com/ilscipio/scipio/common/qrcode/QRCodeServlet.java
+++ framework/common/src/com/ilscipio/scipio/common/qrcode/QRCodeServlet.java
@@ -25,50 +25,50 @@ import net.glxn.qrgen.javase.QRCode;
  */
 public class QRCodeServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
-	private static final String QR_LOGO_PATH = "component://base-theme/webapp/base/images/scipio-logo.png";
+    private static final long serialVersionUID = 1L;
+    private static final String QR_LOGO_PATH = "component://base-theme/webapp/base/images/scipio-logo.png";
 
-	
-	/** 
-	 * QRCode Servlet
-	 * Renders QRCode based on GET-Parameters; uses zxing & qrgen
-	 * 
-	 * @param text	String or url-encoded URL
-	 * @param hasLogo Renders an overlay on top of the QRCode
+
+    /**
+    * QRCode Servlet
+    * Renders QRCode based on GET-Parameters; uses zxing & qrgen
+    *
+    * @param text    String or url-encoded URL
+    * @param hasLogo Renders an overlay on top of the QRCode
      * @param logo location of logo image in component:// notation (defaults to base scipio erp logo)
-	 * @param width QRCode width
-	 * @param height QRCode height
-	 * @param isVCard Currently not supported; renders vcard instead of simple string
-	 *  
-	 * */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		Boolean hasLogo = request.getParameter("hasLogo") != null ? Boolean.valueOf(request.getParameter("hasLogo")) : true;
-		String logo = request.getParameter("logo") != null ? request.getParameter("logo") : QR_LOGO_PATH;
-		Boolean isVCard = request.getParameter("isVCard") != null ? Boolean.valueOf(request.getParameter("isVCard")) : false; // currently not implemented
-		String urlParam = request.getParameter("url");
-		int width = request.getParameter("width") != null ? Integer.valueOf(request.getParameter("width")) : 250;
-		int height = request.getParameter("height") != null ? Integer.valueOf(request.getParameter("height")) : 250;
-		String text = null;
-		if(UtilValidate.isNotEmpty(urlParam)){
-			text = URLDecoder.decode(urlParam,"UTF-8");		
-		}
-		if(isVCard){
-			// Validate if this is a VCard
-		}
+    * @param width QRCode width
+    * @param height QRCode height
+    * @param isVCard Currently not supported; renders vcard instead of simple string
+    *
+    * */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Boolean hasLogo = request.getParameter("hasLogo") != null ? Boolean.valueOf(request.getParameter("hasLogo")) : true;
+        String logo = request.getParameter("logo") != null ? request.getParameter("logo") : QR_LOGO_PATH;
+        Boolean isVCard = request.getParameter("isVCard") != null ? Boolean.valueOf(request.getParameter("isVCard")) : false; // currently not implemented
+        String urlParam = request.getParameter("url");
+        int width = request.getParameter("width") != null ? Integer.valueOf(request.getParameter("width")) : 250;
+        int height = request.getParameter("height") != null ? Integer.valueOf(request.getParameter("height")) : 250;
+        String text = null;
+        if(UtilValidate.isNotEmpty(urlParam)){
+            text = URLDecoder.decode(urlParam,"UTF-8");
+        }
+        if(isVCard){
+            // Validate if this is a VCard
+        }
 
-		// Print response
-		response.setContentType("image/jpeg");
-		QRCode qrCode = QRCode.from(text).withSize(width, height).to(ImageType.PNG);
-		//qrCode.withColor(0x3d4a5d, 0xffffff);
-		qrCode.withHint(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
-		File f = qrCode.file();
-		if(hasLogo){
-			f = QRCodeUtil.drawLogo(qrCode,logo);			
-		}
-		BufferedImage bi = ImageIO.read(f);
-		OutputStream out = response.getOutputStream();
-		ImageIO.write(bi, "png", out);
-		out.close();
-	}
+        // Print response
+        response.setContentType("image/jpeg");
+        QRCode qrCode = QRCode.from(text).withSize(width, height).to(ImageType.PNG);
+        //qrCode.withColor(0x3d4a5d, 0xffffff);
+        qrCode.withHint(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        File f = qrCode.file();
+        if(hasLogo){
+            f = QRCodeUtil.drawLogo(qrCode,logo);
+        }
+        BufferedImage bi = ImageIO.read(f);
+        OutputStream out = response.getOutputStream();
+        ImageIO.write(bi, "png", out);
+        out.close();
+    }
 
 }

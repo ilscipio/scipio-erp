948f71ee2e2fb0a17848b08da484b729889fcf6c
Pascal Proulx <pplx@ilscipio.com>
2018-09-11 14:28:57 -0400

    upstream merge: manual: content: add ftp/sftp client services
    Manual merge of new services and code from upstream revision e7777d1346:
    * ftp/sftp client services, for future use (no UI integration merged at
    this time)
    
    Upstream-Version-Commit: e7777d13468236ae3f3f8af43d2fcfa56c019700
    Upstream-Repo: https://github.com/apache/ofbiz-framework.git
    Upstream-Branch: trunk
    
    Refs https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/556

---
+++ applications/content/src/org/ofbiz/content/ftp/SecureFtpClient.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+package org.ofbiz.content.ftp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class SecureFtpClient implements FtpClientInterface {
+
+    public static final String module = SecureFtpClient.class.getName();
+
+    /**
+     * TODO : to implements
+     */
+    @Override
+    public void connect(String hostname, String username, String password, Long port, Long timeout) throws IOException {
+
+    }
+
+    @Override
+    public void copy(String path, String fileName, InputStream file) throws IOException {
+
+    }
+
+    @Override
+    public List<String> list(String path) throws IOException {
+        return null;
+    }
+
+    @Override
+    public void setBinaryTransfer(boolean isBinary) throws IOException {
+
+    }
+
+    @Override
+    public void setPassiveMode(boolean isPassive) throws IOException {
+
+    }
+
+    @Override
+    public void closeConnection() {
+
+    }
+}

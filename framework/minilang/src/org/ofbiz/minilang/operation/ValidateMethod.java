e3a01af3a56c324917c2239726b4c94abc57e2e5
Pascal Proulx <pplx@ilscipio.com>
2018-09-11 14:39:02 -0400

    stock patch: minilang: ValidateMethod.java: unhardcode bad package name

--- framework/minilang/src/org/ofbiz/minilang/operation/ValidateMethod.java
+++ framework/minilang/src/org/ofbiz/minilang/operation/ValidateMethod.java
@@ -26,6 +26,7 @@ import java.util.Map;
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.GeneralException;
 import org.ofbiz.base.util.ObjectType;
+import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.minilang.MiniLangValidate;
 import org.w3c.dom.Element;
 
@@ -42,7 +43,7 @@ public class ValidateMethod extends SimpleMapOperation {
     public ValidateMethod(Element element, SimpleMapProcess simpleMapProcess) {
         super(element, simpleMapProcess);
         this.methodName = element.getAttribute("method");
-        this.className = MiniLangValidate.checkAttribute(element.getAttribute("class"), "org.apache.ofbiz.base.util.UtilValidate");
+        this.className = MiniLangValidate.checkAttribute(element.getAttribute("class"), UtilValidate.class.getName());
     }
 
     @Override

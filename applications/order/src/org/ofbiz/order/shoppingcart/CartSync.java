f66d38b80440715db90bd8bb7d20ec760ed01b5d
Pascal Proulx <pplx@ilscipio.com>
2018-11-29 14:15:05 -0500

    ShoppingCart: CartUpdate, CartSync: improved cart update/sync pattern
    * simplifies CartUpdate so no need for manual ugly synchronized block,
    will simplify all code
    * logging improvements: now tries to print caller
    * removed sectionName, no longer needed since better logging
    * doc
    * various fixes
    
    Refs https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/757

---
+++ applications/order/src/org/ofbiz/order/shoppingcart/CartSync.java
@@ -0,0 +1,172 @@
+package org.ofbiz.order.shoppingcart;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
+
+/**
+ * SCIPIO: Cart synchronization object; replaces synchronized blocks on a cart lock.
+ * <p>
+ * Use this for synchronizing cart-related operations that do not actually modify the cart itself.
+ * If the operation modifies the cart, use {@link CartUpdate#updateSection(HttpServletRequest)} instead.
+ * <p>
+ * Basic usage:
+ * <pre>{@code
+ * try (CartSync cartSync = CartSync.synchronizedSection(request)) {
+ *    // ...
+ * }</pre>
+ * <p>
+ * NOTE: If used from pre-java 8 code, must be closed in a finally block. 
+ * <p>
+ * Added 2018-11-29.
+ *
+ * @see CartUpdate
+ */
+@SuppressWarnings("serial")
+public class CartSync implements AutoCloseable, Serializable {
+    private static final Debug.OfbizLogger module = Debug.getOfbizLogger(java.lang.invoke.MethodHandles.lookup().lookupClass());
+
+    /**
+     * Controls whether the CartSync is cloned every section or not.
+     * <p>
+     * It is not technically necessary if all calling code is correct, but because it's possible
+     * for caller to accidentally forget to call {@link #clone()}, we need this to detect those cases
+     * in {@link #finalize()}.
+     */
+    private static final boolean oneInstancePerSection = true;
+    private static final Collection<String> excludeClassesLogCaller = UtilMisc.toSet(CartSync.class.getName(), CartUpdate.class.getName());
+
+    private final ReentrantLock cartLock;
+    // NOTE: This flag may evaluate only once per session (roughly), which should be OK
+    protected final boolean debug = (ShoppingCart.DEBUG || Debug.verboseOn());
+
+    // Following fields are only used if oneInstancePerSection==true
+    private boolean endCalledOrOk = false;
+
+    /**
+     * Constructor for main lock instance (stored in session).
+     */
+    private CartSync(ReentrantLock cartUpdateLock) {
+        this.cartLock = cartUpdateLock;
+        endCalledOrOk = true;
+    }
+
+    /**
+     * Lock-sharing copy constructor.
+     */
+    private CartSync(CartSync other) {
+        this.cartLock = other.cartLock;
+        endCalledOrOk = false;
+    }
+
+    /**
+     * Returns a cart synchronization section. This takes the place of
+     * a synchronized block and should be enclosed in a try-with-resources block.
+     */
+    public static CartSync synchronizedSection(HttpServletRequest request) {
+        CartSync cartSync = getCartLockObject(request);
+        // NOTE: we only need a local copy if verifying with finalize
+        if (oneInstancePerSection) {
+            cartSync = new CartSync(cartSync);
+        }
+        cartSync.begin();
+        return cartSync;
+    }
+
+    protected void begin() {
+        if (isDebug()) { // Usually not necessary to log this; covered by CartUpdate
+            Debug.logInfo("Begin cart sync section" + getLogSuffix(), module);
+        }
+        cartLock.lock();
+    }
+
+    protected void end() {
+        if (isDebug()) {
+            Debug.logInfo("End cart sync section" + getLogSuffix(), module);
+        }
+        try {
+            cartLock.unlock();
+        } catch(Exception e) {
+            Debug.logError(e, "CartSync: fatal: could not release cart lock - this should not happen" 
+                    + getLogSuffixDetailed(), module);
+        }
+        endCalledOrOk = true;
+    }
+    
+    @Override
+    public void close() {
+        end();
+    }
+
+    public static CartSync create() {
+        return new CartSync(new ReentrantLock());
+    }
+
+    /** SCIPIO: Returns an object which should be used to lock on whether modifying the cart.
+     * Added 2018-11-20. */
+    protected static CartSync getCartLockObject(HttpServletRequest request) {
+        HttpSession session = request.getSession(true);
+        CartSync lock = (CartSync) session.getAttribute("shoppingCartLock");
+        if (lock == null) {
+            // NOTE: Synchronizing on session is not officially supported by servlets, but it can't hurt here
+            synchronized(session) {
+                lock = (CartSync) session.getAttribute("shoppingCartLock");
+                if (lock == null) {
+                    // Check if cart has it but for some reason it's not in session
+                    ShoppingCart cart = (ShoppingCart) request.getSession(true).getAttribute("shoppingCart");
+                    if (cart != null) {
+                        lock = cart.getLockObject();
+                        request.getSession(true).setAttribute("shoppingCartLock", lock);
+                        Debug.logInfo("shoppingCartLock not found in session; setting to lock object found in cart (" 
+                                + lock + ")", module); 
+                    } else {
+                        lock = ShoppingCart.createLockObject();
+                        request.getSession(true).setAttribute("shoppingCartLock", lock);
+                        // NOTE: In theory this should be a warning (because lock on session is not officially supported)
+                        // , but it will happen regularly in orderentry
+                        // and extremely unlikely to be a problem
+                        Debug.logInfo("shoppingCartLock not found in session; creating", module); 
+                    }
+                }
+            }
+        }
+        return lock;
+    }
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    String getLogSuffix() {
+        if (isDebug()) {
+            return getLogSuffixDetailed();
+        } else {
+            return " (thread " + Thread.currentThread().getId() + ")";
+        }
+    }
+
+    String getLogSuffixDetailed() {
+        return " (" + Debug.getCallerShortInfo(excludeClassesLogCaller) + "; thread " + Thread.currentThread().getId() + ")";
+    }
+    
+    @Override
+    protected void finalize() throws Throwable {
+        if (!endCalledOrOk) {
+            Debug.logError("CartSync: finalize: fatal: cart sync section was never closed - this should"
+                    + " not happen - close() must always be called, usually in a try/finally or try-with-resources block"
+                    + getLogSuffixDetailed(), module);
+            try {
+                cartLock.unlock();
+            } catch(Exception e) {
+                Debug.logError(e, "CartSync: finalize: fatal: could not release cart lock; session may be ruined" 
+                        + getLogSuffixDetailed(), module);
+            }
+        }
+    }
+}
--- applications/order/src/org/ofbiz/order/shoppingcart/CartUpdate.java
+++ applications/order/src/org/ofbiz/order/shoppingcart/CartUpdate.java
@@ -10,78 +10,110 @@ import org.ofbiz.base.util.UtilProperties;
 /**
  * SCIPIO: Helper object to manage atomic, synchronized cart updates.
  * <p>
- * Uses auto-closeable to help manage nested updates;
- * prevents nested updates from actually updating the session.
+ * Automatically synchronizes updates on a shared object (using a ReentrantLock instead
+ * of a synchronized block) when {@link #updateSection(HttpServletRequest)} is called
+ * and releases the lock when {@link #close()} is called. Automatically manages
+ * nested update sections.
+ * <strong>WARNING:</strong> If used in pre-java8 code, {@link #close()} must be called
+ * from a <code>finally</code> block.
  * <p>
- * The basic usage is:
+ * Inside the section, simply call {@link #getCartForUpdate()} to get a safe, modifiable cart instance.
+ * The modifiable cart instance is a copy of the (immutable) session cart.
+ * Once changes are made, call {@link #commit(ShoppingCart)} to commit the changes, or don't
+ * call it to discard the changes in case of error.
+ * <p>
+ * Basic usage:
  * <pre>{@code
- * try (CartUpdate cartUpdate = new CartUpdate(request)) { // SCIPIO
- *    synchronized (cartUpdate.getLockObject()) {
- *        ShoppingCart cart = cartUpdate.getCartForUpdate();
- *        
- *        // modify cart...
- *        
- *        cartUpdate.commit(cart);
- *    }
- * }
+ * try (CartSync cartUpdate = CartUpdate.updateSection(request)) { // SCIPIO
+ *    ShoppingCart cart = cartUpdate.getCartForUpdate(); 
+ *    // modify cart...
+ *    cartUpdate.commit(cart);
  * }</pre>
  * <p>
- * NOTE: It is extremely important that the {@link #getCartForUpdate()} call occurs within
- * the synchronized block, because the cart reference is subject to change outside the synchronized block.
+ * NOTE: If you need to synchronize on the cart lock without actually updating the cart itself,
+ * use {@link CartSync#synchronizedSection(HttpServletRequest)} instead.
  * <p>
- * If {@link #commit(ShoppingCart)} is never called, the cart changes will be discarded.
+ * TODO: REVIEW: Currently this does NOT try to set the request attribute "shoppingCart" to the modifiable
+ * cart at the beginning of the update section, because all known instances of this require caller modifications
+ * which are made to use {@link {@link #getCartForUpdate()}}
+ * instead. However, this has potential to cause problems for some nested code... it might be required...
  * <p>
  * Added 2018-11-20.
  *
- * @see ShoppingCartEvents
- * @see ShoppingCartEvents#getCartLockObject(HttpServletRequest)
+ * @see CartSync
  */
 public class CartUpdate implements AutoCloseable {
     private static final Debug.OfbizLogger module = Debug.getOfbizLogger(java.lang.invoke.MethodHandles.lookup().lookupClass());
 
-    private static final boolean CART_COPIES_ENABLED = UtilProperties.getPropertyAsBoolean("order", "shoppingcart.update.useCartCopies", true);
+    private static final boolean CART_COPIES_ENABLED = UtilProperties.getPropertyAsBoolean("order",
+            "shoppingcart.update.useCartCopies", true);
     
+    private static final ThreadLocal<CartUpdateStatus> currentStatus = new ThreadLocal<>();
+
     private final HttpServletRequest request;
     private final CartUpdateStatus status;
     private boolean localCommitCalled = false;
-    private final String logSuffix;
     
-    public CartUpdate(HttpServletRequest request, String sectionName) {
+    private CartUpdate(HttpServletRequest request, CartUpdateStatus status) {
         this.request = request;
-        // DEV NOTE: could use a ThreadLocal in place of req attr, but this less likely to cause leaks in case of errors
-        CartUpdateStatus status = getCartUpdateStatus(request);
+        this.status = status;
+    }
+
+    /**
+     * Returns a full cart update section object. Must be closed using {@link #close()}, preferably
+     * using try-with-resources. Automatically locks.
+     */
+    public static CartUpdate updateSection(HttpServletRequest request) {
+        CartUpdateStatus status = currentStatus.get();
         if (status == null) {
-            status = new CartUpdateStatus(1, null);
-            request.setAttribute("cartUpdateStatus", status);
+            // Top level
+            status = new CartUpdateStatus(1, null, CartSync.getCartLockObject(request));
+        }
+        CartUpdate cartUpdate = new CartUpdate(request, status);
+        cartUpdate.begin();
+        return cartUpdate;
+    }
+
+    
+    protected void begin() {
+        if (isDebug()) {
+            Debug.logInfo("Begin cart update section (depth: " + status.nestedLevel + ")" 
+                    + getLogSuffix(), module);
+        }
+        if (isTopLevel()) {
+            currentStatus.set(status);
+            status.cartSync.begin();
         } else {
             status.nestedLevel++;
         }
-        this.status = status;
-        this.logSuffix = (sectionName != null) ? " (" + sectionName  + ")" : "";
-        if (status.debug) {
-            Debug.logInfo("Begin cart update section (depth: " + status.nestedLevel + ")" + getLogSuffix(), module);
+    }
+
+    protected void end() {
+        if (isTopLevel()) {
+            status.cartSync.end();
+            currentStatus.remove();
+        } else {
+            status.nestedLevel--;
+        }
+        if (isDebug()) {
+            if (localCommitCalled) {
+                Debug.logInfo("End cart update section (depth: " + status.nestedLevel + ")" 
+                        + getLogSuffix(), module);
+            } else {
+                Debug.logWarning("End cart update section (depth: " + status.nestedLevel 
+                        + ") - commit never invoked" + getLogSuffix(), module);
+            }
         }
     }
 
-    public CartUpdate(HttpServletRequest request) {
-        this(request, null);
+    @Override
+    public void close() {
+        end();
     }
-
-    /**
-     * A lock object the caller MUST synchronize on inside the try/catch block.
-     */
-    public Object getLockObject() {
-        return ShoppingCartEvents.getCartLockObject(request);
-    }
-
+    
     /**
      * Returns a modifiable cart instance derived from the current (request/session) cart, 
      * which may be committed if changes successful (using {@link #commit(ShoppingCart)}).
-     * <p>
-     * NOTE: This also acts as a "begin" section flag, at start of synchronized block.
-     * <p>
-     * WARN: This must ONLY be called inside a synchronized block that synchronizes
-     * on the object returned by {@link #getLockObject()}.
      */
     public ShoppingCart getCartForUpdate() {
         if (status.cartForUpdate == null) {
@@ -102,14 +134,15 @@ public class CartUpdate implements AutoCloseable {
             return cart;
         }
         ShoppingCart newCart = cart.exactCopy();
-        if (status.debug) {
+        if (isDebug()) {
             try {
                 newCart.ensureExactEquals(cart);
                 Debug.logInfo("Cloned cart " + getLogCartDesc(cart) + " to " + getLogCartDesc(newCart)
                 + " for update" + getLogSuffix(), module);
             } catch(IllegalStateException e) {
                 Debug.logError(e, "Cloned cart " + getLogCartDesc(cart) + " to " + getLogCartDesc(newCart)
-                + " for update, but differences encountered - please report this issue" + getLogSuffix(), module);
+                    + " for update, but differences encountered - please report this issue" 
+                    + getLogSuffixDetailed(), module);
             }
         }
         return newCart;
@@ -119,36 +152,20 @@ public class CartUpdate implements AutoCloseable {
      * Commits the cart changes (if top level).
      */
     public ShoppingCart commit(ShoppingCart cart) {
-        if (status.isTopLevel()) {
-            if (status.debug) {
-                Debug.logInfo("Committing modified shopping cart " + getLogCartDesc(cart) + getLogSuffix(), module);
+        if (isTopLevel()) {
+            if (isDebug()) {
+                Debug.logInfo("Committing modified shopping cart " + getLogCartDesc(cart)
+                    + getLogSuffix(), module);
             }
             ShoppingCartEvents.replaceCurrentCartObject(request, cart, true);
             status.committed = true;
         } else {
-            if (status.debug) {
+            if (isDebug()) {
                 Debug.logInfo("Delaying shopping cart commit (nested)" + getLogSuffix(), module);
             }
         }
         localCommitCalled = true;
         return cart;
-    }
-
-    @Override
-    public void close() {
-        if (status.isTopLevel()) {
-            request.removeAttribute("cartUpdateStatus");
-        } else {
-            status.nestedLevel--;
-        }
-        if (ShoppingCart.DEBUG || Debug.verboseOn()) {
-            if (localCommitCalled) {
-                Debug.logInfo("End cart update section (depth: " + status.nestedLevel + ")" + getLogSuffix(), module);
-            } else {
-                Debug.logWarning("End cart update section (depth: " + status.nestedLevel 
-                        + ") - commit never invoked" + getLogSuffix(), module);
-            }
-        }
     }
 
     /**
@@ -163,56 +180,62 @@ public class CartUpdate implements AutoCloseable {
      * Returns true if this OR a nested CartUpdate section committed and switched out the cart already.
      */
     public boolean isCommitted() {
-        return status.isCommitted();
+        return status.committed;
     }
 
     public boolean isDebug() {
         return status.debug;
     }
+ 
+    public int getNestedLevel() {
+        return status.nestedLevel;
+    }
+
+    public boolean isTopLevel() {
+        return (status.nestedLevel <= 1);
+    }
     
+    /**
+     * Returns the CartSync synchronization object in use by this update section.
+     * <p>
+     * NOTE: There is no need to manually lock or synchronize on this anymore; 
+     * locked automatically by {@link #updateSection(HttpServletRequest)} and unlocked
+     * by {@link #close()}.
+     */
+    public CartSync getLockObject() {
+        return status.cartSync;
+    }
+
     private String getLogCartDesc(ShoppingCart cart) {
         return "@" + Integer.toHexString(System.identityHashCode(cart));
     }
-    
+
     private String getLogSuffix() {
-        return logSuffix + "; threadId: " + Thread.currentThread().getId();
+        return status.cartSync.getLogSuffix();
     }
 
-    
-    public static CartUpdateStatus getCartUpdateStatus(HttpServletRequest request) {
-        return (CartUpdateStatus) request.getAttribute("cartUpdateStatus");
+    private String getLogSuffixDetailed() {
+        return status.cartSync.getLogSuffixDetailed();
     }
 
-    /**
-     * Stored in request attribute cartUpdateStatus and records nesting level.
-     * Also lowers need for attributes.
-     */
     @SuppressWarnings("serial")
-    public static class CartUpdateStatus implements Serializable {
+    private static class CartUpdateStatus implements Serializable {
         private int nestedLevel = 1;
         private ShoppingCart cartForUpdate;
         private final boolean debug = (ShoppingCart.DEBUG || Debug.verboseOn());
         private boolean committed = false;
-        
-        protected CartUpdateStatus(int nestedLevel, ShoppingCart cartForUpdate) {
+        /**
+         * The cart synchronization provider (emulates synchronized block).
+         * <p>
+         * NOTE: Even if there are nested CartUpdate instances/calls, we only need
+         * one CartSync for the entire thing.
+         */
+        private final CartSync cartSync;
+
+        CartUpdateStatus(int nestedLevel, ShoppingCart cartForUpdate, CartSync cartSync) {
             this.nestedLevel = nestedLevel;
             this.cartForUpdate = cartForUpdate;
-        }
-
-        public int getNestedLevel() {
-            return nestedLevel;
-        }
-
-        public ShoppingCart getCartForUpdate() {
-            return cartForUpdate;
-        }
-        
-        public boolean isTopLevel() {
-            return (nestedLevel <= 1);
-        }
-
-        public boolean isCommitted() {
-            return committed;
+            this.cartSync = cartSync;
         }
     }
 }

85f7c922ea32eeb767ff2265f19bec1731e892fe
Pascal Proulx <pplx@ilscipio.com>
2018-12-06 14:30:30 -0500

    webapp: EventUtil: new helper class
    defines all the standard event request attributes.
    
    Refs https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/593

---
+++ framework/webapp/src/org/ofbiz/webapp/event/EventUtil.java
@@ -0,0 +1,66 @@
+package org.ofbiz.webapp.event;
+
+import java.util.Set;
+
+import org.ofbiz.base.util.UtilMisc;
+
+/**
+ * SCIPIO: Event utilities and definitions.
+ */
+public final class EventUtil {
+
+    public static final String ERROR_MESSAGE_LIST = "_ERROR_MESSAGE_LIST_";
+    public static final String ERROR_MESSAGE_MAP = "_ERROR_MESSAGE_MAP_";
+    public static final String ERROR_MESSAGE = "_ERROR_MESSAGE_";
+    public static final String EVENT_MESSAGE_LIST = "_EVENT_MESSAGE_LIST_";
+    public static final String EVENT_MESSAGE = "_EVENT_MESSAGE_";
+
+    private static final Set<String> eventMsgAttrNames = UtilMisc.unmodifiableHashSet(
+            EVENT_MESSAGE_LIST, EVENT_MESSAGE);
+    
+    private static final Set<String> errorMsgAttrNames = UtilMisc.unmodifiableHashSet(
+            ERROR_MESSAGE_LIST, ERROR_MESSAGE_MAP, ERROR_MESSAGE);
+    
+    private static final Set<String> eventErrorMsgAttrNames = UtilMisc.unmodifiableHashSet(
+            ERROR_MESSAGE_LIST, ERROR_MESSAGE_MAP, ERROR_MESSAGE,
+            EVENT_MESSAGE_LIST, EVENT_MESSAGE);
+
+    private EventUtil() {
+    }
+
+    /**
+     * Returns the standard event regular (success) AND error message attribute names:
+     *  _ERROR_MESSAGE_LIST_, _EVENT_MESSAGE_LIST_, etc.
+     */
+    public static Set<String> getEventErrorMsgAttrNames() {
+        return eventErrorMsgAttrNames;
+    }
+    
+    public static boolean isEventErrorMsgAttrName(String attributeName) {
+        return eventErrorMsgAttrNames.contains(attributeName);
+    }
+    
+    /**
+     * Returns the standard event message attribute names:
+     *  _EVENT_MESSAGE_LIST_, etc.
+     */
+    public static Set<String> getEventMsgAttrNames() {
+        return eventMsgAttrNames;
+    }
+
+    public static boolean isEventMsgAttrName(String attributeName) {
+        return eventMsgAttrNames.contains(attributeName);
+    }
+    
+    /**
+     * Returns the standard error message attribute names:
+     *  _ERROR_MESSAGE_LIST_, etc.
+     */
+    public static Set<String> getErrorMsgAttrNames() {
+        return errorMsgAttrNames;
+    }
+    
+    public static boolean isErrorMsgAttrName(String attributeName) {
+        return errorMsgAttrNames.contains(attributeName);
+    }
+}

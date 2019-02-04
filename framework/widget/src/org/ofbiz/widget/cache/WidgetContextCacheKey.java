06a4940467ba794159bbe0cc66eb4a34c4d4e320
Pascal Proulx <pplx@ilscipio.com>
2019-02-04 18:03:57 -0500

    stock patch: widget: remove deprecated "page" field code
    NOTE: A few instances like ScreenRenderer.java and
    WidgetContextCacheKey still keep the variable just in case,
    to prevent issues, but it is fully deprecated.

--- framework/widget/src/org/ofbiz/widget/cache/WidgetContextCacheKey.java
+++ framework/widget/src/org/ofbiz/widget/cache/WidgetContextCacheKey.java
@@ -54,7 +54,7 @@ public final class WidgetContextCacheKey {
         fieldNamesToSkip.add("requestAttributes");
         fieldNamesToSkip.add("JspTaglibs");
         fieldNamesToSkip.add("requestParameters");
-        fieldNamesToSkip.add("page");
+        fieldNamesToSkip.add("page"); // SCIPIO: DEPRECATED (TODO: REMOVE IN FUTURE, keeping just in case of issues for now...)
         fieldNamesToSkip.add("controlPath");
         fieldNamesToSkip.add("contextRoot");
         fieldNamesToSkip.add("serverRoot");

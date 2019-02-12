60efa0adadd5b82e4f28079d52c953a150b0eae5
Pascal Proulx <pplx@ilscipio.com>
2019-02-12 17:39:22 -0500

    webapp, cms: warning for widgetContextScripts (no TX)
    When these render context population scripts run, they may not be
    wrapped in any database transaction; therefore they must be careful and
    generally only query cached/cacheable data (entity cache).
    
    Refs https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/911

--- applications/cms/script/com/ilscipio/scipio/cms/content/CmsContentProcessor.groovy
+++ applications/cms/script/com/ilscipio/scipio/cms/content/CmsContentProcessor.groovy
@@ -1,8 +1,14 @@
 /**
  * Scipio CMS Content processor placeholder - can be used to put Variable content into
  * the context of the CMS Editor.
+ * <p>
  * NOTE: 2017: the CMS-specific processor script is no longer of major importance in Scipio,
  * because Scipio supports custom system-wide and webapp-specific global scripts.
  * It is preferable to reuse those mechanisms, unless the script is truly CMS-specific.
+ * <p>
+ * WARNING: This script may not be running under any database transaction.
+ * If you use this script and must query the entity engine, it is recommended
+ * to query with the entity cache enabled only - any non-cached entity lookups
+ * may impose an extra performance cost.
  */
 

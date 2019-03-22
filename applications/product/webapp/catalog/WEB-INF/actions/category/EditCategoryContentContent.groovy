491669fd7b1caa6adbb94fe1d6dd715274904ecb
Jonatan Soto <jonatan.soto@ilscipio.com>
2019-03-22 13:40:18 +0100

    catalog: category: can't reproduce the original error. However, handling any possible error while parsing dates in a nicer way.
    https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/979

--- applications/product/webapp/catalog/WEB-INF/actions/category/EditCategoryContentContent.groovy
+++ applications/product/webapp/catalog/WEB-INF/actions/category/EditCategoryContentContent.groovy
@@ -21,47 +21,52 @@ import org.ofbiz.base.util.*
 import org.ofbiz.entity.*
 import org.ofbiz.entity.util.*
 
+final String module = "EditCategoryContentContent.groovy";
 uiLabelMap = UtilProperties.getResourceBundleMap("ProductUiLabels", locale);
 
 // Show update form
 // SCIPIO: invalid for groovy: requestAttributes.contentId
 if (!request.getAttribute("contentId") && (parameters.contentId && parameters.productCategoryId && parameters.prodCatContentTypeId && parameters.fromDate)) {
-    fromDate = UtilDateTime.stringToTimeStamp(parameters.fromDate, "yyyy-MM-dd HH:mm:ss.S", timeZone, locale)    
-    prodCatContentTypeId = parameters.prodCatContentTypeId;
-    productCategoryContent = from("ProductCategoryContent").
-            where(["contentId" : parameters.contentId, "productCategoryId" : parameters.productCategoryId, "prodCatContentTypeId" : parameters.prodCatContentTypeId, "fromDate" : fromDate]).queryOne();    
-    if (productCategoryContent) {
-        context.productCategoryContent = productCategoryContent;
-        context.contentFormName = "EditCategoryContentSimpleText";
-        context.contentFormAction = "updateSimpleTextContentForCategory";
-        if (("PAGE_TITLE".equals(prodCatContentTypeId))||("META_KEYWORD".equals(prodCatContentTypeId))||("META_DESCRIPTION".equals(prodCatContentTypeId))) {
-            context.contentFormName = "EditCategoryContentSEO";
-            context.contentFormAction = "updateContentSEOForCategory";
-        } else if ("RELATED_URL".equals(prodCatContentTypeId)) {
-            context.contentFormName = "EditCategoryContentRelatedUrl";
-            context.contentFormAction = "updateRelatedUrlContentForCategory";           
-        } else if ("VIDEO".equals(prodCatContentTypeId) || "CATEGORY_IMAGE".equals(prodCatContentTypeId)) {
-            context.contentFormName = "EditCategoryContentDownload";
-            context.contentFormAction = "updateDownloadContentForCategory";
-            if("CATEGORY_IMAGE".equals(prodCatContentTypeId)){
-                context.dataResourceTypeId = "IMAGE_OBJECT";
-            }else{
-                context.dataResourceTypeId = "VIDEO_OBJECT";
+    try {
+        fromDate = UtilDateTime.stringToTimeStamp(parameters.fromDate, "yyyy-MM-dd HH:mm:ss.S", timeZone, locale)    
+        prodCatContentTypeId = parameters.prodCatContentTypeId;
+        productCategoryContent = from("ProductCategoryContent").
+                where(["contentId" : parameters.contentId, "productCategoryId" : parameters.productCategoryId, "prodCatContentTypeId" : parameters.prodCatContentTypeId, "fromDate" : fromDate]).queryOne();    
+        if (productCategoryContent) {
+            context.productCategoryContent = productCategoryContent;
+            context.contentFormName = "EditCategoryContentSimpleText";
+            context.contentFormAction = "updateSimpleTextContentForCategory";
+            if (("PAGE_TITLE".equals(prodCatContentTypeId))||("META_KEYWORD".equals(prodCatContentTypeId))||("META_DESCRIPTION".equals(prodCatContentTypeId))) {
+                context.contentFormName = "EditCategoryContentSEO";
+                context.contentFormAction = "updateContentSEOForCategory";
+            } else if ("RELATED_URL".equals(prodCatContentTypeId)) {
+                context.contentFormName = "EditCategoryContentRelatedUrl";
+                context.contentFormAction = "updateRelatedUrlContentForCategory";           
+            } else if ("VIDEO".equals(prodCatContentTypeId) || "CATEGORY_IMAGE".equals(prodCatContentTypeId)) {
+                context.contentFormName = "EditCategoryContentDownload";
+                context.contentFormAction = "updateDownloadContentForCategory";
+                if("CATEGORY_IMAGE".equals(prodCatContentTypeId)){
+                    context.dataResourceTypeId = "IMAGE_OBJECT";
+                }else{
+                    context.dataResourceTypeId = "VIDEO_OBJECT";
+                }
             }
-        }
-        
-        if ("RELATED_URL".equals(prodCatContentTypeId) || "VIDEO".equals(prodCatContentTypeId) || "CATEGORY_IMAGE".equals(prodCatContentTypeId)) {
-            contentList = from("ContentDataResourceView").where("contentId", parameters.contentId).queryList();
-            if (contentList) {
-                context.contentDataResourceView = contentList.get(0);
+            
+            if ("RELATED_URL".equals(prodCatContentTypeId) || "VIDEO".equals(prodCatContentTypeId) || "CATEGORY_IMAGE".equals(prodCatContentTypeId)) {
+                contentList = from("ContentDataResourceView").where("contentId", parameters.contentId).queryList();
+                if (contentList) {
+                    context.contentDataResourceView = contentList.get(0);
+                }
             }
+            
+            content = productCategoryContent.getRelatedOne("Content", false);
+            context.content = content;
+            context.contentId = content?.contentId; // SCIPIO: needed for some forms
+            context.textDataMap = delegator.findOne("ElectronicText", ["dataResourceId" : content.dataResourceId], false);
+            context.prodCatContentTypeId = prodCatContentTypeId;
         }
-        
-        content = productCategoryContent.getRelatedOne("Content", false);
-        context.content = content;
-        context.contentId = content?.contentId; // SCIPIO: needed for some forms
-        context.textDataMap = delegator.findOne("ElectronicText", ["dataResourceId" : content.dataResourceId], false);
-        context.prodCatContentTypeId = prodCatContentTypeId;
+    } catch (Exception e) {
+        Debug.logError(e.getMessage(), module)
     }
     // Show create form
 } else if (!request.getAttribute("contentId")) { // SCIPIO: invalid for groovy: requestAttributes.contentId 

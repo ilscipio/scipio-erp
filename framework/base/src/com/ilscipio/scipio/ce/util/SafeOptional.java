98622ed78b0c26a9aac9e2212f42fab91fe91585
Pascal Proulx <pplx@ilscipio.com>
2019-04-04 22:15:26 -0400

    *.java: use SafeOptional instead of Optional (serial fix)
    Replaces java.util.Optional back with SafeOptional again due to
    java.util.Optional not implementing Serializable (shame) per java
    design, in all the cases where Optional was stored as a field or in
    a container. java.util.Optional is primarily meant for return values.
    
    Refs https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/1009

--- framework/base/src/com/ilscipio/scipio/ce/util/SafeOptional.java
+++ framework/base/src/com/ilscipio/scipio/ce/util/SafeOptional.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 import java.util.Optional;
 
 /**
- * Emulation of Java 8's {@link java.util.Optional}.
+ * SCIPIO: Emulation of Java 8's {@link java.util.Optional} that implements Serializable.
  * <p>
  * 2016: ABSOLUTELY REQUIRED in many cms data object classes to record lookups that returned
  * null values - to ensure preloadContent actually works properly and
@@ -15,12 +15,16 @@ import java.util.Optional;
  */
 @SuppressWarnings("serial")
 public class SafeOptional<A> implements Serializable {
-    private static final SafeOptional<?> EMPTY = new SafeOptional<>(null);
+    private static final SafeOptional<?> EMPTY = new SafeOptional<>();
 
     private final A value;
 
+    private SafeOptional() {
+        this.value = null;
+    }
+
     private SafeOptional(A value) {
-        this.value = value;
+        this.value = Objects.requireNonNull(value);
     }
 
     public static <T> SafeOptional<T> of(T value) {
@@ -57,12 +61,18 @@ public class SafeOptional<A> implements Serializable {
         if (this == obj) {
             return true;
         }
-
-        if (!(obj instanceof SafeOptional)) {
-            return false;
+        if (obj instanceof SafeOptional) {
+            SafeOptional<?> other = (SafeOptional<?>) obj;
+            return Objects.equals(value, other.value);
+        } else if (obj instanceof Optional) {
+            Optional<?> other = (Optional<?>) obj;
+            return Objects.equals(value, other.orElse(null));
         }
+        return false;
+    }
 
-        SafeOptional<?> other = (SafeOptional<?>) obj;
-        return Objects.equals(value, other.value);
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
     }
 }

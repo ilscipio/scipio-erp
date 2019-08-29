e23f1a7dd6934ab1119065f2ac38348f54484d20
Pascal Proulx <pplx@ilscipio.com>
2019-08-28 21:53:51 -0400

    solr: fix java imports from commit 09eb152de56100a45528b84a302f757c39ccde36
    (only this file fixed for now, requires further review)
    
    https://gitlab.ilscipio.com/scipio-dev/dev/scipioce-dev/issues/1123

--- applications/solr/src/com/ilscipio/scipio/solr/SolrExprUtil.java
+++ applications/solr/src/com/ilscipio/scipio/solr/SolrExprUtil.java
@@ -3,7 +3,17 @@ package com.ilscipio.scipio.solr;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 

a4143324148db88928e8de32dff449db67f9e6c5
Paul Piper <pp@ilscipio.com>
2022-02-16 08:59:40 +0100

    Fixes a bug in the prewarmcache screen
    (cherry picked from commit 9398c63638e2622fd86559350a4f166a99a46a94)

--- framework/webtools/webapp/webtools/WEB-INF/actions/cache/EditPrewarmCacheUrls.groovy
+++ framework/webtools/webapp/webtools/WEB-INF/actions/cache/EditPrewarmCacheUrls.groovy
@@ -2,5 +2,5 @@ import org.ofbiz.entity.GenericValue
 import org.ofbiz.entity.util.*;
 import org.ofbiz.entity.condition.*;
 
-List<GenericValue> websites = EntityQuery.use(delegator).select("webSiteId").from("WebSite").where(EntityCondition.makeCondition("productStoreId", EntityJoinOperator.NOT_EQUAL, null)).orderBy("siteName").queryList()
+List<GenericValue> websites = delegator.from("WebSite").where(EntityCondition.makeCondition("productStoreId", EntityJoinOperator.NOT_EQUAL, null)).orderBy("siteName").queryList()
 context.websites = websites;

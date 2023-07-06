34f6b31d6d96103749f9c3e8b746ece3848f3686
Pascal Proulx <pplx@ilscipio.com>
2023-07-05 20:52:32 -0400

    sitemap: patched sitemapgen4j 1.1.2 source for improved AltLink support

--- framework/base/src/com/redfin/sitemapgenerator/AltLink.java
+++ framework/base/src/com/redfin/sitemapgenerator/AltLink.java
@@ -10,9 +10,17 @@ public class AltLink { // SCIPIO: 3.0.0: Added
         this.url = url;
     }
 
+    public String url() {
+        return url;
+    }
+
     public AltLink rel(String rel) {
         this.rel = rel;
         return this;
+    }
+
+    public String rel() {
+        return rel;
     }
 
     public AltLink lang(String lang) {
@@ -20,8 +28,20 @@ public class AltLink { // SCIPIO: 3.0.0: Added
         return this;
     }
 
+    public String lang() {
+        return lang;
+    }
+
     public AltLink namespace(String namespace) {
         this.namespace = namespace;
         return this;
+    }
+
+    public String namespace() {
+        return namespace;
+    }
+
+    public String toLangUrlString() {
+        return lang() + "=" + url();
     }
 }

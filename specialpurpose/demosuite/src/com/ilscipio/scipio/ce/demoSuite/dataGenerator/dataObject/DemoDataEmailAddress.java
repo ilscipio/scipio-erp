f9e5d883ec37ff9136f0e6514d655bced2d4ada4
Jonatan Soto <jonatan.soto@ilscipio.com>
2023-09-18 16:57:26 +0200

    admin: data-generator: party data generator now supports email generation (via jfairy provider only)
    noref

---
+++ specialpurpose/demosuite/src/com/ilscipio/scipio/ce/demoSuite/dataGenerator/dataObject/DemoDataEmailAddress.java
@@ -0,0 +1,15 @@
+package com.ilscipio.scipio.ce.demoSuite.dataGenerator.dataObject;
+
+public class DemoDataEmailAddress implements AbstractDataObject {
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    private String emailAddress;
+
+}
